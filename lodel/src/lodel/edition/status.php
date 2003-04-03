d1043a38330b75362d33cce10cd2be3f9831c923
picard <picard>
2003-04-03 06:03:52 +0000

    Ajout de ce fichier pour permettre de modifier le status de publication d'une publication ou d'un document
    git-svn-id: https://subversion.renater.fr/lodel/trunk@85 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/edition/status.php
@@ -0,0 +1,45 @@
+<?
+
+// change le status d'un document ou d'une publication
+
+
+include ("lodelconfig.php");
+include ("$home/auth.php");
+authenticate(LEVEL_EDITEUR,NORECORDURL);
+include ("$home/func.php");
+
+include_once ("$home/connect.php");
+
+if ($status=="protege") {
+  $newstatus=+32;
+} elseif ($status=="deprotege") {
+  $newstatus=+1;
+} elseif ($status=="brouillon") {
+  $newstatus=-32;
+} elseif ($status=="pret") {
+  $newstatus=-1;
+} else {
+  die ("status invalide");
+}
+
+// ce script permet de changer le status, mais pas lui faire changer de signe.
+// pour publie ou depublie (changer le signe de status) il faut utiliser publi.php
+
+// le statut ne doit pas changer de signe...
+$critere=$newstatus>0 ? "status>0" : "status<0";
+
+if ($publication) {
+  $id=intval($publication);
+
+  mysql_query("UPDATE $GLOBALS[tableprefix]publications SET status=$newstatus WHERE id='$id' AND $critere") or die(mysql_error());
+
+} else {
+  $id=intval($id);
+
+  mysql_query("UPDATE $GLOBALS[tableprefix]documents SET status=$newstatus WHERE id='$id' AND $critere") or die(mysql_error());
+}
+
+back();
+return;
+
+?>
