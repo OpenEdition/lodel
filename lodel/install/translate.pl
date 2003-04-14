7b792a7cc3bd4c6def0e0f746cabd3f88540280e
picard <picard>
2003-04-14 20:52:37 +0000

    Script de conversion de l'ancien lodelscript au nouveau. Faire attention, ca ne marche pas forcement tres bien !
    git-svn-id: https://subversion.renater.fr/lodel/trunk@157 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/install/translate.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+#
+# Usage: versionning.pl liste de fichier
+#
+
+
+
+foreach $filename (@ARGV) {
+
+#lecture du fichier
+  open (TXT,$filename);
+  $file=join '',<TXT>;
+  close (TXT);
+
+  $change=0;
+  $change+= $file=~s/(<\/?)boucle\b/$1LOOP/gi;
+  $change+= $file=~s/(<\/?)avant\b/$1BEFORE/gi;
+  $change+= $file=~s/(<\/?)apres\b/$1AFTER/gi;
+  $change+= $file=~s/(<\/?)premier\b/$1FIRST/gi;
+  $change+= $file=~s/(<\/?)dernier\b/$1LAST/gi;
+  $change+= $file=~s/(<\/?)sinon\b/$1ALTERNATIVE/gi;
+  $change+= $file=~s/(<\/?)texte\b/$1TEXT/gi;
+  $change+= $file=~s/<if\s+([^>]+)\s*>/$a=$1; if ($a=~?COND=?) { $&; } else { $a=~y?\"?'?; "<IF COND=\"$a\">"; }/gei;
+
+  next unless $change;
+  print "$filename:",$change,"\n";
+
+# ecriture du fichier
+  open (TXT,">$filename");
+  print TXT $file;
+  close (TXT);
+}
