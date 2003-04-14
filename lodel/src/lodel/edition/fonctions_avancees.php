d26594e2df0aec80c5e7d800a651c2ccbd38d8a9
picard <picard>
2003-04-14 20:42:58 +0000

    Supression suite a discussion avec Nicolas
    git-svn-id: https://subversion.renater.fr/lodel/trunk@155 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/fonctions_avancees.php
+++
@@ -1,9 +0,0 @@
-<?
-// 
-include ("lodelconfig.php");
-include ("$home/auth.php");
-authenticate(LEVEL_EDITEUR,NORECORDURL);
-
-include ("$home/calcul-page.php");
-calcul_page($context,"fonctions_avancees");
-?>
