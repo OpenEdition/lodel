5e9da6bfbcd16eb257771a43d365ede242681f67
picard <picard>
2003-04-24 12:41:39 +0000

    Modif temporaire de index.php pour supprimer parent.
    git-svn-id: https://subversion.renater.fr/lodel/trunk@214 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/index.php
+++ lodel/src/lodel/edition/index.php
@@ -4,15 +4,18 @@ require("revueconfig.php");
 include ("$home/auth.php");
 authenticate(LEVEL_VISITEUR);
 
-if ($id) { // c'est l'id et non le parent qu'on veut... il faut chercher le parent
-  $id=intval($id);
-  $result=mysql_query ("SELECT parent FROM $GLOBALS[tableprefix]publications WHERE id='$id'") or die (mysql_error());
-  if (mysql_num_rows($result)<1) { header ("Location: not-found.html"); return; }
-  list($parent)=mysql_fetch_row($result);
-}
+//if ($id) { // c'est l'id et non le parent qu'on veut... il faut chercher le parent
+//  $id=intval($id);
+//  $result=mysql_query ("SELECT parent FROM $GLOBALS[tableprefix]publications WHERE id='$id'") or die (mysql_error());
+//  if (mysql_num_rows($result)<1) { header ("Location: not-found.html"); return; }
+//  list($parent)=mysql_fetch_row($result);
+//}
+//
 
+// workaround temporaire. TRES TEMPORAIRE
+if ($id) $parent=$id;
 
-$context[parent]=$parent=intval($parent);
+$context[id]=$context[parent]=$parent=intval($parent);
 
 if ($parent) {
   $result=mysql_query ("SELECT tpledit FROM $GLOBALS[tableprefix]typepublis,$GLOBALS[tableprefix]publications WHERE $GLOBALS[tableprefix]publications.id='$parent' AND type=$GLOBALS[tableprefix]typepublis.nom") or die (mysql_error());
