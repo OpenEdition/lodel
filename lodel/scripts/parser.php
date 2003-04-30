111c66de889abedb7bebaa23df11243d17a94883
picard <picard>
2003-04-30 09:45:42 +0000

    Correction du bug $pathroot vide. Lie a l'appel de revueconfig.php dans une fonction
    git-svn-id: https://subversion.renater.fr/lodel/trunk@250 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/parser.php
+++ lodel/scripts/parser.php
@@ -46,7 +46,7 @@ function parse ($in,$out)
   parse_texte($contents);
 
   $contents='<?
-require("revueconfig.php");include_once ("$home/connect.php");
+require_once("revueconfig.php");include_once ("$home/connect.php");
 '.$fct.'?>'.$contents;
 
   $contents=preg_replace(array('/\?><\?/',
