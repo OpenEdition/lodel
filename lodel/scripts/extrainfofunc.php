1e70b8b37cff0f2e14b3d105d462188707379c63
picard <picard>
2003-06-21 20:26:54 +0000

    Correction du bug des documentsannexes qui disparaissent quand on edite les meta
    git-svn-id: https://subversion.renater.fr/lodel/trunk@448 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/extrainfofunc.php
+++ lodel/scripts/extrainfofunc.php
@@ -137,7 +137,7 @@ function ei_edition($filename,$row,&$context,&$text,&$motcles,&$periodes,&$geogr
     $result=mysql_query("SELECT meta,status from documents WHERE id='$row[iddocument]'") or die (mysql_error());
     list($row[meta],$status)=mysql_fetch_row($result);
     if (!$row[status]) $status=$row[status]; // recupere le status si necessaire
-    supprime_document($row[iddocument]);
+    supprime_document($row[iddocument],TRUE,FALSE);
   } else { # Il n'existe pas, alors on calcule la date
     $context[duree]=intval($context[duree]);
     $time=localtime();
