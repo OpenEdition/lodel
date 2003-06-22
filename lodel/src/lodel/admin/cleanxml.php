e66d0c0c8a105a90283d39c68475d551f3559068
picard <picard>
2003-06-22 10:18:45 +0000

    Script de nettoyage des fichiers XML
    git-svn-id: https://subversion.renater.fr/lodel/trunk@458 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/admin/cleanxml.php
@@ -0,0 +1,38 @@
+<?
+// script de nettoyage des fichiers XML.
+// utile lorsque les versions se succede.
+
+require("revueconfig.php");
+include ("$home/auth.php");
+authenticate(LEVEL_ADMIN,NORECORDURL);
+include_once("$home/func.php");
+
+
+$search=array();
+$rpl=array();
+
+array_push($search,"/<sup><small><\/small><\/sup>/i");
+array_push($rpl,"");
+
+$dirname="../txt";
+if ($dir=@opendir ($dirname)) {
+  while (($file = readdir($dir)) !== false) {
+    $file=$dirname."/".$file;
+    if (!preg_match("/.xml$/",$file) || !is_file($file)) continue; // passe si ce n'est pas un fichier standart
+    echo "$file";
+    $text=join("",file($file));
+    $newtext=preg_replace($search,$rpl,$text);
+    if ($newtext!=$text) {
+      echo "...cleaned";
+      writefile($file,$newtext);
+    }
+    echo "<br>";
+  }  
+  closedir($dir);
+} else {
+  die ("impossible d'ouvrir le repertoire ../txt");
+}
+
+
+
+?>
