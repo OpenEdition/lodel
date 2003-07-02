40cab6da566dc2d6e15b5c54b7e4793449afbffe
picard <picard>
2003-07-02 14:00:53 +0000

    Faute de frappe
    git-svn-id: https://subversion.renater.fr/lodel/trunk@501 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/extrainfo.php
+++ lodel/src/lodel/edition/extrainfo.php
@@ -20,7 +20,7 @@ require_once($home."extrainfofunc.php");
 if ($edit || $plusauteurs) {
 
   if (ei_edition($filename,$row,$context,$text,$motcles,$periodes,$geographies)) { // ca marcher... on termine
-    $iddocument=ei_enregistre($filename,$row,$context,$text);
+    $iddocument=ei_enregistrement($filename,$row,$context,$text);
     //
     // termine en redirigeant correctement
     // 
