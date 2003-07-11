63f0aead563b050456004638f332ab18f9d91796
nutten <nutten>
2003-07-11 15:38:59 +0000

    Script permettant de forcer le t�l�chargement.
    git-svn-id: https://subversion.renater.fr/lodel/trunk@544 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/edition/telecharger.php
@@ -0,0 +1,27 @@
+<?
+require("revueconfig.php");
+include ("$home/auth.php");
+authenticate(LEVEL_REDACTEUR,NORECORDURL);
+
+$id=intval($id);
+if ($id>0 && $type=="xml") {
+ $filename="r2r-$id.xml";
+  $rep="../txt";
+} elseif ($id>0 && $type=="source") {
+ $filename="r2r-$id.rtf";
+  $rep="../rtf";
+} else die ("type ou id inconnu");
+
+if (!file_exists($rep."/".$filename)) die ("le fichier $filename n'existe pas");
+
+telecharger ($rep,$filename);
+
+function telecharger($rep,$filename)
+{
+	$path=$rep."/".$filename;
+	header("Content-type: application/force-download");
+	header("Content-Disposition: attachment; filename=$filename");
+	header("Content-type: application/$type");
+	readfile("$path"); 
+}
+?>
