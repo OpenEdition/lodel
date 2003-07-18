b0eb6b9bb9e78e4cfd213581a4f0ca78e4a158ce
picard <picard>
2003-07-18 12:51:29 +0000

    Fonciton pour inverser l'ordre dans une publi
    git-svn-id: https://subversion.renater.fr/lodel/trunk@582 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/edition/inverseordre.php
@@ -0,0 +1,30 @@
+<?
+
+// gere les publications. L'acces est reserve aux administrateurs de la revue.
+// assure l'edition, la supression, la restauration des publications.
+
+require("revueconfig.php");
+include ($home."auth.php");
+authenticate(LEVEL_EDITEUR,NORECORDURL);
+include($home."func.php");
+
+// calcul le critere pour determiner le user a editer, restorer, detruire...
+
+$parent=intval($parent);
+
+
+$critere="parent='$parent' AND status>-64";
+if (!$admin) $critere=." AND groupe IN ($usergroupes)";
+
+
+lock_write("publications");
+# cherche tous les enfants
+$result=mysql_query ("SELECT max(ordre) FROM $GLOBALS[tableprefix]publications WHERE $critere") or die (mysql_error());
+if (!mysql_num_rows($result)) { die ("vous n'avez pas les droits"); }
+list($max)=mysql_fetch_row($result);
+mysql_query("UPDATE $GLOBALS[tableprefix]publications SET ordre=$max-ordre WHERE $critere") or die (mysql_error());
+
+unlock("publications");
+back();
+
+?>
