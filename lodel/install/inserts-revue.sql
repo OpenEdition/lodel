71787461f85c19baa4b37ea22dd6855ab2d7bfdd
picard <picard>
2003-07-27 08:53:41 +0000

    Petite erreur pour auteur
    git-svn-id: https://subversion.renater.fr/lodel/trunk@613 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/install/inserts-revue.sql
+++ lodel/install/inserts-revue.sql
@@ -12,7 +12,7 @@ REPLACE INTO _PREFIXTABLE_groupes (id,nom) VALUES('1','tous');
 REPLACE INTO _PREFIXTABLE_typeentrees (id,nom,titre,style,tpl,tplindex,status,lineaire,newimportable,useabrev,tri,ordre) VALUES('1','periode','période','periodes','chrono','chronos','1','0','0','1','ordre','2');
 REPLACE INTO _PREFIXTABLE_typeentrees (id,nom,titre,style,tpl,tplindex,status,lineaire,newimportable,useabrev,tri,ordre) VALUES('4','geographie','géographie','geographies','geo','geos','1','0','0','1','ordre','3');
 REPLACE INTO _PREFIXTABLE_typeentrees (id,nom,titre,style,tpl,tplindex,status,lineaire,newimportable,useabrev,tri,ordre) VALUES('2','motcle','mot clé','motscles','mot','mots','1','1','1','0','nom','1');
-REPLACE INTO _PREFIXTABLE_typepersonnes (id,nom,titre,style,tpl,tplindex,status,ordre) VALUES('1','auteur','auteur','auteurs','auteur','auteur','1');
+REPLACE INTO _PREFIXTABLE_typepersonnes (id,nom,titre,style,tpl,tplindex,status,ordre) VALUES('1','auteur','auteur','auteurs','auteur','auteur','1','1');
 #else
 #REPLACE INTO _PREFIXTABLE_typepublis (nom,tpl,tpledit) VALUES('album_photo','sommaire-album','edition-album');
 #REPLACE INTO _PREFIXTABLE_typepublis (nom,tpl,tpledit) VALUES('theme_photo','sommaire-photo','edition-photo');
