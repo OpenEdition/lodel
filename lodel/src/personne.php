5026ddf9ed95f7cd5cce59e91c7ae801b9918876
picard <picard>
2003-07-27 08:42:04 +0000

    Ajout des fichiers de gestion des personnes
    git-svn-id: https://subversion.renater.fr/lodel/trunk@609 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/personne.php
@@ -0,0 +1,24 @@
+<?
+
+require("revueconfig.php");
+include ($home."auth.php");
+authenticate();
+
+
+if (!$type || !preg_match("/[\w-]/",$type)) die("type incorrecte");
+
+$context[id]=$id=intval($id);
+
+include_once($home."connect.php");
+$personnetable="";
+$result=mysql_query ("SELECT * FROM $GLOBALS[prefixtable]personnes id='$id' AND status>0") or die (mysql_error());
+if (!mysql_num_rows($result)) { header("location: not-found.html"); exit(); }
+$context=array_merge($context,mysql_fetch_assoc($result));
+
+
+$result=mysql_query ("SELECT tpl FROM $GLOBALS[prefixtable]typepersonnes WHERE nom='$type' AND status>0") or die (mysql_error());
+list($base)=mysql_fetch_row($result);
+
+include ($home."cache.php");
+
+?>
---
+++ lodel/src/personnes.php
@@ -0,0 +1,29 @@
+<?
+require_once("revueconfig.php");
+include ($home."auth.php");
+authenticate();
+include ($home."func.php");
+
+if (!$type || !preg_match("/[\w-]/",$type)) die("type incorrecte");
+if ($suffix && !preg_match("/^[\w-]+$/",$suffix)) die("suffix non accepte");
+
+include_once($home."connect.php");
+$result=mysql_query ("SELECT * FROM $GLOBALS[tableprefix]typepersonnes WHERE nom='$type' AND status>0") or die (mysql_error());
+$context=array_merge_withprefix($context,"type_",mysql_fetch_assoc($result));
+$context[idtype]=$context[type_id]; // import
+$context[type_tri]=$GLOBALS[tableprefix]."personnes.".$context[type_tri];  // prefix par la table... ca aide
+
+$base=$context[type_tplindex].$suffix;
+include ($home."cache.php");
+
+
+function boucle_alphabet(&$context,$funcname)
+
+{
+  for($l="A"; $l!="AA"; $l++) {
+    $context[lettre]=$l;
+    call_user_func("code_boucle_$funcname",$context);
+  }
+}
+
+?>
