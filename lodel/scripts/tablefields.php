c72024e55fcf558743d097ae86141d049327b03b
picard <picard>
2003-09-16 14:47:23 +0000

    Type:Bm Descr:Remise au propre de la correction de Nicolas
    git-svn-id: https://subversion.renater.fr/lodel/trunk@715 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/tablefields.php
+++ lodel/scripts/tablefields.php
@@ -27,12 +27,12 @@ function var_export($arr,$t)
 foreach (array($GLOBALS[database] =>$GLOBALS[database].".", $GLOBALS[currentdb]=>"") as $db => $prefix) {
   $result=mysql_list_tables($db) or die(mysql_error());
   while (list($table)=mysql_fetch_row($result)) {
-    $GLOBALS[tablefields][$table]=array();
     $result2=mysql_list_fields($db,$table);
     $nfields=mysql_num_fields($result2);
-    $GLOBALS[tablefields][$prefix.$table]=array();
+    $table=$prefix.$table;
+    $GLOBALS[tablefields][$table]=array();
     for($j=0; $j<$nfields; $j++) {
-	  array_push($GLOBALS[tablefields][$prefix.$table],mysql_field_name($result2,$j));
+      array_push($GLOBALS[tablefields][$table],mysql_field_name($result2,$j));
     }
   }
 }
