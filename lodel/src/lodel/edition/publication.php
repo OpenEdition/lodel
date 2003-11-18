c891bda5031c60f04dad61dd95eec3daf4e5bb23
picard <picard>
2003-11-18 21:09:54 +0000

    Type:F Descr:utilisation de tplcreation pour aller chercher le template
    git-svn-id: https://subversion.renater.fr/lodel/trunk@876 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/publication.php
+++ lodel/src/lodel/edition/publication.php
@@ -11,9 +11,8 @@ include($home."entitefunc.php");
 // calcul le critere pour determiner le user a editer, restorer, detruire...
 
 $context[id]=$id=intval($id);
-if ($parent) { die("veuillez changer l'appel, parent =>idparent");$idparent=$parent; }
+if ($parent) { die("veuillez changer l'appel, parent =>idparent"); }
 $context[idparent]=$idparent=intval($idparent);
-
 
 if ($id>0 && !$admin) {
   $critere=" AND groupe IN ($usergroupes)";
@@ -45,27 +44,33 @@ if ($id>0 && $dir) {
 
   extract_post();
   // edition et sort si ca marche
+  if ($context[nom]) 
   if (enregistre_entite($context,$id,"publications")) back();
 
 } elseif ($id>0) {
   include_once ($home."connect.php");
-  $result=mysql_query("SELECT $GLOBALS[tp]publications.*, $GLOBALS[tp]entites.*, type  FROM  $GLOBALS[publicationstypesjoin] WHERE $GLOBALS[tp]entites.id='$id'  $critere") or die (mysql_error());
+  $result=mysql_query("SELECT $GLOBALS[tp]publications.*, $GLOBALS[tp]entites.*, type,tplcreation  FROM  $GLOBALS[publicationstypesjoin] WHERE $GLOBALS[tp]entites.id='$id'  $critere") or die (mysql_error());
   $context[entite]=mysql_fetch_assoc($result);
-  $context[idparent]=$context[entite][idparent];
-  $context[type]=$context[entite][type];
-  $context[idtype]=$context[entite][idtype];
-  $context[nom]=$context[entite][nom];
+  // transfer dans le context
+  foreach(array("idparent","type","idtype","tplcreation","nom") as $k) {
+    $context[$k]=$context[entite][$k]; unset($context[entite][$k]);
+  }
   extrait_personnes($id,&$context);
   extrait_entrees($id,&$context);
 } else {
   include_once ($home."textfunc.php");
   $context[type]=trim(rmscript(strip_tags($type)));
   if ($context[type]) {
-    $result=mysql_query("SELECT id FROM $GLOBALS[tp]types WHERE type='$context[type]' AND statut>0") or die (mysql_error());
+    $result=mysql_query("SELECT id,tplcreation FROM $GLOBALS[tp]types WHERE type='$context[type]' AND statut>0") or die (mysql_error());
     if (!mysql_num_rows($result)) die("type inconnu $context[type]");
-    list($context[idtype])=mysql_fetch_row($result);
+    list($context[idtype],$context[tplcreation])=mysql_fetch_row($result);
   }
   $context[entite]=array();
+}
+
+if (!$context[tplcreation]) {
+  $result=mysql_query("SELECT tplcreation FROM $GLOBALS[tp]types WHERE id='$context[idtype]'") or die (mysql_error());
+  list($context[tplcreation])=mysql_fetch_row($result);
 }
 
 
@@ -73,8 +78,7 @@ if ($id>0 && $dir) {
 posttraitement($context);
 
 include ($home."calcul-page.php");
-calcul_page($context,"publication");
-
-
+calcul_page($context,$context[tplcreation]);
+#calcul_page($context,"publication");
 
 ?>
