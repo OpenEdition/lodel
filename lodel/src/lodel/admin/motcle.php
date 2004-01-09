ce6470879498dd6df8aabf74758dcb0109caad3f
luc <luc>
2004-01-09 14:11:22 +0000

    Type:F Descr:ajout des entetes GPL
    git-svn-id: https://subversion.renater.fr/lodel/trunk@979 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/admin/motcle.php
+++ lodel/src/lodel/admin/motcle.php
@@ -1,4 +1,31 @@
 <?php
+/*
+ *
+ *  LODEL - Logiciel d'Edition ELectronique.
+ *
+ *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ *  Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ *
+ *  Home page: http://www.lodel.org
+ *
+ *  E-Mail: lodel@lodel.org
+ *
+ *                            All Rights Reserved
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
+
 die("fichier a supprimer du CVS");
 // gere les mots cle permanent. L'acces est reserve au adminlodelistrateur.
 // assure l'edition, la supression, la restauration des periodes.
