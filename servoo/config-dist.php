56544df5541bf66f6634bfb5c38fbff1062df619
picard <picard>
2004-02-01 21:48:24 +0000

    Type:F Descr:fichier exemple pour la config du serveur.
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1050 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ servoo/config-dist.php
@@ -0,0 +1,65 @@
+<?
+/*
+ *
+ *  LODEL - Logiciel d'Edition ELectronique.
+ *
+ *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ *  Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ *
+ *  Home page: http://www.lodel.org
+ *
+ *  E-Mail: lodel@lodel.org
+ *
+ *                            All Rights Reserved
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
+
+
+# Nom de la base de donnees
+$database="lodeldevelserveuroo";
+
+# Nom d'utilisateur
+$dbusername="lodeluser";
+# Mot de passe
+$dbpasswd="XXXXXXXXXX";
+# Hote de la BD
+$dbhost="localhost";
+
+# repertoire d'installation de java
+$javapath="/usr/java/j2sdk1.4.1_02";
+
+# directory to the OpenOffice classes.
+$openofficeclassespath="/usr/local/OpenOffice.org1.1.0/program/classes";
+
+# host surlequel est le serveur OpenOffice
+$servoohost="localhost";
+
+# port du serveur OpenOffice sur le host
+$servooport="9303";
+
+# chemin pour la commande zip
+$zipcmd="/usr/bin/zip";
+
+# chemin pour la commande unzip
+$unzipcmd="/usr/bin/unzip";
+
+# Prefix pour les tables, utile quand on utilise qu'une seule database pour plusieurs applications
+$tp="";
+
+# chemin relatif pour le repertoire d'include
+
+$home="include/";
+
+?>
