3bf1630f45d40c232653e601749d1e71347e0c37
picard <picard>
2004-02-03 19:53:42 +0000

    Type:F Descr:maintenant publication.php et document.php sont identiques
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1060 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/publication.php
+++ lodel/src/lodel/edition/publication.php
@@ -32,80 +32,8 @@
 require("siteconfig.php");
 include ($home."auth.php");
 authenticate(LEVEL_EDITEUR,NORECORDURL);
-include($home."func.php");
-include($home."entitefunc.php");
 
-// calcul le critere pour determiner le user a editer, restorer, detruire...
-
-$context[id]=$id=intval($id);
-if ($parent) { die("veuillez changer l'appel, parent =>idparent"); }
-$context[idparent]=$idparent=intval($idparent);
-
-if ($id>0 && !$admin) {
-  $critere=" AND groupe IN ($usergroupes)";
-} else $critere="";
-
-if ($id>0 && $dir) {
-  lock_write("entites");
-  # cherche le parent
-  $result=mysql_query ("SELECT idparent FROM $GLOBALS[tp]entites WHERE id='$id' $critere") or die (mysql_error());
-  if (!mysql_num_rows($result)) { die ("vous n'avez pas les droits"); }
-  list($idparent)=mysql_fetch_row($result);
-  chordre("entites",$id,"idparent='$idparent'",$dir);
-  unlock("entites");
-  back();
-
-//
-// supression et restauration
-//
-} elseif ($id>0 && ($delete || $restore)) { 
-  include ($home."trash.php");
-  die ("il faut utiliser supprime.php a la place");
-  return;
-//
-// ajoute ou edit
-//
-} elseif ($plus) {
-  extract_post();
-} elseif ($edit) { // modifie ou ajoute
-
-  extract_post();
-  // edition et sort si ca marche
-  if ($context[nom]) 
-  if (enregistre_entite($context,$id,"publications")) back();
-
-} elseif ($id>0) {
-  include_once ($home."connect.php");
-  $result=mysql_query("SELECT $GLOBALS[tp]publications.*, $GLOBALS[tp]entites.*, type,tplcreation  FROM  $GLOBALS[publicationstypesjoin] WHERE $GLOBALS[tp]entites.id='$id'  $critere") or die (mysql_error());
-  $context[entite]=mysql_fetch_assoc($result);
-  // transfer dans le context
-  foreach(array("idparent","type","idtype","tplcreation","nom") as $k) {
-    $context[$k]=$context[entite][$k]; unset($context[entite][$k]);
-  }
-  extrait_personnes($id,&$context);
-  extrait_entrees($id,&$context);
-} else {
-  require_once ($home."validfunc.php");
-  $context[type]=trim(($type));
-  if (!$context[type] || !isvalidtype($context[type])) die("preciser un type valide");
-
-  $result=mysql_query("SELECT id,tplcreation FROM $GLOBALS[tp]types WHERE type='$context[type]' AND statut>0") or die (mysql_error());
-  if (!mysql_num_rows($result)) die("type inconnu $context[type]");
-  list($context[idtype],$context[tplcreation])=mysql_fetch_row($result);
-  $context[entite]=array();
-}
-
-if (!$context[tplcreation]) {
-  if (!$context[idtype]) die("preciser un type");
-  $result=mysql_query("SELECT tplcreation FROM $GLOBALS[tp]types WHERE id='$context[idtype]'") or die (mysql_error());
-  list($context[tplcreation])=mysql_fetch_row($result);
-}
-
-
-// post-traitement
-posttraitement($context);
-
-include ($home."calcul-page.php");
-calcul_page($context,$context[tplcreation]);
+$classe="publications";
+require($home."entite.php");
 
 ?>
