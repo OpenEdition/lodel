50991fc5b52fc2398a6b2b05efc34f981421a809
picard <picard>
2004-02-05 21:24:00 +0000

    Type:F Descr:replacement de include en require
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1079 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/admin/user.php
+++ lodel/src/lodel/admin/user.php
@@ -30,15 +30,15 @@
 // assure l'edition, la supression, la restauration des utilisateurs.
 
 require("siteconfig.php");
-include ($home."auth.php");
+require($home."auth.php");
 authenticate(LEVEL_ADMIN,NORECORDURL);
 #include_once ($home."func.php");
 
 #if ($context[privilege]>=LEVEL_ADMINLODEL) return;
-include ($home."userinc.php");
+require($home."userinc.php");
 
 
-include ($home."calcul-page.php");
+require($home."calcul-page.php");
 calcul_page($context,"user");
 
 ?>
