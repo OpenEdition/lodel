3b6f8bfe427c1d8e2eb05bc2e95ccd3874052d4c
picard <picard>
2004-02-23 18:16:22 +0000

    Type:B Descr:gestion des IMG importees avec une URL complete (http:)
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1224 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/func.php
+++ lodel/scripts/func.php
@@ -224,6 +224,7 @@ function copy_images_private (&$text,$callback,$argument,&$count,&$imglist)
     preg_match_all("/<img\b[^>]+src=\"([^\"]+\.([^\"\.]+))\"/i",$text,$results,PREG_SET_ORDER);
     foreach ($results as $result) {
       $imgfile=$result[1];
+      if (substr($imgfile,5)=="http:") continue;
       if ($imglist[$imgfile]) {
 	$text=str_replace($result[0],"<img src=\"$imglist[$imgfile]\"",$text);
       } else {
