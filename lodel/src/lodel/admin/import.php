a7eda936682033dd977cbf98a324c73042d5ba5c
picard <picard>
2004-03-28 20:22:19 +0000

    Type:B Descr:probleme de prefix
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1423 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/admin/import.php
+++ lodel/src/lodel/admin/import.php
@@ -1,30 +1,30 @@
 <?php
-/*
- *
- *  LODEL - Logiciel d'Edition ELectronique.
- *
- *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
- *  Copyright (c) 2003-2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
- *
- *  Home page: http://www.lodel.org
- *
- *  E-Mail: lodel@lodel.org
- *
- *                            All Rights Reserved
- *
- *     This program is free software; you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation; either version 2 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software
- *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
+  /*
+   *
+   *  LODEL - Logiciel d'Edition ELectronique.
+   *
+   *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+   *  Copyright (c) 2003-2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+   *
+   *  Home page: http://www.lodel.org
+   *
+   *  E-Mail: lodel@lodel.org
+   *
+   *                            All Rights Reserved
+   *
+   *     This program is free software; you can redistribute it and/or modify
+   *     it under the terms of the GNU General Public License as published by
+   *     the Free Software Foundation; either version 2 of the License, or
+   *     (at your option) any later version.
+   *
+   *     This program is distributed in the hope that it will be useful,
+   *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+   *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   *     GNU General Public License for more details.
+   *
+   *     You should have received a copy of the GNU General Public License
+   *     along with this program; if not, write to the Free Software
+   *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
 
 require("siteconfig.php");
 include ($home."auth.php");
@@ -41,108 +41,117 @@ $importdirs=array($importdir,"CACHE");
 $archive=$_FILES['archive']['tmp_name'];
 $context['erreur_upload']=$_FILES['archive']['error'];
 if (!$context['erreur_upload'] && $archive && $archive!="none" && is_uploaded_file($archive)) { // Upload
-  $prefix="*";
+  $prefixre="(site|revue)";
+  $prefixunix="{site,revue}";
   $fichier=$archive;
 
 } elseif ($fichier && preg_match("/^(?:".str_replace("/",'\/',join("|",$importdirs)).")\/$fileregexp$/",$fichier,$result) && file_exists($fichier)) { // fichier sur le disque
-  $prefix=$result[1];
+  $prefixre=$prefixunix=$result[1];
 
 } else { // rien
   $fichier="";
 }
 
-
 if ($fichier) {
-#  // detar dans le repertoire du site
-#  $listfiles=`tar ztf $fichier 2>/dev/null`;
-#  $dirs="";
+
+  do { // control block 
+    #  // detar dans le repertoire du site
+    #  $listfiles=`tar ztf $fichier 2>/dev/null`;
+    #  $dirs="";
 #  foreach (array("lodel/txt","lodel/rtf","lodel/sources","docannexe") as $dir) {
 #    if (preg_match("/^(\.\/)?".str_replace("/",'\/',$dir)."\b/m",$listfiles) && file_exists(SITEROOT.$dir)) $dirs.=$dir." ";
-#  }
+    #  }
 #  if ($dirs) {
-#    #echo "tar zxf $fichier -C ".SITEROOT." $dirs 2>&1";
+    #    #echo "tar zxf $fichier -C ".SITEROOT." $dirs 2>&1";
 #    system("tar zxf $fichier -C ".SITEROOT." $dirs 2>&1")!==FALSE or die ("impossible d'executer tar");
-#  }
+    #  }
 
+    $tmpfile=tempnam(tmpdir(),"lodelimport_");
+    $accepteddirs=array("lodel/txt","lodel/rtf","lodel/sources","docannexe/fichier","docannexe/image");
+#system("tar zxf $fichier -O '$prefixunix-*.sql' >$tmpfile")!==FALSE or die ("impossible d'executer tar");
 
-  $tmpfile=tempnam(tmpdir(),"lodelimport_");
-  $accepteddirs=array("lodel/txt","lodel/rtf","lodel/sources","docannexe/fichier","docannexe/image");
-  #system("tar zxf $fichier -O '$prefix-*.sql' >$tmpfile")!==FALSE or die ("impossible d'executer tar");
-
-  if ($unzipcmd && $unzipcmd!="pclzip") {
-    $listfiles=`$unzipcmd -Z -1 $fichier`;
-    $dirs="";
-    foreach ($accepteddirs as $dir) {
-      if (preg_match("/^(\.\/)?".str_replace("/",'\/',$dir)."\//m",$listfiles) && file_exists(SITEROOT.$dir)) $dirs.=$dir."/* ".$dir."/*/* ";
-    }
-    if (!chdir (SITEROOT)) die("ERROR: chdir fails");
-    system ($unzipcmd." -oq $fichier  $dirs");
-    if (!chdir ("lodel/admin")) die("ERROR: chdir 2 fails");
-    system ($unzipcmd." -qp $fichier  $prefix-*.sql >$tmpfile");
-  } else { // PCLZIP
-    require($home."pclzip.lib.php");
-    $archive=new PclZip($fichier);
-    // function callback
-    function preextract($p_event, &$p_header) { // choose the files to extract
-      global $accepteddirs,$prefix,$tmpfile;
-      if (preg_match("/^(\.\/)*$prefix-.*\.sql$/",$p_header['filename'])) { // extract the sql file
-	unlink($tmpfile); // remove the tmpfile if not it is not overwriten... 
-	//                   may cause problem if the file is recreated but it's so uncertain !
-	$p_header['filename']=$tmpfile;
+    if ($unzipcmd && $unzipcmd!="pclzip") {
+      $listfiles=`$unzipcmd -Z -1 $fichier`;
+      if (!$listfiles) { $err=1; break; }
+      $dirs="";
+      foreach ($accepteddirs as $dir) {
+	if (preg_match("/^(\.\/)?".str_replace("/",'\/',$dir)."\//m",$listfiles) && file_exists(SITEROOT.$dir)) $dirs.=$dir."/* ".$dir."/*/* ";
+      }
+      if (!chdir (SITEROOT)) die("ERROR: chdir fails");
+      system ($unzipcmd." -oq $fichier  $dirs");
+      if (!chdir ("lodel/admin")) die("ERROR: chdir 2 fails");
+      system ($unzipcmd." -qp $fichier  $prefixunix-*.sql >$tmpfile");
+      if (filesize($tmpdir)<=0) { $err=1; break; }
+    } else { // PCLZIP
+      require($home."pclzip.lib.php");
+      $archive=new PclZip($fichier);
+      // function callback
+      function preextract($p_event, &$p_header) { // choose the files to extract
+	global $accepteddirs,$prefixre,$tmpfile;
+	if (preg_match("/^(\.\/)*$prefixre-.*\.sql$/",$p_header['filename'])) { // extract the sql file
+	  unlink($tmpfile); // remove the tmpfile if not it is not overwriten... 
+	  //                   may cause problem if the file is recreated but it's so uncertain !
+	  $p_header['filename']=$tmpfile;
+	  return 1;
+	}
+	if (preg_match("/^(\.\/)*".str_replace("/","\/",join("|",$accepteddirs))."\//",$p_header['filename'])) {
+	  $p_header['filename']=SITEROOT.$p_header['filename'];
+	  return 1;
+	}
+	return 0; // don't extract
+      }
+      function postextract($p_event, &$p_header) { // chmod
+	global $tmpfile;
+	if ($p_header['filename']!=$tmpfile && file_exists($p_header['filename'])) {
+	  @chmod($p_header['filename'],octdec($GLOBALS[filemask]) & 
+		 (substr($p_header['filename'],-1)=="/" ? 0777 : 0666));
+	}
 	return 1;
       }
-      if (preg_match("/^(\.\/)*".str_replace("/","\/",join("|",$accepteddirs))."\//",$p_header['filename'])) {
-	$p_header['filename']=SITEROOT.$p_header['filename'];
-	return 1;
-      }
-      return 0; // don't extract
+      $archive->extract(PCLZIP_CB_PRE_EXTRACT, 'preextract',
+			PCLZIP_CB_POST_EXTRACT, 'postextract');
+
+      if (filesize($tmpdir)<=0) { $err=1; $context[erreur_extract]=1; break; }
+A METTRE PARTOUT L'eRREUR erreur_extract
     }
-    function postextract($p_event, &$p_header) { // chmod
-      global $tmpfile;
-      if ($p_header['filename']!=$tmpfile && file_exists($p_header['filename'])) {
-	@chmod($p_header['filename'],octdec($GLOBALS[filemask]) & 
-	       (substr($p_header['filename'],-1)=="/" ? 0777 : 0666));
+
+    require_once ($home."connect.php");
+    require_once ($home."backupfunc.php");
+
+    // drop les tables existantes
+    // recupere la liste des tables
+
+    mysql_query("DROP TABLE IF EXISTS ".join(",",$GLOBALS[lodelsitetables])) or die(mysql_error()); 
+
+    if (!execute_dump($tmpfile)) $context[erreur_execute_dump]=$err=mysql_error();
+    @unlink($tmpfile);
+
+    require_once($home."cachefunc.php");
+    removefilesincache(SITEROOT,SITEROOT."lodel/edition",SITEROOT."lodel/admin");
+
+    // verifie les .htaccess dans le CACHE
+    $dirs=array("CACHE","lodel/admin/CACHE","lodel/edition/CACHE","lodel/txt","lodel/rtf","lodel/sources");
+    foreach ($dirs as $dir) {
+      if (!file_exists(SITEROOT.$dir)) continue;
+      $file=SITEROOT.$dir."/.htaccess";
+      if (file_exists($file)) @unlink($file);
+      $f=@fopen ($file,"w");
+      if (!$f) {
+	$context[erreur_htaccess].=$dir." ";
+	$err=1;
+      } else {
+	fputs($f,"deny from all\n");
+	fclose ($f);
       }
-      return 1;
     }
-    $archive->extract(PCLZIP_CB_PRE_EXTRACT, 'preextract',
-		      PCLZIP_CB_POST_EXTRACT, 'postextract');
+
+  } while(0);
+
+  if (!$err) { 
+    include_once ($home."func.php"); 
+    touch(SITEROOT."CACHE/maj");
+    back();
   }
-
-  require_once ($home."connect.php");
-  require_once ($home."backupfunc.php");
-
-  // drop les tables existantes
-  // recupere la liste des tables
-
-  mysql_query("DROP TABLE IF EXISTS ".join(",",$GLOBALS[lodelsitetables])) or die(mysql_error()); 
-
-  if (!execute_dump($tmpfile)) $context[erreur_execute_dump]=$err=mysql_error();
-  @unlink($tmpfile);
-
-  require_once($home."cachefunc.php");
-  removefilesincache(SITEROOT,SITEROOT."lodel/edition",SITEROOT."lodel/admin");
-
-// verifie les .htaccess dans le CACHE
-  $dirs=array("CACHE","lodel/admin/CACHE","lodel/edition/CACHE","lodel/txt","lodel/rtf","lodel/sources");
-   foreach ($dirs as $dir) {
-     if (!file_exists(SITEROOT.$dir)) continue;
-     $file=SITEROOT.$dir."/.htaccess";
-     if (file_exists($file)) @unlink($file);
-     $f=@fopen ($file,"w");
-     if (!$f) {
-       $context[erreur_htaccess].=$dir." ";
-       $err=1;
-     } else {
-       fputs($f,"deny from all\n");
-       fclose ($f);
-     }
-   }
-   if (!$err) { 
-     include_once ($home."func.php"); 
-     touch(SITEROOT."CACHE/maj");
-     back();
-   }
 }
 
 
