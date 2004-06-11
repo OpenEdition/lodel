5ee105d8f2297b95a6537ff99711493b2dc28e7e
picard <picard>
2004-06-11 21:57:19 +0000

    Type:B Descr:
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1561 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/serveurfunc.php
+++ lodel/scripts/serveurfunc.php
@@ -35,17 +35,17 @@ function contact_servoo ($cmds,$uploadedfiles,$destfile="")
   $options=getoption(array("servoourl","servoousername","servoopasswd"),"");
 
   if (!$options || !$options[servoourl]) { // get form the lodelconfig file
-    $options[servoourl]=$GLOBALS[servoourl];
-    $options[servoousername]=$GLOBALS[servoousername];
-    $options[servoopasswd]=$GLOBALS[servoopasswd];
+    $options['servoourl']=$GLOBALS['servoourl'];
+    $options['servoousername']=$GLOBALS['servoousername'];
+    $options['servoopasswd']=$GLOBALS['servoopasswd'];
   }
   if (!is_array($uploadedfiles)) $uploadedfiles=array($uploadedfiles);
 
-  $ret=upload($options[servoourl],
-	      array("username"=>$options[servoousername],
-		    "passwd"=>$options[servoopasswd],
+  $ret=upload($options['servoourl'],
+	      array("username"=>$options['servoousername'],
+		    "passwd"=>$options['servoopasswd'],
 		    "commands"=>$cmds,
-		    "lodelversion"=>$GLOBALS[version]),
+		    "lodelversion"=>$GLOBALS['version']),
 	      $uploadedfiles, # fichier a uploaded
 	      0, # cookies
 	      $destfile
