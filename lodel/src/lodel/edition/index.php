853c93f98e5896e4ddb493dde781e236b649a4ab
picard <picard>
2004-06-12 10:24:55 +0000

    Type:F Descr:generisication de classe
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1562 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/index.php
+++ lodel/src/lodel/edition/index.php
@@ -35,7 +35,7 @@ $context[idtype]=0;
 
 if ($id) {
   do {
-    $result=mysql_query ("SELECT tpledition,idparent,idtype FROM $GLOBALS[entitestypesjoin] WHERE $GLOBALS[tp]entites.id='$id' AND $GLOBALS[tp]types.classe='publications'") or die (mysql_error());
+    $result=mysql_query ("SELECT tpledition,idparent,idtype FROM $GLOBALS[entitestypesjoin] WHERE $GLOBALS[tp]entites.id='$id'") or die (mysql_error());
     if (mysql_num_rows($result)<1) { header ("Location: not-found.html"); return; }
     list($base,$idparent,$context[idtype])=mysql_fetch_row($result);
     if (!$base) $context[id]=$id=$idparent;
