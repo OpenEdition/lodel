be0ddad8b7ce9aeaaa5fb0b1c8eebb58cf48dd91
picard <picard>
2004-06-13 21:52:31 +0000

    Type:B Descr:probleme de case dans les styles pour la reconnaissance des styles T et P
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1579 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/edition/oochargement.php
+++ lodel/src/lodel/edition/oochargement.php
@@ -471,12 +471,12 @@ Will be reimplemented using a proper XML parser.
       $cssarr=preg_split("/\{([^\}]*)\}/",$css,-1,PREG_SPLIT_DELIM_CAPTURE);
       for($i=0; $i<count($cssarr); $i+=2) {	
 	$style=trim($cssarr[$i]);
-	if (preg_match("/^\w+\.[PT]\d+$/",$style)) $styles[$style]=$cssarr[$i+1]." "; // add a space to be sure it is TRUE.
+	if (preg_match("/^\w+\.[PTpt]\d+$/",$style)) $styles[strtolower($style)]=$cssarr[$i+1]." "; // add a space to be sure it is TRUE.
       }
 
       if ($styles) {
 	// look for the style in the file, and replace by the style
-	$file=preg_replace('/((<(\w+)\b[^>]+)class="([PT]\d+)")/e','($styles["\\3.\\4"]) ? "\\2style=\"".trim($styles["\\3.\\4"])."\"" : "\\1" ',$file);
+	$file=preg_replace('/((<(\w+)\b[^>]+)class="([PTpt]\d+)")/e','($styles["\\3.\\4"]) ? "\\2style=\"".trim($styles[strtolower("\\3.\\4")])."\"" : "\\1" ',$file);
       }
     }
 
