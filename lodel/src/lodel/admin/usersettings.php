db759c3b64e8d24e796887584400fbebf9d852fb
picard <picard>
2004-10-21 07:20:22 +0000

    Type:B Descr:ajout
    git-svn-id: https://subversion.renater.fr/lodel/trunk@1835 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/admin/usersettings.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ *
+ *  LODEL - Logiciel d'Edition ELectronique.
+ *
+ *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ *  Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ *  Copyright (c) 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ *
+ *  Home page: http://www.lodel.org
+ *
+ *  E-Mail: lodel@lodel.org
+ *
+ *                            All Rights Reserved
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
+
+require("siteconfig.php");
+require($home."auth.php");
+authenticate(LEVEL_VISITEUR,NORECORDURL);
+require($home."func.php");
+
+if ($lang) {
+  if (!preg_match("/^\w\w(-\w\w)?$/",$lang)) die("ERROR: invalid lang");
+  mysql_query("UPDATE $GLOBALS[tp]users SET lang='$lang'") or die(mysql_error());
+  setcontext("userlang","setvalue",$lang);
+}
+
+if ($translationmode) {
+  setcontext("usertranslationmode","toggle");
+}
+
+back();
+
+
+function setcontext($var,$operation,$value="") {
+  mysql_select_db($GLOBALS['database']);
+  $where="name='".addslashes($_COOKIE[$GLOBALS[sessionname]])."' AND iduser='".$GLOBALS['iduser']."'";
+  $result=mysql_query("SELECT context FROM $GLOBALS[tp]session WHERE ".$where) or die(mysql_error());
+  list($context)=mysql_fetch_row($result);
+  $arr=unserialize($context);
+  switch ($operation) {
+  case "toggle" :  $arr[$var]=$arr[$var] ? 0 : 1; // toggle
+    break;
+  case "setvalue" : $arr[$var]=$value;  // set
+    break;
+  case "clear" : unset($arr[$var]);  // clear
+    break;
+  }
+  mysql_query ("UPDATE $GLOBALS[tp]session SET context='".addslashes(serialize($arr))."' WHERE ".$where) or die(mysql_error());
+  mysql_select_db($GLOBALS['currentdb']);
+}
+
+?>
