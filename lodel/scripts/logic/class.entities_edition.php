cb47a41a6c89065aa8d5b4717f09f9473f677fd0
picard <picard>
2005-01-18 11:30:13 +0000

    Type:B Descr:debugage rapide. Function a finir
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2058 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic/class.entities_edition.php
+++ lodel/scripts/logic/class.entities_edition.php
@@ -126,6 +126,11 @@ class Entities_EditionLogic extends GenericLogic {
        }
      }
      /////
+       function loop_entity_select($context,$funcname)
+       {
+	 call_user_func("code_alter_$funcname",$context);
+       }
+     /////
 
      $ret = GenericLogic::viewAction($context,$error);
 
