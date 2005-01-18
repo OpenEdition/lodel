0011b9cbf368abc28393056cfb316890d9bf8609
picard <picard>
2005-01-18 11:31:58 +0000

    Type:B Descr:entitybrowser pour les relations
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2060 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/edition/entitybrowser.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ *
+ *  LODEL - Logiciel d'Edition ELectronique.
+ *
+ *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ *  Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ *  Copyright (c) 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ *
+ *  Home page: http://www.lodel.org
+ *
+ *  E-Mail: lodel@lodel.org
+ *
+ *                            All Rights Reserved
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
+
+
+require("siteconfig.php");
+require_once ($home."auth.php");
+authenticate(LEVEL_REDACTOR);
+
+define("UPLOADDIR",SITEROOT."CACHE/upload");
+
+$context['caller']=$_REQUEST['caller'];
+$context['single']=isset($_REQUEST['single']);
+
+$ids=explode(",",$_REQUEST['value']);
+
+$nodesk=true;
+require ($home."view.php");
+$view=New View;
+$view->renderCached($context,"entitybrowser");
+
+function getchecked($id)
+
+{
+  return in_array($GLOBALS['ids'],$id) ? "checked" : "";
+}
+
+?>
