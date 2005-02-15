241872c625f3ea4f855da3a0115728b91cffef17
lamy <lamy>
2005-02-15 14:23:30 +0000

    Type:Bm Descr:Some corrections ; improved accents removal
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2223 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/search.inc.php
+++ lodel/scripts/search.inc.php
@@ -32,6 +32,7 @@
   * @author Jean Lamy
   * @since 2005-02-02
   * 
+  * 
   */
   
 /**
@@ -48,7 +49,7 @@ function search($context)
 	//if decided change the system in order to manage strict queries (with quotes)
 	
 	
-	require_once("../../../lodel/scripts/dao.php");
+	require_once("dao.php");
 	global $db;
 	require_once("func.php");
 	if(!$context['query'])
@@ -61,11 +62,11 @@ function search($context)
    	 $query = strtr( $query , $regs , preg_replace("/./", " " , $regs ) );
 #echo "string=$string<br />\n";
    	 //particular case : two letter acronym or initials
-   	 $query = preg_replace("/ ([A-Z][0-9A-Z]{1,2}) /", ' \\1___ ', $query);
+   	 $query = preg_replace("/\s([A-Z][0-9A-Z]{1,2})\s/", ' \\1___ ', $query);
 	$query = strtolower( $query );
 			
 	//cut query string in token
-	$tokens = preg_split("/[\s]+/", $query );
+	$tokens = preg_split("/\s+/", $query );
 #print_r($tokens);
 	 
 	/* we is an array that contains :
@@ -75,7 +76,7 @@ function search($context)
 	$we = array();
 	while(list(, $token) = each($tokens))
 	{
-		if(trim($token)=="") //if token is empty or just whitespace --> not search it !
+		if($token == "") //if token is empty or just whitespace --> not search it !
 			continue;
 		
 		if($token[0]=='-')
@@ -116,7 +117,7 @@ function search($context)
 		}
 			
 		
-		$token = removeaccents($token);
+		$token = makeSortKey($token);
 		//foreach word search entities that match this word
 		$dao = &getDAO("search_engine");
 		
@@ -128,99 +129,88 @@ function search($context)
 		
 		$criteria_index = "word LIKE '$begin_wildcard$token$end_wildcard'";
 		#echo "criteria_index=$criteria_index bim=$end_wildcard";
-		if($context["q_field"] && $context["q_field"]!="")
+		$from = "#_TP_search_engine";
+		if($context['q_field'] && $context['q_field']!="")
 		{
 			//added by Jean Lamy - get all tablefields for q_field specified
 			$dao_dc_fields = &getDAO("tablefields");
-			$vos_dc_fields = $dao_dc_fields->findMany("g_name='".$context["q_field"]."'");
+			$vos_dc_fields = $dao_dc_fields->findMany("g_name='".addslashes($context['q_field'])."'");
 			$field_in = array();
 			foreach($vos_dc_fields as $vo_field)
-				$field_in[] = "'".$vo_field->name."'";
+				$field_in[] = $vo_field->name;
 			//$criteria_index .= "AND tablefield='".$context["q_field"]."'";
-			if(count($field_in) > 0)
-			$criteria_index .= "AND tablefield IN (".implode(",",$field_in).")";
-	#echo $criteria_index;
+			if($field_in)
+			//$criteria_index .= "AND tablefield IN (".implode(",",$field_in).")";
+				$criteria_index .=" AND tablefield ".sql_in_array($field_in);
+			
+			
 		}
-		$vos = $dao->findMany($criteria_index);
-		
+		if( $context['q_type']!=""  || $context['q_status']!="")
+		{
+			$join = "LEFT JOIN #_TP_entities ON #_TP_search_engine.identity = #_TP_entities.id";
+		}
+			 	
+		if( $context['q_type']!="" )
+		{
+			$criteria_index .=" AND #_TP_entities.idtype ='".intval($context['q_type'])."'";	
+		}
+		if( $context['q_status']!="")
+		{
+			$criteria_index .= " AND #_TP_entities.status ='".intval($context['q_status'])."'";	
+		}
+		$sql = lq("SELECT * FROM ".$from." ".$join." WHERE ".$criteria_index);
+	#echo "hey :".$sql;
+		$result=$db->execute($sql);
 		$we_temp = array();
-		foreach($vos as $vo)
+		while(!$result->EOF)
 		{
-			$dao_entity = &getDAO("entities");
-		#echo $vo->identity." ";
-			$flag_add = true;
-			//if a particular type or status has been selected, must exclude entities that don't match this choice
-			if( $context["q_type"]!="" )
-			{
-				$vo_entity = $dao_entity->getById($vo->identity);
-				if($vo_entity && $vo_entity->idtype != intval($context["q_type"]) )
-					$flag_add = false;
-			}
-			if( $context["q_status"]!="" )
-			{
-				$vo_entity = $dao_entity->getById($vo->identity);
-				if($vo_entity && $vo_entity->status != intval($context["q_status"]) )
-					$flag_add = false;	
-			}
-			
-			if($flag_add)
-				$we_temp[$vo->identity] += $vo->weight;
-			
-			
-			
-		}//end foreach
+			$row = $result->fields;
+			$we_temp[$row['identity']] += $row['weight'];
+			$result->MoveNext();
+		}
 		
-		if( $cond == "")
+		switch($cond) // differents cases : word inclusion, exclusion and no condition
 		{
-			//$we = array_merge($we,$we_temp);
-			foreach($we_temp as $id=> $weight)
-			{
-				if($we[$id])
-					$we[$id] += $weight;
-				else
-					$we[$id] = $weight;
-			}
-		}
-		elseif( $cond=="exclude" )
-		{
-			/*echo "$cond";
-			print_r($we);
-			print_r($we_temp);*/
-			foreach($we_temp as $id=> $weight)
-			{
-				if($we[$id])
-					unset($we[$id]);
-				
-			}
-			//$we = array_diff($we,$we_temp);
-		}
-		elseif ($cond == "include" )
-		{
-			/*echo "$cond";
-			print_r($we);
-			print_r($we_temp);*/
-			if( count($we) > 0 )
-			{	foreach($we as $id=>$weight)
-				{
-					if($we_temp[$id])
-						$we[$id] += $we_temp[$id];
-					else
-						unset($we[$id]);
-				}
-			}
-			else
-			{
+			case "":
 				foreach($we_temp as $id=> $weight)
 				{
-					$we[$id] = $weight;
+					if($we[$id])
+						$we[$id] += $weight;
+					else
+						$we[$id] = $weight;
 				}
-			}
-		}
+				break;
+				
+			case "exclude":
+				foreach($we_temp as $id=> $weight)
+				{
+					if($we[$id])
+						unset($we[$id]);
+					
+				}
+				break;
+			
+			case "include" :
+				if( count($we) > 0 )
+				{	foreach($we as $id=>$weight)
+					{
+						if($we_temp[$id])
+							$we[$id] += $we_temp[$id];
+						else
+							unset($we[$id]);
+					}
+				}
+				else
+				{
+					foreach($we_temp as $id=> $weight)
+					{
+						$we[$id] = $weight;
+					}
+				}
+				break;	
+		}//end switch
+		
 	}
-	
-	
-	
-	
 	
 	/*
 	echo "<br />----------------------------<br />";
@@ -232,16 +222,8 @@ function search($context)
 	$we = array_reverse($we,true);
 	return $we;
 		
-	
-	
-		
-		
 } 
-function removeaccents($string) 
-{
- 	$string = utf8_decode($string);
- 	return utf8_encode(strtr($string, "ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ", "SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy"));
-}
+
 /*
  * NOTA BENE :
  * 	Avec le tableau res, on peut par une boucle LODELSCRIPT afficher chaque resultat.
@@ -259,12 +241,12 @@ function loop_search(&$context,$funcname,$arguments)
 	if(!$results || count($results)==0)
 	{
 		//echo "coucou";
-		$local_context["nbresults"] = 0;
+		$local_context['nbresults'] = 0;
 		call_user_func("code_alter_$funcname",$local_context);
 		return;
 	}
 	
-	$local_context["nbresults"] = count($results);
+	$local_context['nbresults'] = count($results);
 	if(function_exists("code_before_$funcname"))
 		call_user_func("code_before_$funcname",$local_context);
 	
@@ -274,26 +256,26 @@ function loop_search(&$context,$funcname,$arguments)
 		$dao2 = &getDAO("entities");
 		$vo = $dao2->getById($key);
 		//print_r($vo);
-		$local_context["id"] = $key;
-		$local_context["weight"] = $weight;
-		$local_context["idparent"] = $vo->idparent;
-		$local_context["idtype"] = $vo->idtype;
+		$local_context['id'] = $key;
+		$local_context['weight'] = $weight;
+		$local_context['idparent'] = $vo->idparent;
+		$local_context['idtype'] = $vo->idtype;
 		$dao_type = &getDAO("types");
 		$vo_type = $dao_type->getByID($vo->idtype);
-		$local_context["type"] = $vo_type->type;
+		$local_context['type'] = $vo_type->type;
 		//added information on tpledition
-		$local_context["tpledition"] = $vo_type->tpledition;
-		$local_context["g_title"] = $vo->g_title;
-		$local_context["status"] = $vo->status;
-		$local_context["identifier"] = $vo->identifier;
-		$local_context["creationdate"] = $vo->creationdate ;
-		$local_context["iduser"] = $vo->iduser ;
-		$local_context["usergroup"] = $vo->usergroup ;
-		$local_context["modificationdate"] = $vo->modificationdate ;
-		$local_context["creationmethod"] = $vo->creationmethod ;
-		$local_context["rank"] = $vo->rank ;
-		$local_context["upd"] = $vo->upd ;
-		$local_context["count"] = $count;
+		$local_context['tpledition'] = $vo_type->tpledition;
+		$local_context['g_title'] = $vo->g_title;
+		$local_context['status'] = $vo->status;
+		$local_context['identifier'] = $vo->identifier;
+		$local_context['creationdate'] = $vo->creationdate ;
+		$local_context['iduser'] = $vo->iduser ;
+		$local_context['usergroup'] = $vo->usergroup ;
+		$local_context['modificationdate'] = $vo->modificationdate ;
+		$local_context['creationmethod'] = $vo->creationmethod ;
+		$local_context['rank'] = $vo->rank ;
+		$local_context['upd'] = $vo->upd ;
+		$local_context['count'] = $count;
 		//To complete
 		call_user_func("code_do_$funcname",$local_context);
 		$count++;
@@ -312,17 +294,11 @@ function loop_search(&$context,$funcname,$arguments)
 
 
 require_once("view.php");
+require_once("func.php");
 $view=&getView();
 $base="search";
-if($_GET["query"])	
-	$context["query"] = $_GET["query"];
-if($_GET["q_type"])
-	$context["q_type"] = $_GET["q_type"];
-if($_GET["q_status"])
-	$context["q_status"] = $_GET["q_status"];
-if($_GET["q_field"])
-	$context["q_field"] = $_GET["q_field"];
-	
+extract_post($_GET);
+
 	
 recordurl();	
 $view->renderCached($context,$base);
