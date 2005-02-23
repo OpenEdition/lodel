c025f491511a54432fc16d6cb6cb1ddc30bc5b41
lamy <lamy>
2005-02-23 14:59:46 +0000

    Type:Bm Descr:corrected utf8 and iso confusion
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2284 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic/class.entities_index.php
+++ lodel/scripts/logic/class.entities_index.php
@@ -186,10 +186,10 @@ class Entities_IndexLogic extends Logic
    */
   function _decode_html_entities($text) 
   {
-    $text= html_entity_decode($text,ENT_QUOTES,"ISO-8859-1"); #NOTE: UTF-8 does not work!
+    //$text= html_entity_decode($text,ENT_QUOTES,"ISO-8859-1"); #NOTE: UTF-8 does not work!
     //$text= html_entity_decode($text,ENT_QUOTES,"UTF-8"); 
-	$text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); #decimal notation
-	$text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  #hex notation
+	$text= preg_replace('/&#(\d+);/me',utf8_encode("chr(\\1)"),$text); #decimal notation
+	$text= preg_replace('/&#x([a-f0-9]+);/mei',utf8_encode("chr(0x\\1)"),$text);  #hex notation
     return $text;
   }
   function _removeaccents($string){
