76183a20eb1fd95f61bef8c6d19eb10ea1949fbd
picard <picard>
2005-03-03 12:15:59 +0000

    Type:B Descr:oups
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2342 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic.php
+++ lodel/scripts/logic.php
@@ -140,17 +140,20 @@ class Logic {
     * Default implementation
     * $link the fields to make the define the group within which the rank is done.    
     */
-   function changeRankAction(&$context,&$error,$groupfields="",$status="status>0 ")
+   function changeRankAction(&$context,&$error,$groupfields="",$status="status>0")
 
    {
+     $criterias=array();
+
      $id=$context['id'];
      if ($groupfields) {
        $dao=$this->_getMainTableDAO();
        $vo=$dao->getById($id,$groupfields);
-       foreach(explode(",",$groupfields) as $field)  $criteria.=" AND ".$field."='".$vo->$field."'";
+       foreach(explode(",",$groupfields) as $field)  $criterias[]=$field."='".$vo->$field."'";
      }
-     if ($status) $criteria=$status.$criteria;
+     if ($status) $criterias[]=$status;
 
+     $criteria=join(" AND ",$criterias);
      $this->_changeRank($id,$context['dir'],$criteria);
 
      update();
