05333b0ff76afc9798f040c2d54ff136e4c9fb11
lamy <lamy>
2005-04-04 13:08:50 +0000

    Type:F Descr:added indexation of entries and persons
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2451 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic/class.entities_index.php
+++ lodel/scripts/logic/class.entities_index.php
@@ -30,41 +30,37 @@
  *     along with this program; if not, write to the Free Software
  *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
  
- /**
- *  Logic Entities_Index
- *  
+/**
+ * Entities_Index logic.
+ * Define actions for the search engine
  * 
- */
- 
- 
+ * 
+ */ 
 class Entities_IndexLogic extends Logic 
 {
   /**
-    * generic equivalent assoc array
-    */
+   * generic equivalent assoc array
+   */
   var $g_name;
   /** 
-	* Constructor
-   	*/
+	 * Constructor
+   */
   function Entities_IndexLogic() 
   {
     $this->Logic("search_engine");
   }
    	
-  /**
-    *  add an object to the index
-    * needed parameters :
-    * 		- object id
-    */
+	/**
+	 * Add an object to the search_engine. An object is added only its type must be indexed 
+	 * and if its fields have weight defined > 0
+	 * 
+	 */ 
   function addIndexAction(&$context,&$error)
   {
-    
     global $db;
     //no object identity specified
     $id = $context['id'];
-   	if (!$id)
-   		die("ERROR: give the id ");
-   	 	
+   	if (!$id) die("ERROR: no id given");
    	//if this entity is already indexed ==> clean
    	$this->deleteIndexAction($context,$error);
    	 	
@@ -74,86 +70,36 @@ class Entities_IndexLogic extends Logic
    	//where
    	$sql .= " WHERE e.id='$id'";
    	$row = $db->getRow(lq($sql)) ;
-   	   	
-   	if (!$row['id'])
- 		die("ERROR: can't find object $id ".$dao_temp->table);
+   	if (!$row['id']) die("ERROR: can't find object $id ".$dao_temp->table);
  	
- 	$class= $row['class'];
- 	if (!$class) 
- 		die("ERROR: idtype is not valid in Entities_IndexLogic::addIndexAction");
- 	//if the field search is not equal to 1, dont index the entity
- 	if($row['search'] != 1)
- 			return "_back";
+ 		$class= $row['class'];
+ 		if (!$class)die("ERROR: idtype is not valid in Entities_IndexLogic::addIndexAction");
+ 		//if the field search is not equal to 1, dont index the entity
+ 		if($row['search'] != 1)return "_back";
  	
  	#echo "id=$id;class=$class";
  	
- 	//get the fieldnames list to index
- 	$dao_fields = &getDAO("tablefields");
- 	$vos_fields = array();
- 	$vos_fields = $dao_fields->findMany("class='$class' AND weight > 0","weight DESC","id,weight,name");
- 	#print_r($vos_fields);
+	 	//get the fieldnames list to index
+	 	$dao_fields = &getDAO("tablefields");
+	 	$vos_fields = array();
+	 	$vos_fields = $dao_fields->findMany("class='$class' AND weight > 0","weight DESC","id,weight,name");
+ 	#	print_r($vos_fields);
  	
- 	//no fields to index --> return
- 	if(!$vos_fields) 
- 		return ("_back");
+	 	//no fields to index --> return
+	 	if(!$vos_fields)	return ("_back");
  	
- 	//foreach field to index, index the words
- 	//first get the vo
- 	//$dao = &getDAO($class);
- 	//$vo = $dat->getById($id);
- 	
- 	$sql = "SELECT * FROM #_TP_$class WHERE identity='$id'";
- 	$row = $db->getRow(lq($sql)) ;
- 	#echo lq($sql);
- 	if(!$row)
- 		die("ERROR: can't find object $id in table ".lq("#_TP_$class"));
- 	 	
- 	foreach( $vos_fields as $vo_field)
- 	{
- 		$string = $row[$vo_field->name]; //get the string of the current field
- 		//$string = " ".preg_replace("/<[^>]*>/"," ",$string)." ";
- 		$string = preg_replace("/<[^>]*>/","",$string);//HTML tags cleaning
- 		$string = $this->_decode_html_entities($string); //HTML Entities decode
- 		//include utf8 quotes at the end
- 		$regs = "'\.],:;*\"“!\r\t\\/)({}[|@<>$%Â«Â»\342\200\230\342\200\231\342\200\234\342\200\235";
- 	#echo "regs=$regs";
- 		$string = strtr( $string , $regs , preg_replace("/./", " " , $regs ) );//non alphanum chars cleaning
- 	#echo "string=$string<br />\n";
- 		$tokens = preg_split("/[\s]+/", $string );//Separate string in tokens
- 		$indexs = array();//Array of each word weight for this field
- 		while(list(, $token) = each($tokens))
- 		{
- 		  //particular case : two letter acronym or initials
- 		  if(preg_match("/([A-Z][0-9A-Z]{1,2})/",$token) || strlen($token) > 3)
- 		  {
-	 	     //little hack because oe ligature is not supported in ISO-latin!!
-	 	    $token = strtolower(str_replace(array("\305\223","\305\222"),array("oe","OE"),$token));
- 		  	$token = makeSortKey($token);
-	 	    /*require_once("class.stemmer.inc.php");
-	 	    $stemmer = new Stemmer();
-	 	    $token = $stemmer->stem($token);*/
-	 	    $indexs[$token] ++; //simply count word number
-	 	  }
- 		}
- 		$vos_index = array();
- 		
- 		//index ponderation and virtual object creation
- 		//and update database with dao and vo (or manually)
- 		$dao_index = &getDAO("search_engine");
- 		foreach($indexs as  $key => $index)
- 		{
- 		  $dao_index->instantiateObject($vo_index);
- 		  $vo_index->identity = $id;
- 		  $vo_index->tablefield = $vo_field->name;
- 		  $vo_index->word = addslashes($key);
- 		  $vo_index->weight = $indexs[$key] * $vo_field->weight; //ponderation with field weight
- 		  $dao_index->save($vo_index,true);
- 		}
- 		
- 	}//end of foreach fields
- 
- 	return "_back";
- 	
+ 		$sql = "SELECT * FROM #_TP_$class WHERE identity='$id'";
+	 	$row = $db->getRow(lq($sql)) ;
+	 #echo lq($sql);
+	 	if(!$row)	die("ERROR: can't find object $id in table ".lq("#_TP_$class"));
+ 		$daoIndex = &getDAO("search_engine"); 	
+	 	foreach( $vos_fields as $vo_field)
+	 		$this->_indexField($id,$row[$vo_field->name],$vo_field->name,$vo_field->weight,$daoIndex);
+		
+		$this->_indexEntitiesRelations($id,'E',$daoIndex);
+		//fonctionne pas pour les personnes apparemment
+		$this->_indexEntitiesRelations($id,'G',$daoIndex);
+		return "_back";
   }
  
  /**
@@ -184,16 +130,12 @@ class Entities_IndexLogic extends Logic
   }
   
   /**
-   * rebuild the index
+   * Rebuild entirely the Index
    * 
    */
   function rebuildIndexAction(&$context,&$error)
   {
   	global $db;
-  	//TODO
-  	//recuperer les id des entites indexable (par rapport aux types) et non indexées
-  	// prendre un groupe de 10 et les indexées
-  	// ou bien récuperer le timout et tant que on a pas atteint 80% du timeout continuer
   	$timeout = ini_get("max_execution_time");
   	$prudent_timeout = $timeout*0.8;
   	$start = time();
@@ -205,38 +147,173 @@ class Entities_IndexLogic extends Logic
   	while (!$result->EOF) 
   	{
 			//print_r($result->fields);
-			//index
 			$context["id"] = $result->fields['id'];
 			$this->addIndexAction($context,$error);
 			$current = time();
-			//echo "currenttime=".($current - $start)."<br />";
 			if(($current - $start) < $prudent_timeout)
 				$result->MoveNext();
 			else
 			{
 				//80% du timeout est dépassé, il faut rediriger.
-				//header
 				header("Location: index.php?do=rebuildIndex&lo=entities_index");
 			}	  		
   	}
    	return "_back";
-  	
-  	 
   }
   
+  /**
+	 * index a given field in the database (using dao_index)
+	 * @param $id : entity database identifier
+	 * @param $fieldValue : the value of the field
+	 * @param $fieldName : the name of the field
+	 * @param $fieldWeight : the weight used to ponderate the field
+	 * @param $daoIndex : the dao to use to save the data
+	 * @param $prefixtablefield : empty by default but used to prefix the field 'tablefield' (for entries or persons for example)
+	 * 
+	 */
+	function _indexField($id,$fieldValue,$fieldName,$fieldWeight,$daoIndex,$prefixtablefield="")
+	{
+		if(!$fieldValue)
+			return;
+		
+		$fieldValue = preg_replace("/<[^>]*>/"," ",$fieldValue);//HTML tags cleaning
+	 	$fieldValue = $this->_decode_html_entities($fieldValue); //HTML Entities decode
+		$indexs = $this->_cleanAndcountTokens($fieldValue); //clean and count tokens
+		
+		foreach($indexs as  $key => $index)
+	 	{
+			$daoIndex->instantiateObject($voIndex);
+		 	$voIndex->identity = $id;
+		 	$voIndex->tablefield = $prefixtablefield.$fieldName;
+		 	$voIndex->word = addslashes($key);
+		 	$voIndex->weight = $indexs[$key] * $fieldWeight; //ponderation with field weight
+		 	#print_r($voIndex);
+		 	$daoIndex->save($voIndex,true);
+		}
+	}
+    
   /** Private function
-   *  Description : decode HTML entities, 
+   *  Description : decode HTML entities,
+   * @param $text the text where HTML entities must be decoded
+   * @return $text the text with HTML entities decoded
+   *  
    */
   function _decode_html_entities($text) 
   {
     //$text= html_entity_decode($text,ENT_QUOTES,"ISO-8859-1"); #NOTE: UTF-8 does not work!
     //$text= html_entity_decode($text,ENT_QUOTES,"UTF-8"); 
-	$text= preg_replace('/&#(\d+);/me',utf8_encode("chr(\\1)"),$text); #decimal notation
-	$text= preg_replace('/&#x([a-f0-9]+);/mei',utf8_encode("chr(0x\\1)"),$text);  #hex notation
+		$text= preg_replace('/&#(\d+);/me',utf8_encode("chr(\\1)"),$text); #decimal notation
+		$text= preg_replace('/&#x([a-f0-9]+);/mei',utf8_encode("chr(0x\\1)"),$text);  #hex notation
     return $text;
   }
- 
+  
+  /**
+   *  Split a string into tokens by given regs
+   * @param $string the string to be splitted
+   * @param $regs the regs used to split the string
+   * @return an array of tokens
+   */
+	function _splitInTokens($string,$regs=0)
+  {
+  	if(!$regs)
+ 			$regs = "'\.],:;*\"“!\r\t\\/)({}[|@<>$%Â«Â»\342\200\230\342\200\231\342\200\234\342\200\235";
+ 		$string = strtr( $string , $regs , preg_replace("/./", " " , $regs ) );//non alphanum chars cleaning
+ 		#echo "string=$string<br />\n";
+ 		$tokens = preg_split("/[\s]+/", $string );//Separate string in tokens
+ 		return $tokens;	
+  }
 
+  /**
+   * Function to split a string into tokens
+   * @param $string the string to be clean and word count
+   * @param $regs the regs used to clean the string
+   * @return an array with for each word its count
+   * 
+   */
+ 	function _cleanAndcountTokens($string,$regs=0)
+ 	{
+ 		$tokens = $this->_splitInTokens($string,$regs);
+ 		$indexs = array();//Array of each word weight for this field
+ 		while(list(, $token) = each($tokens))
+ 		{
+ 		  //particular case : two letter acronym or initials
+ 		  if(preg_match("/([A-Z][0-9A-Z]{1,2})/",$token) || strlen($token) > 3)
+ 		  {
+	 	     //little hack because oe ligature is not supported in ISO-latin!!
+	 	    $token = strtolower(str_replace(array("\305\223","\305\222"),array("oe","OE"),$token));
+ 		  	$token = makeSortKey($token); // clean accents
+	 	    $indexs[$token] ++; //simply count word number
+	 	    
+	 	    /*require_once("class.stemmer.inc.php");
+	 	    $stemmer = new Stemmer();
+	 	    $token = $stemmer->stem($token);*/
+	 	   }
+ 		}
+ 		return $indexs;
+ 	}
+ 	
+ 	/**
+ 	 * Generic function to index relations of type entries and persons : E and G relations
+ 	 * @param $id the id of the entity
+ 	 * @param $nature the nature of the relation
+ 	 * @param $daoIndex the DAO object
+ 	 */
+	function _indexEntitiesRelations($id,$nature,$daoIndex)
+	{
+		global $db;
+		if(!$id)return false;
+		if($nature!='E' && $nature!='P' && $nature!='G')return false;
+		if($nature=="G"){	$table1 = "persons";$table2 = "person";}
+		if($nature=="P"){	return ;} // don't know how to do it
+		if($nature=="E"){	$table1 = "entries";$table2 = "entry";}
+		
+		//build query to select the good fields to index for the entry or the person
+		if($nature=='G' || $nature=='E')
+		{
+			$sql = "SELECT DISTINCT tf.name, tf.weight, e.id, t.class
+					FROM #_TP_relations as r,#_TP_$table1 as e, #_TP_".$table2."types as t
+					INNER JOIN #_TP_tablefields as tf ON tf.class=t.class
+					WHERE r.id2= e.id AND r.id1='$id' AND r.nature='$nature' 
+					AND t.id=e.idtype
+					AND tf.weight > 0
+					ORDER BY e.id";
+		}
+	
+		
+		#echo "sql=".lq($sql)."<br />";
+		$result = $db->execute(lq($sql));
+		while($result && !$result->EOF)
+		{
+			$row = $result->fields;
+			$sql2 = "SELECT ".$row['name']." FROM #_TP_".$row['class']." WHERE id".$table2."=".$row['id'];
+			#echo "<pre>sql2=".lq($sql2)."<br /></pre>";
+			$field = $db->getOne(lq($sql2));
+			$this->_indexField($id,$field,$row['name'],$row['weight'],$daoIndex,$row['class'].".");
+			$result->moveNext();
+		}
+		
+		//can't do the following above because I must selection idrelation and not id from the table
+		if($nature=='G') //special for nature G, get field from entities_$class table to index
+		{
+			$table2='relation';
+			$sql = "SELECT DISTINCT tf.name, tf.weight, tf.class, r.idrelation AS id 
+							FROM #_TP_relations AS r, #_TP_tablefields AS tf, #_TP_persontypes as t, #_TP_persons as p
+							WHERE r.nature='G' AND r.id1='$id' AND tf.weight > 0 AND t.id=p.idtype AND tf.class=CONCAT('entities_',t.class)" .
+							" ORDER BY r.id";
+			#echo "<pre>sql=".lq($sql)."</pre><br />";
+			$result = $db->execute(lq($sql));
+			while($result && !$result->EOF)
+			{
+				$row = $result->fields;
+				$sql2 = "SELECT ".$row['name']." FROM #_TP_".$row['class']." WHERE id".$table2."=".$row['id'];
+				#echo "<pre>sql2=".lq($sql2)."<br /></pre>";
+				$field = $db->getOne(lq($sql2));
+				$this->_indexField($id,$field,$row['name'],$row['weight'],$daoIndex,$row['class'].".");
+				$result->MoveNext();
+			}
+		}//end if nature==G
+	}
+	
 }//end of class
 
 ?>
