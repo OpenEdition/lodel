1cbb0462fefb4546ec991d455aee9d24ca636d43
lamy <lamy>
2005-09-06 12:18:23 +0000

    Type:A Descr:changed the way that entries are managed
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2720 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic/class.entries.php
+++ lodel/scripts/logic/class.entries.php
@@ -37,253 +37,220 @@ require_once("genericlogic.php");
 
 class EntriesLogic extends GenericLogic {
 
-  /** Constructor
-   */
-   function EntriesLogic() {
-     $this->GenericLogic("entries");
-   }
-
-
-  function viewAction(&$context,&$error)
-
-  {
-    if (!$context['id']) $context['status']=32;
-    $context['classtype']="entries";
-    return GenericLogic::viewAction($context,$error);
-  }
-
-   /**
-    * list action
-    */
-
-  function listAction(&$context,&$error,$clean=false)
-
-  {
-    $daotype=&getDAO("entrytypes");
-    $votype=$daotype->getById($context['idtype']);
-    if (!$votype) die("ERROR: idtype must me known in GenericLogic::viewAction");
-    $this->_populateContext($votype,$context['type']);
-    return "_ok";
-  }
-
-   /**
-    * add/edit Action
-    */
-
-   function editAction(&$context,&$error,$clean=false)
-
-   {
-     global $home;
-
-
-     $id=$context['id'];
-     $idtype=$context['idtype'];
-     if (!$idtype) die("ERROR: internal error in EntriesLogic::editAction");
-     $status=$context['status'];
-
-     // get the class 
-     $daotype=&getDAO("entrytypes");
-     $votype=$daotype->getById($idtype,"class,newbyimportallowed,flat");
-     $class=$context['class']=$votype->class;
-
-     if ($clean!=CLEAN) {
-       if (!$this->validateFields($context,$error)) {
-	 // error.
-	 // if the entity is imported and will be checked
-	 // that's fine, let's continue, if not return an error
-	 if ($status>-64) return "_error";
-       }
-     }
-     $g_index_key=$this->getGenericEquivalent($class,'index key');
-     if (!$g_index_key) die("ERROR: The generic field 'index key' is required. Please edit your editorial model.");
-
-     // get the dao for working with the object
-     $dao=$this->_getMainTableDAO();
-
-     if (isset($context['g_name'])) {
-       if (!$context['g_name']) return "_error"; // empty entry!
-       // search if the entries exists
-       $vo=$dao->find("g_name='".$context['g_name']."' AND idtype='".$idtype."' AND status>-64","id");
-       if ($vo->id) {
-	 $context['id']=$vo->id;
-	 return; // nothing to do.
-       } else {
-	 $context['data'][$g_index_key]=$context['g_name'];
-       }
-     }
-
-     $index_key=&$context['data'][$g_index_key];
-     $index_key=str_replace(","," ",$index_key); // remove the , because it is a separator
-
-     if ($context['lo']=="entries") {  // check it does not exist
-       $vo=$dao->find("g_name='".$index_key."' AND idtype='".$idtype."' AND status>-64 AND id!='".$id."'","id");
-       if ($vo->id) {
-	 $error[$g_index_key]="1";
-	 return "_error";
-       }
-     }
-
-     if (!$vo) {
-       if ($id) { // create or edit the entity
-	 $new=false;
-	 $dao->instantiateObject($vo);
-	 $vo->id=$id;
-       } else {
-	 if (!$votype->newbyimportallowed && $context['lo']!="entries") { return "_error"; }
-	 $new=true;
-	 $vo=$dao->createObject();
-	 $vo->status=$status ? $status : -1;
-       }
-     }
-     if ($dao->rights['protect']) $vo->protect=$context['protected'] ? 1 : 0;
-     if ($votype->flat) {
-       $vo->idparent=0; // force the entry to be at root
-     } else {
-       $vo->idparent=intval($context['idparent']);
-     }
-     // populate the entry table
-     if ($idtype) $vo->idtype=$idtype;
-
-     $vo->g_name=$index_key;
-     $vo->sortkey=makeSortKey($vo->g_name);
-     #print_R($vo);
-     $id=$context['id']=$dao->save($vo);
-     #echo $id;
-     #print_R($dao);
-     // save the class table
-     $gdao=&getGenericDAO($class,"identry");
-     $gdao->instantiateObject($gvo);
-     $context['data']['id']=$context['id'];
-     $this->_populateObject($gvo,$context['data']);
-     $gvo->identry=$id;
-
-     $this->_moveFiles($id,$this->files_to_move,$gvo);
-     $gdao->save($gvo,$new);  // save the related table
-     
-     update();
-     //unlock();
-
-     return "_back";
-   }
-
-
-   /**
-    * Change rank action
-    * Default implementation
-    */
-   function changeRankAction(&$context,&$error)
-
-   {
-     return Logic::changeRankAction(&$context,&$error,"idparent","");
-   }
-
-
-  function makeSelect(&$context,$var)
-
-  {
-    global $db;
-
-    switch($var) {
-    case 'idparent':
-      $arr=array();
-      $rank=array();
-      $parent=array();
-      $ids=array(0);
-      $l=1;
-      do {
-	$result=$db->execute(lq("SELECT * FROM #_TP_entries WHERE idtype='".$context['idtype']."' AND idparent ".sql_in_array($ids)." ORDER BY ".$context['type']['sort'])) or dberror();
-	$ids=array();
-	$i=1;
-	while(!$result->EOF) {
-	  $id=$result->fields['id'];
-	  if ($id!=$context['id']) {
-	    $ids[]=$id;	 
-	    $fullname=$result->fields['g_name'];
-	    $idparent=$result->fields['idparent'];
-	    if ($idparent) $fullname=$parent[$idparent]." / ".$fullname;	   
-	    $d=$rank[$id]=$rank[$idparent]+($i*1.0)/$l;
-	    //echo $d," ";
-	    $arr["p$d"]=array($id,$fullname);
-	    $parent[$id]=$fullname;
-	    $i++;
-	  }
-	  $result->MoveNext();
+	/** Constructor */
+	function EntriesLogic () {
+		$this->GenericLogic ("entries");
 	}
-	$l*=100;
-      } while ($ids);
-      ksort($arr);
-      $arr2=array("0"=>"--"); // reorganize the array $arr
-      foreach($arr as $row) {
-	$arr2[$row[0]]=$row[1];
-      }
-      renderOptions($arr2,$context[$var]);
-      break;
-    }
-  }
 
 
+	function viewAction (&$context, &$error) {
+		if (!$context['id']) $context['status']=32; //why ?
+		$context['classtype']="entries";
+		return GenericLogic::viewAction ($context, $error); //call the parent method
+	}
 
-   /*---------------------------------------------------------------*/
-   //! Private or protected from this point
-   /**
-    * @private
-    */
+	function isdeletelocked ($id, $status=0) {
+		global $db;
+		//if this entry is used by an entity don't delete it.
+		//$count = $db->getOne(lq("SELECT count(*) FROM #_TP_entries,#_TP_relations WHERE #_TP_relations.id2=#_TP_entries.id AND #_TP_relations.id2='$id' AND status>-64"));
+		// or if this entry has child
+		$count += $db->getOne(lq("SELECT count(*) FROM #_TP_entries WHERE idparent='$id' AND status >-64"));
+		if ($db->errorno())  dberror();
+		if ($count==0) {
+			return false;
+		} else {
+			return sprintf(getlodeltextcontents("cannot_delete_hasentrieschild","admin"),$count);
+		}
+	}
+
+	/**
+	 * List action
+	 */
+	function listAction (&$context, &$error, $clean=false) {
+		$daotype=&getDAO ("entrytypes");
+		$votype=$daotype->getById ($context['idtype']);
+		if (!$votype) die ("ERROR: idtype must me known in GenericLogic::viewAction");
+		$this->_populateContext ($votype, $context['type']);
+		return "_ok";
+	}
+
+	/**
+	 * add/edit Action
+	 */
+	function editAction (&$context, &$error, $clean=false) {
+		global $home;
+		$id=$context['id'];
+		$idtype=$context['idtype'];
+		if (!$idtype) die ("ERROR: internal error in EntriesLogic::editAction");
+		$status=$context['status'];
+		// get the class 
+		$daotype=&getDAO ("entrytypes");
+		$votype=$daotype->getById ($idtype, "class,newbyimportallowed,flat");
+		$class=$context['class']=$votype->class;
+		if ($clean!=CLEAN) {
+			if (!$this->validateFields($context,$error)) {
+				// error.
+				// if the entity is imported and will be checked
+				// that's fine, let's continue, if not return an error
+				if ($status>-64) return "_error";
+			}
+		}
+		$g_index_key=$this->getGenericEquivalent($class,'index key');
+		if (!$g_index_key) die ("ERROR: The generic field 'index key' is required. Please edit your editorial model.");
+		// get the dao for working with the object
+		$dao=$this->_getMainTableDAO ();
+		if (isset ($context['g_name'])) {
+			if (!$context['g_name']) return "_error"; // empty entry!
+			// search if the entries exists
+			$vo=$dao->find ("g_name='". $context['g_name']. "' AND idtype='". $idtype."' AND status>-64","id");
+			if ($vo->id) {
+				$context['id']=$vo->id;
+				return; // nothing to do.
+			} else {
+				$context['data'][$g_index_key]=$context['g_name'];
+			}
+		}
+
+		$index_key=&$context['data'][$g_index_key];
+		$index_key=str_replace(","," ",$index_key); // remove the , because it is a separator
+		if ($context['lo']=="entries") {  // check it does not exist
+			$vo=$dao->find("g_name='".$index_key."' AND idtype='".$idtype."' AND status>-64 AND id!='".$id."'","id");
+			if ($vo->id) {
+				$error[$g_index_key]="1";
+				return "_error";
+			}
+		}
+
+		if (!$vo) {
+			if ($id) { // create or edit the entity
+				$new=false;
+				$dao->instantiateObject($vo);
+				$vo->id=$id;
+			} else {
+				if (!$votype->newbyimportallowed && $context['lo']!="entries") { return "_error"; }
+				$new=true;
+				$vo=$dao->createObject();
+				$vo->status=$status ? $status : -1;
+			}
+		}
+		if ($dao->rights['protect']) $vo->protect=$context['protected'] ? 1 : 0;
+		if ($votype->flat) {
+			$vo->idparent=0; // force the entry to be at root
+		} else {
+			$vo->idparent=intval($context['idparent']);
+		}
+		// populate the entry table
+		if ($idtype) $vo->idtype=$idtype;
+		$vo->g_name=$index_key;
+		$vo->sortkey=makeSortKey($vo->g_name);
+		$id=$context['id']=$dao->save($vo);
+		// save the class table
+		$gdao=&getGenericDAO($class,"identry");
+		$gdao->instantiateObject($gvo);
+		$context['data']['id']=$context['id'];
+		$this->_populateObject($gvo,$context['data']);
+		$gvo->identry=$id;
+
+		$this->_moveFiles($id,$this->files_to_move,$gvo);
+		$gdao->save($gvo,$new);  // save the related table
+
+		update();
+		return "_back";
+	}
 
 
-   /**
-    * Used in deleteAction to do extra operation before the object is saved.
-    * Usually it gather information used after in _deleteRelatedTables
-    */
-   function _prepareDelete($dao,&$context) 
-
-   {
-     global $db;
-
-    // get the classes
-    $this->classes=array();
-    $result=$db->execute(lq("SELECT DISTINCT class FROM #_TP_entrytypes INNER JOIN #_TP_entries ON idtype=#_TP_entrytypes.id WHERE #_TP_entries.id ".sql_in_array($context['id']))) or dberror();		 
-    while (!$result->EOF) {
-      $this->classes[]=$result->fields['class'];
-      $result->MoveNext();
-    }
-
-    if (isset($context['idrelation'])) {
-      $this->idrelation=$context['idrelation'];
-    } else {
-      $dao=&getDAO("relations");
-      $vos=$dao->findMany("id2 ".sql_in_array($context['id']));
-      $this->idrelation=array();
-      foreach ($vos as $vo) {
-	$this->idrelation[]=$vo->idrelation;
-      }
-    }
-  }
+	/**
+	 * Change rank action
+	 * Default implementation
+	 */
+	function changeRankAction (&$context, &$error) {
+		return Logic::changeRankAction(&$context,&$error,"idparent","");
+	}
 
 
-   /**
-    * Used in deleteAction to do extra operation after the object has been deleted
-    */
-   function _deleteRelatedTables($id) 
+	function makeSelect (&$context, $var) {
+		global $db;
+		switch($var) {
+			case 'idparent':
+				$arr=array ();
+				$rank=array ();
+				$parent=array ();
+				$ids=array (0);
+				$l=1;
+				do {
+					$result=$db->execute (lq ("SELECT * FROM #_TP_entries WHERE idtype='".$context['idtype']."' AND id!='".$context['id']."' AND idparent ".sql_in_array ($ids). " AND ABS(status)>= 32 ORDER BY ". $context['type']['sort'])) or dberror();
+					$ids=array();
+					$i=1;
+					while (!$result->EOF) {
+						$id=$result->fields['id'];
+						$ids[]=$id;	 
+						$fullname=$result->fields['g_name'];
+						$idparent=$result->fields['idparent'];
+						if ($idparent) $fullname=$parent[$idparent]." / ".$fullname;
+						$d=$rank[$id]=$rank[$idparent]+($i*1.0)/$l;
+						$arr["p$d"]=array($id,$fullname);
+						$parent[$id]=$fullname;
+						$i++;
+						$result->MoveNext();
+					} //end while
+					$l*=100;
+				} while ($ids); // end do while
+				ksort ($arr);
+				$arr2=array ("0"=>"--"); // reorganize the array $arr
+				foreach ($arr as $row) {
+					$arr2[$row[0]]=$row[1];
+				}
+				renderOptions ($arr2, $context[$var]);
+				break;
+		}
+	} //end of function
 
-   {
-    global $db;
+/*---------------------------------------------------------------*/
+//! Private or protected from this point
+/**
+ * @private
+ */
 
-    foreach ($this->classes as $class) {
-      $gdao=&getGenericDAO($class,"identry");
-      $gdao->deleteObject($id);
+/**
+ * Used in deleteAction to do extra operation before the object is saved.
+ * Usually it gather information used after in _deleteRelatedTables
+ */
+	function _prepareDelete ($dao, &$context) {
+		global $db;
+		// get the classes
+		$this->classes=array ();
+		$result=$db->execute (lq ("SELECT DISTINCT class FROM #_TP_entrytypes INNER JOIN #_TP_entries ON idtype=#_TP_entrytypes.id WHERE #_TP_entries.id ".sql_in_array ($context['id']))) or dberror ();
+		while (!$result->EOF) {
+			$this->classes[]=$result->fields['class'];
+			$result->MoveNext();
+		}
 
-      //if ($this->idrelation) {
-      //  $gdao=&getGenericDAO("entities_".$class,"idrelation");
-      //  $gdao->deleteObject($this->idrelation);
-      //}
-    }
-    if ($this->idrelation) {
-      $dao=&getDAO("relations");
-      $dao->delete("idrelation ".sql_in_array($this->idrelation));
-    }
-  }
+		if (isset($context['idrelation'])) {
+			$this->idrelation=$context['idrelation'];
+		} else {
+			$dao=&getDAO ("relations");
+			$vos=$dao->findMany ("id2 ".sql_in_array ($context['id']));
+			$this->idrelation=array ();
+			foreach ($vos as $vo) {
+				$this->idrelation[]=$vo->idrelation;
+			}
+		}
+	}
+
+
+	/**
+	 * Used in deleteAction to do extra operation after the object has been deleted
+	 */
+	function _deleteRelatedTables($id) {
+		global $db;
+		foreach ($this->classes as $class) {
+			$gdao=&getGenericDAO ($class, "identry");
+			$gdao->deleteObject ($id);
+		}
+		if ($this->idrelation) {
+			$dao=&getDAO ("relations");
+			$dao->delete ("idrelation ". sql_in_array ($this->idrelation));
+		}
+	}
 } // class 
-
-
-
 ?>
