daec1107279b431e8d24c104caba32146abdb2e8
Sophie Malafosse <malafoss>
2006-01-26 11:07:07 +0000

    Type:F Descr:Classe pour l'authentification HTTP (basic)
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2910 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/scripts/class.authHTTP.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Fichier de la classe AuthHTTP
+ *
+ * PHP version 4
+ *
+ * LODEL - Logiciel d'Edition ELectronique.
+ *
+ * Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ * Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ * Copyright (c) 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ * Copyright (c) 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Bruno Cénou, Jean Lamy
+ *
+ * Home page: http://www.lodel.org
+ *
+ * E-Mail: lodel@lodel.org
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @author Sophie Malafosse
+ * @copyright 2006
+ * @licence http://www.gnu.org/copyleft/gpl.html
+ * @since Fichier ajouté depuis la version 0.8
+ * @version CVS:$Id: 
+ */
+
+
+/**
+ * Classe permettant l'authentification HTTP
+ * 
+ * <p>Cette classe est utilisée pour l'authentification HTTP (basic)</p>
+ *
+ * @package lodel
+ * @author Ghislain Picard
+ * @author Jean Lamy
+ * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Jean Lamy
+ * @licence http://www.gnu.org/copyleft/gpl.html
+ * @version CVS:$Id:
+ * @since Fichier ajouté depuis la version 0.8
+ * @see auth.php
+ * @see loginHTTP.php
+ * @see src/edition/index.php
+ */
+
+class AuthHTTP
+{
+	/**
+	* Login récupéré dans le header
+	* @var string
+	*/
+	var $login;
+
+	/**
+	* Mot de passe récupéré dans le header
+	* @var string
+	*/
+	var $password;
+
+	
+	/**
+	* 
+	* retourne un booléen : true si le login et le mot de passe sont récupérés,  
+	* false sinon
+	* @return bool
+	*/
+	function getHeader()
+	{
+		$this->reset();
+		if (!headers_sent())
+		{
+			if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']))
+			{
+				// variables non initialisées
+				return false;
+			}
+			elseif (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
+			{
+				// récupère le login et le mot de passe
+				$this->login = $_SERVER['PHP_AUTH_USER'];
+				$this->password = $_SERVER['PHP_AUTH_PW'];
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+        * Renvoi du header avec demande d'authentification
+        *
+        * @return 
+        */
+	function errorLogin()
+	{
+		header('WWW-Authenticate: Basic realm="Authentification requise"');
+	        header('HTTP/1.0 401 Unauthorized');
+		echo "L'accès à cette ressource requiert une authentification : veuillez entrer le nom d'utilisateur
+		et le mot de passe que vous utilisez sous Lodel";
+		exit;
+	}
+
+	
+	/**
+	* initialisation des variables
+	*/
+	function reset()
+	{
+		$this->login = '';
+		$this->password = '';
+	}
+
+	/**
+	* Retourne dans un tableau le login et le mot de passe
+	*
+	* @return array
+	*/
+	function getIdentifiers()
+	{
+		return array(
+		"login" => $this->login,
+		"password" => $this->password);
+	}
+	
+}
+
+?>
