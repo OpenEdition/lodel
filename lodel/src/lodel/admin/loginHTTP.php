326dbbd5e662c155e1b9b7ba441e662dfdf9a049
Sophie Malafosse <malafoss>
2006-01-26 11:09:26 +0000

    Type:F Descr:Fichier g��rant l'authentification HTTP d'un utilisateur (pour les flux RSS du tableau de bord).
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2911 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/src/lodel/admin/loginHTTP.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ *
+ *  LODEL - Logiciel d'Edition ELectronique.
+ *
+ *  Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ *  Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ *  Copyright (c) 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ *  Copyright (c) 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Jean Lamy
+ *
+ *  Home page: http://www.lodel.org
+ *
+ *  E-Mail: lodel@lodel.org
+ *
+ *                            All Rights Reserved
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.*/
+
+require("siteconfig.php");
+require_once("auth.php");
+require_once("class.authHTTP.php");
+
+$httpAuth = new AuthHTTP();
+if ($httpAuth->getHeader())
+{
+	// récupère les identifiants (login/password) du header
+	$identifiers = $httpAuth->getIdentifiers();
+
+	require_once("func.php");
+	extract_post($identifiers);
+
+	require_once("connect.php");
+	require_once("loginfunc.php");
+
+	// les identifiants ne correspondent pas à un utilisateur Lodel
+	if (!check_auth($context['login'],$context['password'],$site))
+	{
+		$httpAuth->errorLogin();	
+ 	 }
+
+// OK
+else return;
+}
+
+else $httpAuth->errorLogin();
+?>
