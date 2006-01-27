c1cc99e4f1620777fcae1777fbdd30a81d966fb9
Bruno Cenou <bruno.cenou@openedition.org>
2006-01-27 08:31:22 +0000

    Type: A Descr: elimination js code
    git-svn-id: https://subversion.renater.fr/lodel/trunk@2942 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- share/js/interface.js
+++ share/js/interface.js
@@ -96,7 +96,133 @@ function changeNameAttributes(obj,index)
 		}
 		}
 	}
-}	
+}
+
+function trim(inputString) {
+   // Removes leading and trailing spaces from the passed string. Also removes
+   // consecutive spaces and replaces it with one space. If something besides
+   // a string is passed in (null, custom object, etc.) then return the input.
+   if (typeof inputString != "string") { return inputString; }
+   var retValue = inputString;
+   var ch = retValue.substring(0, 1);
+   while (ch == " ") { // Check for spaces at the beginning of the string
+      retValue = retValue.substring(1, retValue.length);
+      ch = retValue.substring(0, 1);
+   }
+   ch = retValue.substring(retValue.length-1, retValue.length);
+   while (ch == " ") { // Check for spaces at the end of the string
+      retValue = retValue.substring(0, retValue.length-1);
+      ch = retValue.substring(retValue.length-1, retValue.length);
+   }
+   while (retValue.indexOf("  ") != -1) { // Note that there are two spaces in the string - look for multiple spaces within the string
+      retValue = retValue.substring(0, retValue.indexOf("  ")) + retValue.substring(retValue.indexOf("  ")+1, retValue.length); // Again, there are two spaces in each of the strings
+   }
+   return retValue; // Return the trimmed string back to the user
+} // Ends the "trim" function
+
+
+// POOL
+
+function addmember(candidats, membres, textfield){	
+	var cand = document.getElementById(candidats);
+	var memb = document.getElementById(membres);
+	for(i=0; i<cand.length; i++){
+		if(cand.options[i].selected){
+			opttxt = cand.options[i].text;
+			optval = cand.options[i].value;
+			dbl = false;
+			if(memb.length > 0){
+				for(j=0; j < memb.length; j++){
+					if(memb.options[j].value == optval){
+						dbl = true;
+					}
+				}
+			}
+			if(dbl == false){
+				newopt = new Option(opttxt, optval);
+				memb.options[memb.length] = newopt;
+			}
+			cand.options[i].selected=false;
+		}
+	}
+	updatetxt(textfield, membres);
+}
+
+function removemember(membres, textfield){	
+	var memb = document.getElementById(membres);
+	if(memb.length>0){
+		for(i=0; i < memb.length; i++){
+			if(memb.options[i].selected){
+				memb.options[i] = null;
+				i--;
+			}
+		}
+	}
+	updatetxt(textfield, membres);
+}
+
+function addnewmember(membres, textfield, msg) {
+
+	var val = prompt(msg,"");
+	if (val==null) return;
+
+	var memb = document.getElementById(membres);
+	var tab = val.split(",");
+	for(i=0; i<tab.length; i++){
+		opttxt = trim(tab[i]);
+		optval = trim(tab[i]);
+		dbl = false;
+		if(memb.length > 0){
+			for(j=0; j < memb.length; j++){
+				if(memb.options[j].value == optval){
+					dbl = true;
+				}
+			}
+		}
+		if(dbl == false && optval){
+			newopt = new Option(opttxt, optval);
+			memb.options[memb.length] = newopt;
+		}
+	}
+	updatetxt(textfield, membres);
+}
+
+
+function updatetxt(textfield, selectfield){
+	var txt = document.getElementById(textfield);
+	var slct = document.getElementById(selectfield);
+	var str = ''
+	if(slct.length > 0){
+		for(i=0; i< slct.length; i++){
+			str += slct.options[i].value + ',';
+		}
+	}
+	txt.value = str.substring(0, str.length - 1);
+}
+
+
+function modifymember(members,textfield){
+	memb = document.getElementById(members);
+	if(memb.length>0){
+		for(i=0; i < memb.length; i++){
+			if(memb.options[i].selected){
+				var val = prompt("",memb.options[i].value);
+				if(val == null) break;//val=memb.options[i].value; // get out, stop editing!
+				val=val.replace(/,/," ");
+				if(val != ''){
+					memb.options[i].value = val;
+					memb.options[i].text = val;
+				}else{
+					memb.options[i]=null;
+				}
+
+			}
+		}
+	}
+	updatetxt(textfield, members);
+}
+
+// END POOL
 
 /************************************************************************/
 /* END Miscellaneous functions
