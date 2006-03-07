d5009943e24a88338a0534e6a2e6fd30edf85884
lamy <lamy>
2006-03-07 10:32:49 +0000

    Type:A Descr:Mise a jour de la gestion de l'import de ME
    git-svn-id: https://subversion.renater.fr/lodel/trunk@3336 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodeladmin/site.php
+++ lodeladmin/site.php
@@ -494,7 +494,7 @@ if ($task == 'file') {
 		$context['path'] = '/'. $context['rep'];
 	}
 	if ($import) {
-		$go = $context['url']. '/lodel/admin/importmodel.php?frominstall=1';
+		$go = $context['url']. "/lodel/admin/index.php?do=importmodel&lo=data&frominstall=1";
 	} else {
 		$go = $context['url']. '/lodel/edition';
 	}
