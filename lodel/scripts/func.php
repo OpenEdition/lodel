1cbeb3e41bbfb8d22ddf7086ed11b991d792c2bb
Sophie Malafosse <malafoss>
2006-06-07 15:21:39 +0000

    Type:Bm Descr: Correction de la fonction _indent_xhtml pour prevoir les fichiers sources vides.
    git-svn-id: https://subversion.renater.fr/lodel/trunk@3517 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/func.php
+++ lodel/scripts/func.php
@@ -950,7 +950,11 @@ function fix_newlines_for_clean_html($fixthistext)
 		
 	}
 	
-	return implode("\n", $fixedtext_array);
+	if (is_array($fixedtext_array)) {
+		return implode("\n", $fixedtext_array);
+	} else {
+		return false;
+	}
 }
 
 /**
@@ -966,74 +970,75 @@ function _indent_xhtml ($uncleanhtml, $indent = "  ")
 {
 	//Set wanted indentation
 	//$indent = "    ";
-
-
 	//Uses previous function to seperate tags
-	$fixed_uncleanhtml = fix_newlines_for_clean_html($uncleanhtml);
-	$uncleanhtml_array = explode("\n", $fixed_uncleanhtml); 
+	if ($fixed_uncleanhtml = fix_newlines_for_clean_html($uncleanhtml)) {
+		$uncleanhtml_array = explode("\n", $fixed_uncleanhtml);
 	
-	//Sets no indentation
-	$indentlevel = 0;
-	foreach ($uncleanhtml_array as $uncleanhtml_key => $currentuncleanhtml)
-	{
-		//Removes all indentation
-		$currentuncleanhtml = preg_replace("/\t+/", "", $currentuncleanhtml);
-		$currentuncleanhtml = preg_replace("/^\s+/", "", $currentuncleanhtml);
-		
-		$replaceindent = "";
-		
-		//Sets the indentation from current indentlevel
-		for ($o = 0; $o < $indentlevel; $o++)
+		//Sets no indentation
+		$indentlevel = 0;
+		foreach ($uncleanhtml_array as $uncleanhtml_key => $currentuncleanhtml)
 		{
-			$replaceindent .= $indent;
-		}
+			//Removes all indentation
+			$currentuncleanhtml = preg_replace("/\t+/", "", $currentuncleanhtml);
+			$currentuncleanhtml = preg_replace("/^\s+/", "", $currentuncleanhtml);
 		
-		//If self-closing tag, simply apply indent
-		if (preg_match("/<(.+)\/>/", $currentuncleanhtml))
-		{ 
-			$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
-		}
-		//If doctype declaration, simply apply indent
-		else if (preg_match("/<!(.*)>/", $currentuncleanhtml))
-		{ 
-			$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
-		}
-		//If opening AND closing tag on same line, simply apply indent
-		else if (preg_match("/<[^\/](.*)>/", $currentuncleanhtml) && preg_match("/<\/(.*)>/", $currentuncleanhtml))
-		{ 
-			$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
-		}
-		//If closing HTML tag or closing JavaScript clams, decrease indentation and then apply the new level
-		else if (preg_match("/<\/(.*)>/", $currentuncleanhtml) || preg_match("/^(\s|\t)*\}{1}(\s|\t)*$/", $currentuncleanhtml))
-		{
-			$indentlevel--;
 			$replaceindent = "";
+		
+			//Sets the indentation from current indentlevel
 			for ($o = 0; $o < $indentlevel; $o++)
 			{
 				$replaceindent .= $indent;
 			}
-			
-			$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
-		}
-		//If opening HTML tag AND not a stand-alone tag, or opening JavaScript clams, increase indentation and then apply new level
-		else if ((preg_match("/<[^\/](.*)>/", $currentuncleanhtml) && !preg_match("/<(link|meta|base|br|img|hr)(.*)>/", $currentuncleanhtml)) || preg_match("/^(\s|\t)*\{{1}(\s|\t)*$/", $currentuncleanhtml))
-		{
-			$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
-			
-			$indentlevel++;
-			$replaceindent = "";
-			for ($o = 0; $o < $indentlevel; $o++)
-			{
-				$replaceindent .= $indent;
+		
+			//If self-closing tag, simply apply indent
+			if (preg_match("/<(.+)\/>/", $currentuncleanhtml))
+			{ 
+				$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
 			}
+			//If doctype declaration, simply apply indent
+			else if (preg_match("/<!(.*)>/", $currentuncleanhtml))
+			{ 
+				$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
+			}
+			//If opening AND closing tag on same line, simply apply indent
+			else if (preg_match("/<[^\/](.*)>/", $currentuncleanhtml) && preg_match("/<\/(.*)>/", $currentuncleanhtml))
+			{ 
+				$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
+			}
+			//If closing HTML tag or closing JavaScript clams, decrease indentation and then apply the new level
+			else if (preg_match("/<\/(.*)>/", $currentuncleanhtml) || preg_match("/^(\s|\t)*\}{1}(\s|\t)*$/", $currentuncleanhtml))
+			{
+				$indentlevel--;
+				$replaceindent = "";
+				for ($o = 0; $o < $indentlevel; $o++)
+				{
+					$replaceindent .= $indent;
+				}
+			
+				$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
+			}
+			//If opening HTML tag AND not a stand-alone tag, or opening JavaScript clams, increase indentation and then apply new level
+			else if ((preg_match("/<[^\/](.*)>/", $currentuncleanhtml) && !preg_match("/<(link|meta|base|br|img|hr)(.*)>/", $currentuncleanhtml)) || preg_match("/^(\s|\t)*\{{1}(\s|\t)*$/", $currentuncleanhtml))
+			{
+				$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;
+			
+				$indentlevel++;
+				$replaceindent = "";
+				for ($o = 0; $o < $indentlevel; $o++)
+				{
+					$replaceindent .= $indent;
+				}
+			}
+			else
+			//Else, only apply indentation
+			{$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;}
 		}
-		else
-		//Else, only apply indentation
-		{$cleanhtml_array[$uncleanhtml_key] = $replaceindent.$currentuncleanhtml;}
-	}
-	//Return single string seperated by newline
+		//Return single string seperated by newline
 
-	return implode("\n", $cleanhtml_array);	
+		return implode("\n", $cleanhtml_array);	
+	} else {
+			return '';
+		}
 }
 
 
