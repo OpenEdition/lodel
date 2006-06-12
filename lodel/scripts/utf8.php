22cf3140cee8de0d04df7eeeb0722e8ed527691c
Sophie Malafosse <malafoss>
2006-06-12 09:59:59 +0000

    Type: Bm Descr: Changement de l'encodage du fichier (cp 1252)
    git-svn-id: https://subversion.renater.fr/lodel/trunk@3529 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/utf8.php
+++ lodel/scripts/utf8.php
@@ -33,7 +33,7 @@
  *
  * @author Ghislain Picard
  * @author Jean Lamy
- * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Bruno Cénou, Jean Lamy
+ * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Bruno C�nnou, Jean Lamy
  * @licence http://www.gnu.org/copyleft/gpl.html
  * @version CVS:$Id:
  * @package lodel
@@ -41,109 +41,109 @@
 
 
 /**
- * Conversion des entités HTML en UTF-8
+ * Conversion des entit�s HTML en UTF-8
  *
- * <p>Cette fonction utilitaire permet de transformer toutes les entités HTML d'un texte en
+ * <p>Cette fonction utilitaire permet de transformer toutes les entit�s HTML d'un texte en
  * UTF-8.</p>
  *
- * @param string &$text le texte à transformer (par référence).
- * @return le texte transformé
+ * @param string &$text le texte � transformer (par r�f�rence).
+ * @return le texte transform�
  */
 function convertHTMLtoUTF8(&$text)
 {
-	$hash = array(
-				"eacute"=>'é',
-				"Eacute"=>'É',
-				"iacute"=>'í',
-				"Iacute"=>'Í',
-				"oacute"=>'ó',
-				"Oacute"=>'Ó',
-				"aacute"=>'á',
-				"Aacute"=>'Á',
-				"uacute"=>'ú',
-				"Uacute"=>'Ú',
+	$hash=array(
+	      "eacute"=>'é',
+	      "Eacute"=>'É',
+	      "iacute"=>'í',
+	      "Iacute"=>'�?',
+	      "oacute"=>'ó',
+	      "Oacute"=>'Ó',
+	      "aacute"=>'á',
+	      "Aacute"=>'�?',
+	      "uacute"=>'ú',
+	      "Uacute"=>'Ú',
 
-				"egrave"=>'è',
-				"Egrave"=>'È',
-				"agrave"=>'à',
-				"Agrave"=>'À',
-				"ugrave"=>'ù',
-				"Ugrave"=>'Ù',
-				"ograve"=>'ò',
-				"Ograve"=>'Ò',
+	      "egrave"=>'è',
+	      "Egrave"=>'È',
+	      "agrave"=>'à',
+	      "Agrave"=>'À',
+	      "ugrave"=>'ù',
+	      "Ugrave"=>'Ù',
+	      "ograve"=>'ò',
+	      "Ograve"=>'Ò',
 
-				"ecirc"=>'ê',
-				"Ecirc"=>'Ê',
-				"icirc"=>'î',
-				"Icirc"=>'Î',
-				"ocirc"=>'ô',
-				"Ocirc"=>'Ô',
-				"acirc"=>'â',
-				"Acirc"=>'Â',
-				"ucirc"=>'û',
-				"Ucirc"=>'Û',
+	      "ecirc"=>'ê',
+	      "Ecirc"=>'Ê',
+	      "icirc"=>'î',
+	      "Icirc"=>'Î',
+	      "ocirc"=>'ô',
+	      "Ocirc"=>'Ô',
+	      "acirc"=>'â',
+	      "Acirc"=>'Â',
+	      "ucirc"=>'û',
+	      "Ucirc"=>'Û',
 
-				"Atilde"=>'Ã',
-				"Auml"=>'Ä',
-					"AElig"=>'Æ',
-				"OElig"=>"\305\222",
-				"oelig"=>"\305\223",
-				"Ccedil"=>'Ç',
-				"Euml"=>'Ë',
-				"Igrave"=>'Ì',
-				"Ntilde"=>'Ñ',
-				"Iuml"=>'Ï',
-				"Ograve"=>'Ò',
-				"Oacute"=>'Ó',
-				"Ocirc"=>'Ô',
-				"Otilde"=>'Õ',
-				"Ouml"=>'Ö',
-				"Uuml"=>'Ü',
+	      "Atilde"=>'Ã',
+	      "Auml"=>'Ä',
+	      "AElig"=>'Æ',
+	      "OElig"=>"\305\222",
+	      "oelig"=>"\305\223",
+	      "Ccedil"=>'Ç',
+	      "Euml"=>'Ë',
+	      "Igrave"=>'Ì',
+	      "Ntilde"=>'Ñ',
+	      "Iuml"=>'�?',
+	      "Ograve"=>'Ò',
+	      "Oacute"=>'Ó',
+	      "Ocirc"=>'Ô',
+	      "Otilde"=>'Õ',
+	      "Ouml"=>'Ö',
+	      "Uuml"=>'Ü',
 
-				"atilde"=>'ã',
-				"auml"=>'ä',
-				"aelig"=>'æ',
-				"ccedil"=>'ç',
-				"euml"=>'ë',
-				"igrave"=>'ì',
-				"iuml"=>'ï',
-				"ntilde"=>'ñ',
-				"ograve"=>'ò',
-				"otilde"=>'õ',
-				"ouml"=>'ö',
-				"uuml"=>'ü',
-				"yacute"=>'ý',
-				"yuml"=>'ÿ',
-				"Aring" =>"\303\205",
-				"aring" =>"\303\245",
-				"curren"=>"\302\244",
-				"micro"=> "\302\265",
-				"Oslash"=>"\303\230",
-				"cent"=>"\302\242",
-				"pound"=>"\302\243",
-				"ordf"=>"\302\252",
-				"copy"=>"\302\251",
-				"para"=>"\303\266",
-				"plusmm"=>"\302\261",
-				"THORN"=>"\303\236",
-				"shy"=>"\302\255",
-				"not"=>"\302\254",
-				"hellip"=>"\342\200\246",
-				"laquo"=>'«',
-				"raquo"=>'»',
-				"lsquo"=>"\342\200\230",
-				"rsquo"=>"\342\200\231",
-				"ldquo"=>"\342\200\234",
-				"rdquo"=>"\342\200\235",
-				"deg"=>'°',
-				"nbsp"=>"Â\240",
-				"mdash"=>"\342\200\224",
-				"ndash"=>"\342\200\223",
+	      "atilde"=>'ã',
+	      "auml"=>'ä',
+	      "aelig"=>'æ',
+	      "ccedil"=>'ç',
+	      "euml"=>'ë',
+	      "igrave"=>'ì',
+	      "iuml"=>'ï',
+	      "ntilde"=>'ñ',
+	      "ograve"=>'ò',
+	      "otilde"=>'õ',
+	      "ouml"=>'ö',
+	      "uuml"=>'ü',
+	      "yacute"=>'ý',
+	      "yuml"=>'ÿ',
+	      "Aring" =>"\303\205",
+	      "aring" =>"\303\245",
+	      "curren"=>"\302\244",
+	      "micro"=> "\302\265",
+	      "Oslash"=>"\303\230",
+	      "cent"=>"\302\242",
+	      "pound"=>"\302\243",
+	      "ordf"=>"\302\252",
+	      "copy"=>"\302\251",
+	      "para"=>"\303\266",
+	      "plusmm"=>"\302\261",
+	      "THORN"=>"\303\236",
+	      "shy"=>"\302\255",
+	      "not"=>"\302\254",
+	      "hellip"=>"\342\200\246",
+	      "laquo"=>'«',
+	      "raquo"=>'»',
+	      "lsquo"=>"\342\200\230",
+	      "rsquo"=>"\342\200\231",
+	      "ldquo"=>"\342\200\234",
+	      "rdquo"=>"\342\200\235",
+	      "deg"=>'°',
+	      "nbsp"=>"�\240",
+	      "mdash"=>"\342\200\224",
+	      "ndash"=>"\342\200\223",
 
-				"reg"=>"\302\256",
-				"sect"=>"\302\247"
-				);
-
+	      "reg"=>"\302\256",
+	      "sect"=>"\302\247"
+	      );
+	
 	$text = preg_replace("/&(\w+);/e", '$hash[\\1] ? $hash[\\1] : "\\0"', $text);
 	return $text;
 }
