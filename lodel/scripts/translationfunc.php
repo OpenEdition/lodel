e01ed978b58fc9211958c75fe113686c073ae66a
Bruno Cenou <bruno.cenou@openedition.org>
2006-06-23 15:23:06 +0000

    Type: A Descr: retouche
    git-svn-id: https://subversion.renater.fr/lodel/trunk@3614 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/translationfunc.php
+++ lodel/scripts/translationfunc.php
@@ -63,7 +63,7 @@ function mkeditlodeltext($name, $textgroup, $lang = -1)
 	if ($nrows > 10)
 		$nrows = 10; // limit for very long text, it's not usefull anyway
 	echo '<dt><label for="contents'.$id.'">@'.strtoupper($name).'</label></dt>
-	<dd><textarea name="contents['.$id.']" id="contents'.$id.'" cols="'.$ncols.'" rows="'.$nrows.'" " onchange=" tachanged('.$id.');" >'.htmlspecialchars($text).'</textarea>
+	<dd><textarea name="contents['.$id.']" id="contents'.$id.'" cols="'.$ncols.'" rows="'.$nrows.'"  onchange=" tachanged('.$id.');" >'.htmlspecialchars($text).'</textarea>
 	 <select class="select'.lodeltextcolor($status).'" onchange="selectchanged(this);" id="selectstatus'.$id.'" name="status['.$id.']">';
 
 	foreach (array (-1, 1, 2) as $s) {
