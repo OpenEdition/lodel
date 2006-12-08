cedbcb447e6c3800443f8072a576fd93cf5b144e
Sophie Malafosse <malafosse>
2006-12-08 16:55:25 +0000

    Type:Bm Descr:Ajout des tables OAI dans les fichiers DAO
    git-svn-id: https://subversion.renater.fr/lodel/trunk@3777 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/scripts/dao/class.oai_token.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Fichier DAO de la table SQL oai_token.
+ *
+ * PHP version 4
+ *
+ * LODEL - Logiciel d'Edition ELectronique.
+ *
+ * Copyright (c) 2001-2002, Ghislain Picard, Marin Dacos
+ * Copyright (c) 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ * Copyright (c) 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ * Copyright (c) 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Bruno Cénou, Jean Lamy
+ *
+ * Home page: http://www.lodel.org
+ *
+ * E-Mail: lodel@lodel.org
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @author Ghislain Picard
+ * @author Jean Lamy
+ * @copyright 2001-2002, Ghislain Picard, Marin Dacos
+ * @copyright 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ * @copyright 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Jean Lamy
+ * @licence http://www.gnu.org/copyleft/gpl.html
+ * @since Fichier ajouté depuis la version 0.8
+ * @version CVS:$Id: generate.php 2821 2006-01-05 14:44:33Z lamy $
+ * @package lodel/dao
+ */
+
+//
+// Fichier généré automatiquement le 08-12-2006.
+//
+
+
+/**
+ * Classe d'objet virtuel de la table SQL oai_token
+ *
+ * @package lodel/dao
+ * @author Ghislain Picard
+ * @author Jean Lamy
+ * @copyright 2001-2002, Ghislain Picard, Marin Dacos
+ * @copyright 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ * @copyright 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Jean Lamy
+ * @licence http://www.gnu.org/copyleft/gpl.html
+ * @since Classe ajoutée depuis la version 0.8
+ * @see dao.php
+ */
+class oai_tokenVO 
+{
+	/**#@+
+	 * @access public
+	 */
+	var $token;
+	var $query;
+	var $metadataprefix;
+	var $deliveredrecords;
+	var $expirationdatetime;
+	/**#@-*/
+}
+
+/**
+ * Classe d'abstraction de la base de données de la table oai_token
+ *
+ * Fille de la classe DAO
+ *
+ * @package lodel/dao
+ * @author Ghislain Picard
+ * @author Jean Lamy
+ * @copyright 2001-2002, Ghislain Picard, Marin Dacos
+ * @copyright 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
+ * @copyright 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
+ * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Jean Lamy
+ * @licence http://www.gnu.org/copyleft/gpl.html
+ * @since Classe ajoutée depuis la version 0.8
+ * @see dao.php
+ */
+class oai_tokenDAO extends DAO 
+{
+	/**
+	 * Constructeur
+	 *
+	 * <p>Appelle le constructeur de la classe mère DAO en lui passant le nom de la classe.
+	 * Renseigne aussi le tableau rights des droits.
+	 * </p>
+	 */
+	function oai_tokenDAO()
+	{
+		$this->DAO("oai_token", false);
+		$this->rights = array();
+	}
+
+}
+
+?>
