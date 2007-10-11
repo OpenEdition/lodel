ee35de52b782aa44de6dc287463a843ec7057247
Pierre-Alain Mignot <mignot>
2007-10-11 08:48:14 +0000

    Type:Bm Descr:Retour en arrière de la version de FCKEditor suite au bug #3313 qui reste ouvert en attendant
    git-svn-id: https://subversion.renater.fr/lodel/trunk@4018 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- share/plugins/fckeditor/fckconfig.js
+++ share/plugins/fckeditor/fckconfig.js
@@ -1,38 +1,28 @@
-/*
- * FCKeditor - The text editor for Internet - http://www.fckeditor.net
- * Copyright (C) 2003-2007 Frederico Caldeira Knabben
- *
- * == BEGIN LICENSE ==
- *
- * Licensed under the terms of any of the following licenses at your
- * choice:
- *
- *  - GNU General Public License Version 2 or later (the "GPL")
- *    http://www.gnu.org/licenses/gpl.html
- *
- *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
- *    http://www.gnu.org/licenses/lgpl.html
- *
- *  - Mozilla Public License Version 1.1 or later (the "MPL")
- *    http://www.mozilla.org/MPL/MPL-1.1.html
- *
- * == END LICENSE ==
- *
- * Editor configuration settings.
- *
- * Follow this link for more information:
- * http://wiki.fckeditor.net/Developer%27s_Guide/Configuration/Configurations_Settings
+ï»¿/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2005 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * "Support Open Source software. What about a donation today?"
+ * 
+ * File Name: fckconfig.js
+ * 	Editor configuration settings.
+ * 	See the documentation for more info.
+ * 
+ * File Authors:
+ * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
  */
-
-// Disable the custom Enter Key Handler. This option will be removed in version 2.5.
-FCKConfig.DisableEnterKeyHandler = false ;
 
 FCKConfig.CustomConfigurationsPath = '' ;
 
 FCKConfig.EditorAreaCSS = FCKConfig.BasePath + 'css/fck_editorarea.css' ;
-FCKConfig.ToolbarComboPreviewCSS = '' ;
 
-FCKConfig.DocType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' ; //LODEL
+FCKConfig.DocType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' ;
 
 FCKConfig.BaseHref = '' ;
 
@@ -42,32 +32,26 @@ FCKConfig.Debug = false ;
 FCKConfig.AllowQueryStringDebug = true ;
 
 FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/default/' ;
-FCKConfig.PreloadImages = [ FCKConfig.SkinPath + 'images/toolbar.start.gif', FCKConfig.SkinPath + 'images/toolbar.buttonarrow.gif' ] ;
 
 FCKConfig.PluginsPath = FCKConfig.BasePath + 'plugins/' ;
 
-// FCKConfig.Plugins.Add( 'autogrow' ) ;
-FCKConfig.AutoGrowMax = 400 ;
+// FCKConfig.Plugins.Add( 'placeholder', 'de,en,fr,it,pl' ) ;
 
-//LODEL
-FCKConfig.ProtectedSource.Add( /<script[\s\S]*?\/script>/gi ) ; // <SCRIPT> tags.
+FCKConfig.ProtectedSource.Add( /<script[\s\S]*?\/script>/gi ) ;	// <SCRIPT> tags.
 // FCKConfig.ProtectedSource.Add( /<%[\s\S]*?%>/g ) ;	// ASP style server side code <%...%>
-// FCKConfig.ProtectedSource.Add( /<\?[\s\S]*?\?>/g ) ;	// PHP style server side code
+// FCKConfig.ProtectedSource.Add( /<\?[\s\S]*?\?>/g ) ;	// PHP style server side code <?...?>
 // FCKConfig.ProtectedSource.Add( /(<asp:[^\>]+>[\s|\S]*?<\/asp:[^\>]+>)|(<asp:[^\>]+\/>)/gi ) ;	// ASP.Net style tags <asp:control>
 
-FCKConfig.AutoDetectLanguage	= true ;
-
-FCKConfig.DefaultLanguage		= 'fr' ; // LODEL
-
+FCKConfig.AutoDetectLanguage	= false ;
+FCKConfig.DefaultLanguage		= "fr" ;
 FCKConfig.ContentLangDirection	= 'ltr' ;
 
-FCKConfig.ProcessHTMLEntities	= false ; // LODEL
+FCKConfig.EnableXHTML		= true ;	// Unsupported: Do not change.
+FCKConfig.EnableSourceXHTML	= true ;	// Unsupported: Do not change.
+
+FCKConfig.ProcessHTMLEntities	= false ; // à false ! Très important pour Lodel !!!!!!!!!!!!!
 FCKConfig.IncludeLatinEntities	= true ;
 FCKConfig.IncludeGreekEntities	= true ;
-
-FCKConfig.ProcessNumericEntities = false ;
-
-FCKConfig.AdditionalNumericEntities = ''  ;		// Single Quote: "'"
 
 FCKConfig.FillEmptyBlocks	= true ;
 
@@ -76,26 +60,21 @@ FCKConfig.FormatOutput		= true ;
 FCKConfig.FormatIndentator	= '    ' ;
 
 FCKConfig.ForceStrongEm = true ;
-FCKConfig.GeckoUseSPAN	= false ;
+FCKConfig.GeckoUseSPAN	= true ;
 FCKConfig.StartupFocus	= false ;
 FCKConfig.ForcePasteAsPlainText	= false ;
 FCKConfig.AutoDetectPasteFromWord = true ;	// IE only.
 FCKConfig.ForceSimpleAmpersand	= false ;
 FCKConfig.TabSpaces		= 0 ;
 FCKConfig.ShowBorders	= true ;
-FCKConfig.SourcePopup	= false ;
+FCKConfig.UseBROnCarriageReturn	= false ;	// IE only.
 FCKConfig.ToolbarStartExpanded	= true ;
 FCKConfig.ToolbarCanCollapse	= true ;
-
-FCKConfig.IEForceVScroll = false ;//popop
+FCKConfig.IEForceVScroll = false ;
 FCKConfig.IgnoreEmptyParagraphValue = true ;
 FCKConfig.PreserveSessionOnFileBrowser = false ;
 FCKConfig.FloatingPanelsZIndex = 10000 ;
 
-FCKConfig.TemplateReplaceAll = true ;
-FCKConfig.TemplateReplaceCheckbox = true ;
-
-FCKConfig.ToolbarLocation = 'In' ;
 
 FCKConfig.ToolbarSets["Advanced"] = [
         ['NewPage','-','Cut','Copy','Paste','PasteText','PasteWord'],
@@ -142,31 +121,10 @@ FCKConfig.ToolbarSets["Simple"] = [
 ] ;
 
 
-FCKConfig.EnterMode = 'p' ;			// p | div | br
-FCKConfig.ShiftEnterMode = 'br' ;	// p | div | br
 
-FCKConfig.Keystrokes = [
-	[ CTRL + 65 /*A*/, true ],
-	[ CTRL + 67 /*C*/, true ],
-	[ CTRL + 70 /*F*/, true ],
-	[ CTRL + 83 /*S*/, true ],
-	[ CTRL + 88 /*X*/, true ],
-	[ CTRL + 86 /*V*/, 'Paste' ],
-	[ SHIFT + 45 /*INS*/, 'Paste' ],
-	[ CTRL + 90 /*Z*/, 'Undo' ],
-	[ CTRL + 89 /*Y*/, 'Redo' ],
-	[ CTRL + SHIFT + 90 /*Z*/, 'Redo' ],
-	[ CTRL + 76 /*L*/, 'Link' ],
-	[ CTRL + 66 /*B*/, 'Bold' ],
-	[ CTRL + 73 /*I*/, 'Italic' ],
-	[ CTRL + 85 /*U*/, 'Underline' ],
-	[ CTRL + SHIFT + 83 /*S*/, 'Save' ],
-	[ CTRL + ALT + 13 /*ENTER*/, 'FitWindow' ],
-	[ CTRL + 9 /*TAB*/, 'Source' ]
-] ;
 
-FCKConfig.ContextMenu = ['Generic','Link','Anchor','Image','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ;
-FCKConfig.BrowserContextMenuOnCtrl = false ;
+
+FCKConfig.ContextMenu = ['Generic','Link','Anchor','Image','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','TableCell','Table','Form'] ;
 
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
 
@@ -178,14 +136,12 @@ FCKConfig.StylesXmlPath		= FCKConfig.EditorPath + 'fckstyles.xml' ;
 FCKConfig.TemplatesXmlPath	= FCKConfig.EditorPath + 'fcktemplates.xml' ;
 
 FCKConfig.SpellChecker			= 'ieSpell' ;	// 'ieSpell' | 'SpellerPages'
-FCKConfig.IeSpellDownloadUrl	= 'http://www.iespell.com/download.php' ;
-FCKConfig.SpellerPagesServerScript = 'server-scripts/spellchecker.php' ;	// Available extension: .php .cfm .pl
-FCKConfig.FirefoxSpellChecker	= false ;
+FCKConfig.IeSpellDownloadUrl	= 'http://www.iespell.com/rel/ieSpellSetup211325.exe' ;
 
 FCKConfig.MaxUndoLevels = 15 ;
 
-FCKConfig.DisableObjectResizing = false ;
-FCKConfig.DisableFFTableHandles = true ;
+FCKConfig.DisableImageHandles = false ;
+FCKConfig.DisableTableHandles = false ;
 
 FCKConfig.LinkDlgHideTarget		= false ;
 FCKConfig.LinkDlgHideAdvanced	= false ;
@@ -195,29 +151,17 @@ FCKConfig.ImageDlgHideAdvanced	= false ;
 
 FCKConfig.FlashDlgHideAdvanced	= false ;
 
-FCKConfig.ProtectedTags = '' ;
-
-// This will be applied to the body element of the editor
-FCKConfig.BodyId = '' ;
-FCKConfig.BodyClass = '' ;
-
-FCKConfig.DefaultLinkTarget = '' ;
-
-// The option switches between trying to keep the html structure or do the changes so the content looks like it was in Word
-FCKConfig.CleanWordKeepsStructure = false ;
-
-// The following value defines which File Browser connector and Quick Upload
+// The following value defines which File Browser connector and Quick Upload 
 // "uploader" to use. It is valid for the default implementaion and it is here
-// just to make this configuration file cleaner.
-// It is not possible to change this value using an external file or even
-// inline when creating the editor instance. In that cases you must set the
+// just to make this configuration file cleaner. 
+// It is not possible to change this value using an external file or even 
+// inline when creating the editor instance. In that cases you must set the 
 // values of LinkBrowserURL, ImageBrowserURL and so on.
 // Custom implementations should just ignore it.
 var _FileBrowserLanguage	= 'asp' ;	// asp | aspx | cfm | lasso | perl | php | py
 var _QuickUploadLanguage	= 'asp' ;	// asp | aspx | cfm | lasso | php
 
-
-// Don't care about the following line. It just calculates the correct connector
+// Don't care about the following line. It just calculates the correct connector 
 // extension to use for the default File Browser (Perl uses "cgi").
 var _FileBrowserExtension = _FileBrowserLanguage == 'perl' ? 'cgi' : _FileBrowserLanguage ;
 
@@ -237,22 +181,24 @@ FCKConfig.FlashBrowserWindowWidth  = FCKConfig.ScreenWidth * 0.7 ;	//70% ;
 FCKConfig.FlashBrowserWindowHeight = FCKConfig.ScreenHeight * 0.7 ;	//70% ;
 
 FCKConfig.LinkUpload = true ;
-FCKConfig.LinkUploadURL = FCKConfig.BasePath + 'filemanager/upload/' + _QuickUploadLanguage + '/upload.' + _QuickUploadLanguage ;
+FCKConfig.LinkUploadURL = FCKConfig.BasePath + 'filemanager/upload/' + FCKConfig.QuickUploadLanguage + '/upload.' + _QuickUploadLanguage ;
 FCKConfig.LinkUploadAllowedExtensions	= "" ;			// empty for all
-FCKConfig.LinkUploadDeniedExtensions	= ".(html|htm|php|php2|php3|php4|php5|phtml|pwml|inc|asp|aspx|ascx|jsp|cfm|cfc|pl|bat|exe|com|dll|vbs|js|reg|cgi|htaccess|asis|sh|shtml|shtm|phtm)$" ;	// empty for no one
+FCKConfig.LinkUploadDeniedExtensions	= ".(php|php3|php5|phtml|asp|aspx|ascx|jsp|cfm|cfc|pl|bat|exe|dll|reg|cgi)$" ;	// empty for no one
 
 FCKConfig.ImageUpload = true ;
-FCKConfig.ImageUploadURL = FCKConfig.BasePath + 'filemanager/upload/' + _QuickUploadLanguage + '/upload.' + _QuickUploadLanguage + '?Type=Image' ;
-FCKConfig.ImageUploadAllowedExtensions	= ".(jpg|gif|jpeg|png|bmp)$" ;		// empty for all
+FCKConfig.ImageUploadURL = FCKConfig.BasePath + 'filemanager/upload/' + FCKConfig.QuickUploadLanguage + '/upload.' + _QuickUploadLanguage + '?Type=Image' ;
+FCKConfig.ImageUploadAllowedExtensions	= ".(jpg|gif|jpeg|png)$" ;		// empty for all
 FCKConfig.ImageUploadDeniedExtensions	= "" ;							// empty for no one
 
 FCKConfig.FlashUpload = true ;
-FCKConfig.FlashUploadURL = FCKConfig.BasePath + 'filemanager/upload/' + _QuickUploadLanguage + '/upload.' + _QuickUploadLanguage + '?Type=Flash' ;
+FCKConfig.FlashUploadURL = FCKConfig.BasePath + 'filemanager/upload/' + FCKConfig.QuickUploadLanguage + '/upload.' + _QuickUploadLanguage + '?Type=Flash' ;
 FCKConfig.FlashUploadAllowedExtensions	= ".(swf|fla)$" ;		// empty for all
 FCKConfig.FlashUploadDeniedExtensions	= "" ;					// empty for no one
 
-//FCKConfig.SmileyPath	= FCKConfig.BasePath + 'images/smiley/msn/' ;
-//FCKConfig.SmileyImages	= ['regular_smile.gif','sad_smile.gif','wink_smile.gif','teeth_smile.gif','confused_smile.gif','tounge_smile.gif','embaressed_smile.gif','omg_smile.gif','whatchutalkingabout_smile.gif','angry_smile.gif','angel_smile.gif','shades_smile.gif','devil_smile.gif','cry_smile.gif','lightbulb.gif','thumbs_down.gif','thumbs_up.gif','heart.gif','broken_heart.gif','kiss.gif','envelope.gif'] ;
-//FCKConfig.SmileyColumns = 8 ;
-//FCKConfig.SmileyWindowWidth		= 320 ;
-//FCKConfig.SmileyWindowHeight	= 240 ;
+FCKConfig.SmileyPath	= FCKConfig.BasePath + 'images/smiley/msn/' ;
+FCKConfig.SmileyImages	= ['regular_smile.gif','sad_smile.gif','wink_smile.gif','teeth_smile.gif','confused_smile.gif','tounge_smile.gif','embaressed_smile.gif','omg_smile.gif','whatchutalkingabout_smile.gif','angry_smile.gif','angel_smile.gif','shades_smile.gif','devil_smile.gif','cry_smile.gif','lightbulb.gif','thumbs_down.gif','thumbs_up.gif','heart.gif','broken_heart.gif','kiss.gif','envelope.gif'] ;
+FCKConfig.SmileyColumns = 8 ;
+FCKConfig.SmileyWindowWidth		= 320 ;
+FCKConfig.SmileyWindowHeight	= 240 ;
+
+if( window.console ) window.console.log( 'Config is loaded!' ) ;	// @Packager.Compactor.RemoveLine
--- share/plugins/fckeditor/fckeditor.js
+++ share/plugins/fckeditor/fckeditor.js
@@ -1,28 +1,24 @@
-/*
- * FCKeditor - The text editor for Internet - http://www.fckeditor.net
- * Copyright (C) 2003-2007 Frederico Caldeira Knabben
- *
- * == BEGIN LICENSE ==
- *
- * Licensed under the terms of any of the following licenses at your
- * choice:
- *
- *  - GNU General Public License Version 2 or later (the "GPL")
- *    http://www.gnu.org/licenses/gpl.html
- *
- *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
- *    http://www.gnu.org/licenses/lgpl.html
- *
- *  - Mozilla Public License Version 1.1 or later (the "MPL")
- *    http://www.mozilla.org/MPL/MPL-1.1.html
- *
- * == END LICENSE ==
- *
- * This is the integration file for JavaScript.
- *
- * It defines the FCKeditor class that can be used to create editor
- * instances in a HTML page in the client side. For server side
- * operations, use the specific integration system.
+﻿/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2005 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * "Support Open Source software. What about a donation today?"
+ * 
+ * File Name: fckeditor.js
+ * 	This is the integration file for JavaScript.
+ * 
+ * 	It defines the FCKeditor class that can be used to create editor
+ * 	instances in a HTML page in the client side. For server side
+ * 	operations, use the specific integration system.
+ * 
+ * File Authors:
+ * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
  */
 
 // FCKeditor Class
@@ -46,41 +42,31 @@ var FCKeditor = function( instanceName, width, height, toolbarSet, value )
 	this.OnError		= null ;	// function( source, errorNumber, errorDescription )
 }
 
-FCKeditor.prototype.Version			= '2.4.3' ;
-FCKeditor.prototype.VersionBuild	= '15657' ;
-
 FCKeditor.prototype.Create = function()
-{
-	document.write( this.CreateHtml() ) ;
-}
-
-FCKeditor.prototype.CreateHtml = function()
 {
 	// Check for errors
 	if ( !this.InstanceName || this.InstanceName.length == 0 )
 	{
 		this._ThrowError( 701, 'You must specify an instance name.' ) ;
-		return '' ;
+		return ;
 	}
 
-	var sHtml = '<div>' ;
+	document.write( '<div>' ) ;
 
 	if ( !this.CheckBrowser || this._IsCompatibleBrowser() )
 	{
-		sHtml += '<input type="hidden" id="' + this.InstanceName + '" name="' + this.InstanceName + '" value="' + this._HTMLEncode( this.Value ) + '" style="display:none" />' ;
-		sHtml += this._GetConfigHtml() ;
-		sHtml += this._GetIFrameHtml() ;
+		document.write( '<input type="hidden" id="' + this.InstanceName + '" name="' + this.InstanceName + '" value="' + this._HTMLEncode( this.Value ) + '" style="display:none" />' ) ;
+		document.write( this._GetConfigHtml() ) ;
+		document.write( this._GetIFrameHtml() ) ;
 	}
 	else
 	{
 		var sWidth  = this.Width.toString().indexOf('%')  > 0 ? this.Width  : this.Width  + 'px' ;
 		var sHeight = this.Height.toString().indexOf('%') > 0 ? this.Height : this.Height + 'px' ;
-		sHtml += '<textarea name="' + this.InstanceName + '" rows="4" cols="40" style="width:' + sWidth + ';height:' + sHeight + '">' + this._HTMLEncode( this.Value ) + '<\/textarea>' ;
+		document.write('<textarea name="' + this.InstanceName + '" rows="4" cols="40" style="WIDTH: ' + sWidth + '; HEIGHT: ' + sHeight + '">' + this._HTMLEncode( this.Value ) + '<\/textarea>') ;
 	}
 
-	sHtml += '</div>' ;
-
-	return sHtml ;
+	document.write( '</div>' ) ;
 }
 
 FCKeditor.prototype.ReplaceTextarea = function()
@@ -93,11 +79,11 @@ FCKeditor.prototype.ReplaceTextarea = function()
 		var i = 0;
 		while ( oTextarea || i == 0 )
 		{
-			if ( oTextarea && oTextarea.tagName.toLowerCase() == 'textarea' )
+			if ( oTextarea && oTextarea.tagName == 'TEXTAREA' )
 				break ;
 			oTextarea = colElementsByName[i++] ;
 		}
-
+		
 		if ( !oTextarea )
 		{
 			alert( 'Error: The TEXTAREA with id or name set to "' + this.InstanceName + '" was not found' ) ;
@@ -129,7 +115,7 @@ FCKeditor.prototype._GetConfigHtml = function()
 	for ( var o in this.Config )
 	{
 		if ( sConfig.length > 0 ) sConfig += '&amp;' ;
-		sConfig += encodeURIComponent( o ) + '=' + encodeURIComponent( this.Config[o] ) ;
+		sConfig += escape(o) + '=' + escape( this.Config[o] ) ;
 	}
 
 	return '<input type="hidden" id="' + this.InstanceName + '___Config" value="' + sConfig + '" style="display:none" />' ;
@@ -137,24 +123,42 @@ FCKeditor.prototype._GetConfigHtml = function()
 
 FCKeditor.prototype._GetIFrameHtml = function()
 {
-	var sFile = 'fckeditor.html' ;
+	var sFile = (/fcksource=true/i).test( window.top.location.search ) ? 'fckeditor.original.html' : 'fckeditor.html' ;
 
-	try
-	{
-		if ( (/fcksource=true/i).test( window.top.location.search ) )
-			sFile = 'fckeditor.original.html' ;
-	}
-	catch (e) { /* Ignore it. Much probably we are inside a FRAME where the "top" is in another domain (security error). */ }
+	var sLink = this.BasePath + 'editor/' + sFile + '?InstanceName=' + this.InstanceName ;
+	if (this.ToolbarSet) sLink += '&Toolbar=' + this.ToolbarSet ;
 
-	var sLink = this.BasePath + 'editor/' + sFile + '?InstanceName=' + encodeURIComponent( this.InstanceName ) ;
-	if (this.ToolbarSet) sLink += '&amp;Toolbar=' + this.ToolbarSet ;
-
-	return '<iframe id="' + this.InstanceName + '___Frame" src="' + sLink + '" width="' + this.Width + '" height="' + this.Height + '" frameborder="0" scrolling="no"></iframe>' ;
+	return '<iframe id="' + this.InstanceName + '___Frame" src="' + sLink + '" width="' + this.Width + '" height="' + this.Height + '" frameborder="no" scrolling="no"></iframe>' ;
 }
 
 FCKeditor.prototype._IsCompatibleBrowser = function()
 {
-	return FCKeditor_IsCompatibleBrowser( this.EnableSafari, this.EnableOpera ) ;
+	var sAgent = navigator.userAgent.toLowerCase() ;
+	
+	// Internet Explorer
+	if ( sAgent.indexOf("msie") != -1 && sAgent.indexOf("mac") == -1 && sAgent.indexOf("opera") == -1 )
+	{
+		var sBrowserVersion = navigator.appVersion.match(/MSIE (.\..)/)[1] ;
+		return ( sBrowserVersion >= 5.5 ) ;
+	}
+	
+	// Gecko
+	if ( navigator.product == "Gecko" && navigator.productSub >= 20030210 )
+		return true ;
+	
+	// Opera
+	if ( this.EnableOpera )
+	{
+		var aMatch = sAgent.match( /^opera\/(\d+\.\d+)/ ) ;
+		if ( aMatch && aMatch[1] >= 9.0 )
+			return true ;
+	}
+	
+	// Safari
+	if ( this.EnableSafari && sAgent.indexOf( 'safari' ) != -1 )
+		return ( sAgent.match( /safari\/(\d+)/ )[1] >= 312 ) ;	// Build must be at least 312 (1.3)
+	
+	return false ;
 }
 
 FCKeditor.prototype._ThrowError = function( errorNumber, errorDescription )
@@ -178,37 +182,11 @@ FCKeditor.prototype._HTMLEncode = function( text )
 	if ( typeof( text ) != "string" )
 		text = text.toString() ;
 
-	text = text.replace(
-		/&/g, "&amp;").replace(
-		/"/g, "&quot;").replace(
-		/</g, "&lt;").replace(
-		/>/g, "&gt;") ;
+	text = text.replace(/&/g, "&amp;") ;
+	text = text.replace(/"/g, "&quot;") ;
+	text = text.replace(/</g, "&lt;") ;
+	text = text.replace(/>/g, "&gt;") ;
+	text = text.replace(/'/g, "&#39;") ;
 
 	return text ;
-}
-
-function FCKeditor_IsCompatibleBrowser( enableSafari, enableOpera )
-{
-	var sAgent = navigator.userAgent.toLowerCase() ;
-
-	// Internet Explorer
-	if ( sAgent.indexOf("msie") != -1 && sAgent.indexOf("mac") == -1 && sAgent.indexOf("opera") == -1 )
-	{
-		var sBrowserVersion = navigator.appVersion.match(/MSIE (.\..)/)[1] ;
-		return ( sBrowserVersion >= 5.5 ) ;
-	}
-
-	// Gecko (Opera 9 tries to behave like Gecko at this point).
-	if ( navigator.product == "Gecko" && navigator.productSub >= 20030210 && !( typeof(opera) == 'object' && opera.postError ) )
-		return true ;
-
-	// Opera
-	if ( enableOpera && sAgent.indexOf( 'opera' ) == 0 && parseInt( navigator.appVersion, 10 ) >= 9 )
-			return true ;
-
-	// Safari
-	if ( enableSafari && sAgent.indexOf( 'safari' ) != -1 )
-		return ( sAgent.match( /safari\/(\d+)/ )[1] >= 312 ) ;	// Build must be at least 312 (1.3)
-
-	return false ;
 }
