ad55c84452c5dcff17d6d11c117a375af95ef5e1
Pierre-Alain Mignot <mignot>
2008-04-29 09:17:38 +0000

    Type:O Descr:defaultlang prend comme valeur la globale languages / modif de cette globale pour valeur dans la langue éponyme
    git-svn-id: https://subversion.renater.fr/lodel/branches/version_0_8-bugfixes-branch@4285 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/auth.php
+++ lodel/scripts/auth.php
@@ -385,7 +385,8 @@ else {
 	else {	setcookie('language', $GLOBALS['lang']); }
 }
 // tableaux des langues disponibles
-$context['defaultlang'] = array(0=>'FR', 1=>'EN', 2=>'ES', 3=>'DE', 4=>'IT', 5=>'PT');
+require_once 'lang.php';
+$context['defaultlang'] = $GLOBALS['languages'];
 // accès à la langue dans les templates
 $context['sitelang'] = $GLOBALS['lang'];
 //sommes nous en maintenance ?
