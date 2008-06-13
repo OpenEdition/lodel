9921e81d6a45e2e14eadef2ea29cd1b62abb2ec5
Pierre-Alain Mignot <mignot>
2008-06-13 13:25:05 +0000

    Type:Bm Descr:bug du refresh manager template principal
    git-svn-id: https://subversion.renater.fr/lodel/branches/version_0_8-bugfixes-branch@4357 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/view.php
+++ lodel/scripts/view.php
@@ -369,14 +369,15 @@ $code .= $content . '
 				$this->_error("CACHE directory is not writeable.", __FUNCTION__);
 			}
 			$tmpFileName = "./CACHE/require_caching/".md5(uniqid(mt_rand(0, 999999999999).mt_rand(0, 999999999999), true));
-			file_put_contents($tmpFileName, $content, LOCK_EX);
+			if(FALSE === file_put_contents($tmpFileName, $content, LOCK_EX))
+				$this->_error("Error while writing CACHE required file.", __FUNCTION__);
 			ob_start();
-			require $tmpFileName;
+			$refresh = require $tmpFileName;
 			$ret = ob_get_contents();
 			ob_end_clean();
 			unlink($tmpFileName);
-			if('refresh' == $ret) {
-				return $ret;
+			if('refresh' == $refresh) {
+				return $refresh;
 			} 
 			$content = $ret;
 			$ret = null;
