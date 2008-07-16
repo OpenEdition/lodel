8ad946672c02979f36475ba838b7ae902608a6d8
Pierre-Alain Mignot <mignot>
2008-07-16 08:44:53 +0000

    Type:O Descr:essai ajax OK / optimisation et sécurisation du script
    git-svn-id: https://subversion.renater.fr/lodel/trunk@4461 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- share/ajax/dragndrop.php
+++ share/ajax/dragndrop.php
@@ -1,29 +1,32 @@
 <?php
 $tabIds = explode(',', $_POST['tabids']);
 
-if(!preg_match("/^[a-z0-9\-]+$/", $_POST['site']) || in_array($_POST['site'], array('lodel-0.8', 'share-0.8', 'lodeladmin-0.8', 'lodel', 'share', 'lodeladmin'))) {
+if(!preg_match("/^[a-z0-9\-]+$/", $_POST['site']) || 
+	in_array($_POST['site'], array('lodel-0.8', 'share-0.8', 'lodeladmin-0.8', 'lodel', 'share', 'lodeladmin')) || 
+	!is_dir('../../'.$_POST['site'])) {
 	// tentative ?
 	echo 'error';
 	exit();
 } else {
 	$GLOBALS['site'] = $_POST['site'];
-	if(!is_dir('../../'.$GLOBALS['site'])) {
-		echo 'error';
-		exit();
-	}
 }
 $GLOBALS['ajax'] = true;
 require '../../'.$GLOBALS['site'].'/siteconfig.php';
 require '../'.$home.'connect.php';
 require '../'.$home.'auth.php';
+// accès seulement aux personnes autorisées
+// pas de log de l'url dans la base
+$GLOBALS['norecordurl'] = true;
 authenticate(LEVEL_VISITOR);
+if(!$lodeluser['visitor'])
+	return;
+
 $table = lq("#_TP_entities");
 foreach($tabIds as $k=>$v) {
-	$key = intval($k);
+	$key = (0 === $k) ? 1 : intval($k)+1;
 	$v = intval($v);
 	if($key>0 && $v>0) {
-		$key++;
-		$db->execute(lq("UPDATE {$table} SET rank = '{$key}' WHERE id='{$v}'")) or dberror();
+		$db->execute("UPDATE {$table} SET rank = '{$key}' WHERE id='{$v}'") or dberror();
 	}
 }
 require '../'.$home.'cachefunc.php';
