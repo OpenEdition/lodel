a090158bbf78bdcfb1fb9fd9dfdf6166ed7a3533
Pierre-Alain Mignot <mignot>
2009-08-21 10:05:33 +0000

    Type:O Descr:amélioration gestion des plugins
    git-svn-id: https://subversion.renater.fr/lodel/branches/version_0_9-bugfixes-branch@4867 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/plugins.php
+++ lodel/scripts/plugins.php
@@ -50,12 +50,9 @@
  * @since Fichier ajouté depuis la version 0.9
  */
 
-
 /**
  * Base class for plugins using class as hook
- * Forces the definition of static methods enableAction and disableAction
  * Classe servant de base pour les plugins utilisant les hook de type class
- * Force la définition des méthodes statiques enableAction et disableAction
  *
  * @author Pierre-Alain Mignot
  * @copyright 2001-2002, Ghislain Picard, Marin Dacos
@@ -67,8 +64,79 @@
  * @copyright 2008, Marin Dacos, Bruno Cénou, Pierre-Alain Mignot, Inès Secondat de Montesquieu, Jean-François Rivière
  * @copyright 2009, Marin Dacos, Bruno Cénou, Pierre-Alain Mignot, Inès Secondat de Montesquieu, Jean-François Rivière
  */
-interface MainPlugins
+abstract class Plugins
 {
+	/**
+	 * @var array
+	 */
+	static private $_instances = array();
+	/**
+	 * @var array
+	 */
+	static private $_triggers = array('preview','postview','preedit','postedit','prelogin','postlogin','preauth','postauth');
+	/**
+	 * @var array
+	 */
+	protected $_config = array();
+
+	/**
+	 * Constructor
+	 * Set the config vars
+	 *
+	 * @param string $classname the class name of the calling plugin
+	 */
+	protected function __construct($classname)
+	{
+		$this->_config = C::get($classname.'.config', 'triggers');
+		if(false === $this->_config)
+		{
+			defined('INC_CONNECT') || include 'connect.php';
+			global $db;
+			$config = $db->GetOne(lq('SELECT config FROM #_TP_plugins WHERE name='.$db->quote($classname)));
+			if(false === $config)
+				trigger_error('ERROR: can not fetch config values for plugin '.$classname, E_USER_ERROR);
+			$this->_config = unserialize($config);
+		}
+	}
+
+	static public function get($plugin)
+	{
+		if(!isset(self::$_instances[$plugin]))
+		{
+			global $db;
+			$enabled = $db->GetOne(lq('SELECT status FROM #_TP_plugins WHERE name='.$db->quote($plugin)));
+			if(!$enabled) trigger_error('ERROR: sorry this plugin is not enabled, please contact your administrator', E_USER_ERROR);
+			self::$_instances[$plugin] = new $plugin($plugin);
+		}
+
+		return self::$_instances[$plugin];
+	}
+
+	static public function getTriggers()
+	{
+		return self::$_triggers;
+	}
+
+	/**
+	 * Returns the config vars
+	 *
+	 * @param string $classname the class name of the calling plugin
+	 */
+	public function getConfig()
+	{
+		return $this->_config;
+	}
+
+	/**
+	 * Compare the user rights against level passed by argument
+	 *
+	 * @param int $level the level to compare the user rights to
+	 */
+	protected function _checkRights($level)
+	{
+		return ((bool)(C::get('rights','lodeluser') > $level));
+	}
+
 	/**
 	 * Called when enabling a plugin
 	 * This method is abstract, it HAS to be defined in child class
@@ -76,7 +144,7 @@ interface MainPlugins
 	 * @param array $context the $context, by reference
 	 * @param array $error the error array, by reference
 	 */
-	static public function enableAction(&$context, &$error);
+	abstract public function enableAction(&$context, &$error);
 
 	/**
 	 * Called when disabling a plugin
@@ -85,55 +153,6 @@ interface MainPlugins
 	 * @param array $context the $context, by reference
 	 * @param array $error the error array, by reference
 	 */
-	static public function disableAction(&$context, &$error);
-}
-
-
-/**
- * Base class for plugins using class as hook
- * Classe servant de base pour les plugins utilisant les hook de type class
- *
- * @author Pierre-Alain Mignot
- * @copyright 2001-2002, Ghislain Picard, Marin Dacos
- * @copyright 2003, Ghislain Picard, Marin Dacos, Luc Santeramo, Nicolas Nutten, Anne Gentil-Beccot
- * @copyright 2004, Ghislain Picard, Marin Dacos, Luc Santeramo, Anne Gentil-Beccot, Bruno Cénou
- * @copyright 2005, Ghislain Picard, Marin Dacos, Luc Santeramo, Gautier Poupeau, Jean Lamy, Bruno Cénou
- * @copyright 2006, Marin Dacos, Luc Santeramo, Bruno Cénou, Jean Lamy, Mikaël Cixous, Sophie Malafosse
- * @copyright 2007, Marin Dacos, Bruno Cénou, Sophie Malafosse, Pierre-Alain Mignot
- * @copyright 2008, Marin Dacos, Bruno Cénou, Pierre-Alain Mignot, Inès Secondat de Montesquieu, Jean-François Rivière
- * @copyright 2009, Marin Dacos, Bruno Cénou, Pierre-Alain Mignot, Inès Secondat de Montesquieu, Jean-François Rivière
- */
-abstract class Plugins implements MainPlugins
-{
-	/**
-	 * @var array
-	 */
-	static protected $_config;
-	/**
-	 * @var array
-	 */
-	static public $triggers = array('preview','postview','preedit','postedit','prelogin','postlogin','preauth','postauth');
-
-	/**
-	 * Init the config var
-	 *
-	 * @param string $classname the class name of the calling plugin
-	 */
-	static public function init($classname)
-	{ // until PHP 5.3
-	//	self::$_config = C::get(get_called_class().'.config', 'triggers');
-		self::$_config = C::get($classname.'.config', 'triggers');
-		if(false === self::$_config) trigger_error('ERROR: cannot fetch config values for plugin '.$classname, E_USER_ERROR);
-	}
-
-	/**
-	 * Compare the user rights against level passed in argument
-	 *
-	 * @param int $level the level to compare the user rights to
-	 */
-	static protected function _checkRights($level)
-	{
-		return ((bool)(C::get('rights','lodeluser') > $level));
-	}
+	abstract public function disableAction(&$context, &$error);
 }
 ?>
