6f6515b8d35f99751091c632c5778513afbdc6fe
Bruno Cenou <bruno.cenou@openedition.org>
2010-03-24 09:27:30 +0000

    Type:A Descr:reset de la langue au logout
    git-svn-id: https://subversion.renater.fr/lodel/branches/version_0_9-bugfixes-branch@4979 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/src/lodel/admin/logout.php
+++ lodel/src/lodel/admin/logout.php
@@ -54,35 +54,37 @@ require 'siteconfig.php';
 
 try
 {
-    include 'auth.php';
-    authenticate();
-    
-    if(isset($_COOKIE[C::get('sessionname', 'cfg')]))
-    {
-        $name = addslashes($_COOKIE[C::get('sessionname', 'cfg')]);
-        $time = time()-1;
-        
-        $db->execute(lq("
-        UPDATE #_MTP_session 
-            SET expire2='$time' 
-            WHERE name='$name'")) 
-            or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
-        
-        $db->execute(lq("
-        DELETE FROM #_MTP_urlstack 
-            WHERE idsession='".C::get('idsession', 'lodeluser')."'")) 
-            or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
-            
-        setcookie(C::get('sessionname', 'cfg'), "", $time,C::get('urlroot', 'cfg'));
-    }
-    
-    // la norme ne supporte pas les chemins relatifs !! :/
-    if(C::get('url_retour'))
-        header('Location: '.C::get('url_retour'));
-    else
-    {
-        header('Location: '.('' !== SITEROOT ? SITEROOT : './'));
-    }
+	include 'auth.php';
+	authenticate();
+	$time = time()-1;
+
+	if(isset($_COOKIE[C::get('sessionname', 'cfg')]))
+	{
+		$name = addslashes($_COOKIE[C::get('sessionname', 'cfg')]);
+		
+		$db->execute(lq("
+			UPDATE #_MTP_session
+				SET expire2='$time'
+				WHERE name='$name'"))
+			or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
+		
+		$db->execute(lq("
+			DELETE FROM #_MTP_urlstack
+				WHERE idsession='".C::get('idsession', 'lodeluser')."'"))
+			or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
+		
+		setcookie(C::get('sessionname', 'cfg'), "", $time,C::get('urlroot', 'cfg'));
+	}
+
+	@setcookie('language', '', $time, C::get('urlroot', 'cfg'));
+	
+	// la norme ne supporte pas les chemins relatifs !! :/
+	if(C::get('url_retour'))
+		header('Location: '.C::get('url_retour'));
+	else
+	{
+		header('Location: '.('' !== SITEROOT ? SITEROOT : './'));
+	}
 }
 catch(LodelException $e)
 {
