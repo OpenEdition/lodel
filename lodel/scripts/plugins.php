369a6ed8f021627bbdca99be746f1fa218ebc46e
Bruno Cenou <bruno.cenou@openedition.org>
2010-03-29 09:07:40 +0000

    Type:A Descr:doirts supérieur ou égaux
    git-svn-id: https://subversion.renater.fr/lodel/branches/version_0_9-bugfixes-branch@4989 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/plugins.php
+++ lodel/scripts/plugins.php
@@ -144,7 +144,7 @@ abstract class Plugins
 	 */
 	protected function _checkRights($level)
 	{
-		return ((bool)(C::get('rights','lodeluser') > $level));
+		return ((bool)(C::get('rights','lodeluser') >= $level));
 	}
 
 	/**
