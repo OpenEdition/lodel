d2e79d66fc6010645eded3d1ee94fa78192b8c52
Bruno Cenou <bruno.cenou@openedition.org>
2010-06-09 13:39:32 +0000

    Type: Bm Descr: Corrections pour E_NOTICE
    git-svn-id: https://subversion.renater.fr/lodel/branches/version_0_9-bugfixes-branch@5054 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic/class.data.php
+++ lodel/scripts/logic/class.data.php
@@ -904,6 +904,7 @@ class DataLogic
 	{
 		$archive = @$_FILES['archive']['tmp_name'];
 		$context['error_upload'] = @$_FILES['archive']['error'];
+		$file = '' ;
 		if (!$context['error_upload'] && $archive && $archive != 'none' && is_uploaded_file($archive)) { // Le fichier a été uploadé
 			$file = $_FILES['archive']['name'];
 			if (!preg_match("/^".$this->fileRegexp."$/", $file)) {
@@ -2244,7 +2245,7 @@ class DataLogic
 		foreach($context['data'] as $table=>$fields) {
 			$tablefieldgroup = isset($fields['dropped']['tablefieldgroup']) ? $fields['dropped']['tablefieldgroup'] : array();
 			unset($fields['dropped']['tablefieldgroup']);
-			$flipped = is_array($fields['dropped']) ? array_flip($fields['dropped']) : array();
+			$flipped = !empty($fields['dropped']) && is_array($fields['dropped']) ? array_flip($fields['dropped']) : array();
 			if(!empty($fields['added']) && is_array($fields['added'])) {
 				foreach($fields['added'] as $field=>$equivalent) {
 					$fieldType = multidimArrayLocate($this->_xmlStruct[$table], $field);
