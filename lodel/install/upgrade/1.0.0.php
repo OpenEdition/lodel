26092c8a309df56cd8582ac472575b9d67d5629e
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2011-03-15 13:10:34 +0000

    Type:A Descr:Ajout du script de migration vers la 1.0
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5184 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/install/upgrade/1.0.0.php
@@ -0,0 +1,151 @@
+<?php
+require 'siteconfig.php';
+require 'connect.php';
+require 'auth.php';
+
+authenticate(LEVEL_ADMINLODEL);
+
+$sql = array();
+
+foreach( array('entrytypes', 'persontypes', 'internalstyles', 'characterstyles', 'tablefields') as $table ) {
+        $sql[] = "ALTER TABLE #_TP_$table ADD COLUMN `otx` tinytext NOT NULL AFTER `rank`";
+}
+
+$sql[] = "ALTER TABLE #_TP_$table ADD COLUMN `mask` text NOT NULL AFTER `comment`";
+
+$correspondances = array(
+	"motsclesfr" 	=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='keyword']",
+	"motsclesen" 	=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='keyword']",
+	"motscleses" 	=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='keyword']",
+	"motsclesde" 	=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='keyword']",
+	"chrono"		=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='chronological']",
+	"theme"			=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='subject']",
+	"license"		=> "/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:availability",
+	"geographie"	=> "/tei:TEI/tei:teiHeader/tei:profileDesc/tei:textClass/tei:keywords[@scheme='geographical']"
+);
+
+$sql[] = lq("update #_TP_entrytypes set otx=''");
+
+foreach($correspondances as $k => $v)
+{
+	$sql[] = lq("UPDATE #_TP_entrytypes SET otx=".$db->quote($v)." WHERE type=".$db->quote($k));
+}
+
+$correspondances = array(
+	"auteur"				=> "/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:author",
+	"traducteur" 			=> "/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:editor[@role='translator']",
+	"auteuroeuvre" 			=> "/tei:TEI/tei:text/tei:front/tei:div[@type='review']/tei:p[@rend='review-author']",
+	"editeurscientifique"	=> "/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:editor[not(@role)]"
+);
+
+$sql[] = lq("update #_TP_persontypes set otx=''");
+
+foreach($correspondances as $k => $v)
+{
+	$sql[] = lq("UPDATE #_TP_persontypes SET otx=".$db->quote($v)." WHERE type=".$db->quote($k));
+}
+
+$sql[] = lq("update #_TP_internalstyles set otx=''");
+
+$correspondances = array(
+	"citation" 					=>	"//*[@rend='quotation']",
+	"quotations"				=>	"//*[@rend='reference']",
+	"citationbis"				=>	"//*[@rend='quotation2']",
+	"citationter"				=>	"//*[@rend='quotation3']",
+	"titreillustration"			=>	"//*[@rend='figure-title']",
+	"legendeillustration"		=>	"//*[@rend='figure-legend']",
+	"code"						=>	"//*[@rend='code']",
+	"question"                  =>	"//*[@rend='question']",
+	"reponse"					=>	"//*[@rend='answer']",
+	"separateur"				=>	"//*[@rend='break']",
+	"section1"					=>	"//tei:head[@subtype='level1']",
+	"section3"					=>	"//tei:head[@subtype='level3']",
+	"section4"					=>	"//tei:head[@subtype='level4']",
+	"section5"					=>	"//tei:head[@subtype='level5']",
+	"section6"					=>	"//tei:head[@subtype='level6']",
+	"paragraphesansretrait"		=>	"//*[@rend='noindent']",
+	"epigraphe"					=>	"//*[@rend='epigraph']",
+	"section2"					=>	"//tei:head[@subtype='level2']",
+	"quotation"					=>	"//*[@rend='quotation']",
+	"bibliographiereference"	=>	"//*[@rend='bibliographicreference']",
+	"creditillustration,crditillustration,creditsillustration,crditsillustration"
+								=>	"//*[@rend='figure-license']",
+	"remerciements,acknowledgment"
+								=>	"/tei:TEI/tei:text/tei:front/tei:div[@type='ack']",
+	"encadre"					=>  "//*[@rend='box']"
+// 	"pigraphe"					=>	"body:epigraph",
+// 	"sparateur"					=>	"text:break",
+// 	"puces"						=>	"text:item",
+
+);
+
+foreach($correspondances as $k => $v)
+{
+	$sql[] = lq("UPDATE #_TP_internalstyles SET otx=".$db->quote($v)." WHERE style=".$db->quote($k));
+}
+
+$sql[] = lq("update #_TP_tablefields set otx=''");
+
+$correspondances = array(
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:title[@type='main']"	=> array('titre', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:title[@type='sup']"	=> array('surtitre', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:title[@type='sub']"	=> array('soustitre', 'textes'),
+	"/tei:TEI/tei:text/tei:body/child::*"										=> array('texte', 'textes'),
+	"/tei:TEI/tei:text/tei:back/tei:div[@type='appendix']"						=> array('annexe', 'textes'),
+	"/tei:TEI/tei:text/tei:back/tei:div[@type='bibliography']"					=> array('bibliographie', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:date"			=> array('datepubli', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:sourceDesc/tei:biblFull/tei:publicationStmt/tei:date" 
+																				=> array('datepublipapier', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:sourceDesc/tei:biblFull/tei:notesStmt/tei:note[@type='bibl']" 
+																				=> array('noticebiblio', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:sourceDesc/tei:biblFull/tei:publicationStmt/tei:idno[@type='pp']"
+																				=> array('pagination', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:profileDesc/tei:langUsage/tei:language" 		=> array('langue', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:div[@type='correction']" 					=> array('addendum', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:note[@resp='editor']/tei:p" 				=> array('ndlr', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:div[@type='dedication']" 					=> array('dedicace', 'textes'),
+	"//tei:roleName[@type='honorific']" 										=> array('prefix', 'entities_auteurs'),
+	"//tei:orgName"																=> array('affiliation', 'entities_auteurs'),
+ 	"//tei:roleName[@type='function']" 											=> array('fonction', 'entities_auteurs'),
+	"//tei:affiliation" 														=> array('description', 'entities_auteurs'),
+	"//tei:email" 																=> array('courriel', 'entities_auteurs'),
+	"//tei:ref[@type='website']" 												=> array('site', 'entities_auteurs'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:title[@type='alt']"	=> array('altertitre', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:div[@type='abstract']" 					=> array('resume', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:div[@type='review']/tei:p[@rend='review-title']" 			=> array('titreoeuvre', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:div[@type='review']/tei:p[@rend='review-bibliography']" 	=> array('noticebibliooeuvre', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:div[@type='review']/tei:p[@rend='review-date']" 			=> array('datepublicationoeuvre', 'textes'),
+	"/tei:TEI/tei:text/tei:front/tei:note[@resp='author']/tei:p" 								=> array('ndla', 'textes'),
+	"/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:idno[@type='documentnumber']" 	=> array('numerodocument', 'textes'),
+//	"/tei:TEI/tei:text/tei:body/tei:*/tei:note[@place='end']" 									=> array('notefin', 'textes'),
+//	"/tei:TEI/tei:text/tei:body/tei:*/tei:note[@place='foot']"					=> array('notesbaspage', 'textes'),
+//	'//tei:roleName' => array('role', 'entities_auteurs'),
+// 	'header:keywords-fr' => array('motsclesfr', 'textes'),
+// 	'lodel:author-lastname' => array('nomfamille', 'auteurs'),
+// 	'lodel:author-firstname' => array('prenom', 'auteurs'),
+// 	'' => array('editeurscientifique', 'textes'),
+);
+
+foreach($correspondances as $k => $v)
+{
+	$sql[] = lq("UPDATE #_TP_tablefields SET otx=".$db->quote($k)." WHERE name=".$db->quote(current($v))." AND class=".$db->quote(end($v)));
+}
+
+
+echo '<pre>';
+
+print_r($sql);
+
+echo '</pre>';
+
+if(!isset($_GET['valid']))
+	echo '<p>Si les requêtes vous semblent bonnes, et que vous avez fait un backup/dump de la base de données, vous pouvez les exécuter en cliquant <a href="./to10.php?valid=1">ici</a>.</p>';
+else
+{
+    foreach($sql as $s)
+    {
+            $db->execute($s) or die($db->errormsg());
+    }
+
+    echo "migration terminée";
+}
