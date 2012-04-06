5d4d38f0bfbb8c96fec0b1809dfebb8704fca4c1
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2012-04-06 08:48:19 +0000

    Type:A Descr:Ajout du script de mise à jour pour les attributs des entries
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5456 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/install/upgrade/1.0.1.php
@@ -0,0 +1,32 @@
+<?php
+
+/* Ajout des tables pour les attributs d'entrées d'index */
+
+define('backoffice-lodeladmin', true);
+
+require_once 'lodelconfig.php';
+require_once 'lodel/scripts/context.php';
+C::setCfg($cfg);
+
+require_once 'lodel/scripts/connect.php';
+require_once 'lodel/scripts/auth.php';
+
+/* Vérification que nous sommes en php-cli, sinon nous vérifions les droits*/
+if (!array_key_exists('SHELL', $_ENV))
+authenticate(LEVEL_ADMINLODEL);
+
+global $db;
+$sites = $db->Execute(lq("
+            SELECT name, status 
+                FROM #_MTP_sites 
+                WHERE status > 0")) 
+or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
+
+
+foreach($sites as $site){
+	$db->SelectDB(DATABASE . "_{$site['name']}");
+	$classes = $db->Execute(lq('SELECT * FROM #_TP_classes WHERE classtype = "entries";'));
+	foreach($classes as $class){
+		$db->execute(lq ("CREATE TABLE IF NOT EXISTS #_TP_entities_". $class['class'] ." ( idrelation INTEGER UNSIGNED UNIQUE, KEY index_idrelation (idrelation) )"));
+	}
+}
