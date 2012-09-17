43ef2451d71556a8fa6ea804d102e5e601e7be65
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2012-09-17 12:43:43 +0000

    Type:F Descr:Possibilité de traiter tous les sites hébergés
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5541 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/install/scripts/delete_old_sources.php
+++ lodel/install/scripts/delete_old_sources.php
@@ -13,27 +13,35 @@ require_once 'lodel/scripts/auth.php';
 
 global $db;
 
-if(!isset($argv[1])) die("Forgotten site name");
-if(!preg_match("/^\w+$/", $argv[1])) die("Site name incorrect");
-
-$site = $argv[1];
-
-$db->SelectDB(DATABASE . "_{$site}");
-
-$db_ids = $db->GetArray(lq("SELECT id FROM entities;"));
-
-$ids = array();
-foreach($db_ids as $id){
-	$ids[] = $id['id'];
+if(isset($argv[1])){
+	if(!preg_match("/^\w+$/", $argv[1])) die("Site name incorrect");
+	$sites = array($argv[1]);
+}else{
+	$site_array = $db->GetArray(lq('SELECT name FROM sites WHERE status > 0'));
+	foreach($site_array as $new_site){
+		$sites[] = $new_site['name'];
+	}
 }
 
-$sources = new DirectoryIterator($site . DIRECTORY_SEPARATOR . "lodel" . DIRECTORY_SEPARATOR . "sources");
-foreach($sources as $source){
-	if(preg_match("/^entite(\-\w+)?\-(\d+)\./", $source->getFilename(), $matches)){
-		if(!in_array($matches[2], $ids)){
-			echo "Deleting {$source->getPathName()}\n";
-			unlink($source->getPathName());
+foreach($sites as $site){
+
+	$db->SelectDB(DATABASE . "_{$site}");
+
+	$db_ids = $db->GetArray(lq("SELECT id FROM entities;"));
+	
+	$ids = array();
+	foreach($db_ids as $id){
+		$ids[] = $id['id'];
+	}
+	
+	$sources = new DirectoryIterator($site . DIRECTORY_SEPARATOR . "lodel" . DIRECTORY_SEPARATOR . "sources");
+	foreach($sources as $source){
+		if(preg_match("/^entite(\-\w+)?\-(\d+)\./", $source->getFilename(), $matches)){
+			if(!in_array($matches[2], $ids)){
+				echo "Deleting {$source->getPathName()}\n";
+				unlink($source->getPathName());
+			}
+			
 		}
-		
 	}
 }
