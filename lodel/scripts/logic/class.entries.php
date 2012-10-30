08a9d42a8d9a1b023f6981235f5579d8b3620707
Arnaud Cordier <arnaud.cordier@openedition.org>
2012-10-30 15:32:11 +0000

    Type:Bm Descr:Ne pas planter quand il n'y a pas de screenname ni de g_name
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5576 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/logic/class.entries.php
+++ lodel/scripts/logic/class.entries.php
@@ -238,8 +238,8 @@ class EntriesLogic extends GenericLogic
 			$name = str_replace(' ', '_', $g_name->g_name);
 			$context[$name] = $g_name->name;
 		}
-		if (!isset($context['screen_name']) && isset($context['index_key']))
-			$context['screen_name'] = $context['index_key'];
+		if (!isset($context['screen_name']))
+			$context['screen_name'] = isset($context['index_key']) ? $context['index_key'] : "";
 
 		$this->_populateContext ($votype, $context['type']);
 		return '_ok';
