00e6d37dd2cfa7004ea0a55e2a04dca8aa596d09
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2012-10-31 08:57:23 +0000

    Type:F Descr:Ajout d'un script permettant d'ajouter un hook à un champ d'une classe donnée
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5581 9f73b02b-de1c-0410-bbc7-d36e22b4c400

---
+++ lodel/install/scripts/apply_hook.php
@@ -0,0 +1,38 @@
+<?php
+
+if( php_sapi_name() != "cli") die();
+
+if(!( isset($argv[1]) || isset($argv[2]) || isset($argv[3]) ) ) die("Usage php apply_hook.php [class] [field] [hook]");
+
+list($command, $class, $field, $hook) = $argv;
+
+define('backoffice-lodeladmin', true);
+
+require_once 'lodelconfig.php';
+require_once 'lodel/scripts/context.php';
+C::setCfg($cfg);
+
+require_once 'lodel/scripts/connect.php';
+require_once 'lodel/scripts/auth.php';
+
+global $db;
+
+$site_array = $db->GetArray(lq('SELECT name FROM sites WHERE status > 0'));
+foreach($site_array as $site){
+
+	$db->SelectDB(DATABASE . "_{$site['name']}");
+
+	if($tablefields = $db->GetArray(lq("SELECT id, name, class, editionhooks FROM tablefields WHERE class = " . $db->Quote($class) . " AND name = " . $db->Quote($field))))
+	{
+	
+		foreach($tablefields as $tablefield){
+			$hooks = array_filter(explode(',', $tablefield['editionhooks']));
+			$hooks[] = $hook;
+			
+			$db->Execute(lq("UPDATE tablefields SET editionhooks = " . $db->Quote(implode(",",$hooks)) . " WHERE id = " . $db->Quote($tablefield['id'])));
+			
+			implode($hooks);
+		}
+	}
+}
+
