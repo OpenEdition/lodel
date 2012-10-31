4f5c7b4b56a7670fd832bb905078c2f6843a60ae
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2012-10-31 08:22:17 +0000

    Type:Bm Descr:Préserver la transparence des images
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5579 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/func.php
+++ lodel/scripts/func.php
@@ -1469,7 +1469,7 @@ function thumbnail($path, $width = null, $height = null)
 	$image->enlarge_smaller_images = false;
 	$image->preserve_time = true;
 	
-	if (!$image->resize($width, $height, ZEBRA_IMAGE_NOT_BOXED)) {
+	if (!$image->resize($width, $height, ZEBRA_IMAGE_NOT_BOXED, -1)) {
 		return $path;
 	}
 	else
