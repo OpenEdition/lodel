8d27f075f007af9cda9c37f06d3fec5fde6488b5
Arnaud Cordier <arnaud.cordier@openedition.org>
2013-04-09 09:22:49 +0000

    Type:Bm Descr:Vider le cache au moment où on le demande, pas au prochain hit
    git-svn-id: https://subversion.renater.fr/lodel/trunk@5677 9f73b02b-de1c-0410-bbc7-d36e22b4c400

--- lodel/scripts/cachefunc.php
+++ lodel/scripts/cachefunc.php
@@ -100,7 +100,7 @@ function getCacheIdFromId( $id ){
 	$cache_config = cache_get_config();
 	$site  = C::get('site','cfg') ? C::get('site','cfg') : 'general';
 	$env   = defined('backoffice-lodelindex') ? 'lodelindex' : defined('backoffice-admin') ? 'admin' : defined('backoffice-edition') ? 'edition' : 'site' ;
-	if ($sessionsite === null)
+	if ($sessionsite === null || C::get('clearcache'))
 		$sessionsite = $cache->get( "session_{$site}", 0 );
 
 	return  ( isset($cache_config['prefix']) ? "{$cache_config['prefix']}_" : "" ) 
