899f04e4a13df8640c775f45ecfa3ead615d10bd
Driky <chatelain.cedric.pro@gmail.com>
2014-01-16 16:39:08 +0100

    Added function is_write_query

--- lodel/scripts/adodb/drivers/adodb-lodel.inc.php
+++ lodel/scripts/adodb/drivers/adodb-lodel.inc.php
@@ -23,6 +23,17 @@ if (! defined("_ADODB_LODEL_LAYER")) {
 // 			}
 			return parent::_query($sql,$inputarr);
 		}
+                
+                /**
+                * Determine the likelihood that this query could alter anything
+                * @param string query
+                * @return bool
+                */
+               static function is_write_query( $q ) {
+                       // Quick and dirty: only SELECT statements are considered read-only.
+                       $q = ltrim($q, "\r\n\t (");
+                       return !preg_match('/^(?:SELECT|SHOW|DESCRIBE|EXPLAIN)\s/i', $q);
+               }
 
 	}
 
