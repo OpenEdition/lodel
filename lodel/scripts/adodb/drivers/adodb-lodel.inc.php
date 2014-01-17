ffad241e0b1dc4f4966a2bd0d7f092093f5dd989
ArnAud Cordier <arnaud.cordier@openedition.org>
2014-01-17 16:53:52 +0100

    Dernière tentative d'écrire  un driver multibase dans une classe pas faite pour ça

--- lodel/scripts/adodb/drivers/adodb-lodel.inc.php
+++ lodel/scripts/adodb/drivers/adodb-lodel.inc.php
@@ -1,145 +1,126 @@
 <?php
 if (!defined('ADODB_DIR')) die('pas bon');
 
-$file = ADODB_DIR."/drivers/adodb-mysql.inc.php";
+$file = ADODB_DIR."/drivers/adodb-mysqli.inc.php";
 include_once($file);
 
 if (! defined("_ADODB_LODEL_LAYER")) {
 	define("_ADODB_LODEL_LAYER", 1 );
 
 	class ADODB_lodel extends ADODB_mysqli {
-                var $_connectionID = false;    /// The returned link identifier whenever a successful database connection is made.
-                var $_connectionIDR = false;    /// The returned link identifier whenever a successful database connection is made (read only).
-                
-            function _connect(Array $databases, $argDatabasename, $persist)/*($argHostname = NULL, $argUsername = NULL, $argPassword = NULL, $argDatabasename = NULL, $persist=false)*/
-            {
-                    if(!extension_loaded("mysqli")) {
-                           return null;
-                    }
+        var $_connectionID = false;    /// The returned link identifier whenever a successful database connection is made.
+        var $_connectionIDR = false;    /// The returned link identifier whenever a successful database connection is made (read only).
+        var $databases = false;
 
-                    $this->_connectionID = @mysqli_init();
-                    $this->_connectionIDR = @mysqli_init();
+        function Connect($argHostname = "", $argUsername = "", $argPassword = "", $argDatabaseName = "", $forceNew = false) 
+        {
+            $this->databases = ADODB_lodel::createDatabaseArray($argHostname, $argUsername, $argPassword, $argDatabaseName);
+            foreach (array('read', 'write') as $mode) {
+                foreach (array('host', 'user', 'password', 'database') as $var)
+                    $$var = $this->databases[$mode][0][$var];
+//                     error_log("$host, $user, $password, $database,".var_export("",true));
+                $ret = parent::connect($host, $user, $password, $database);
+                $this->databases[$mode][0]['_connectionID'] = $this->_connectionID;
+                error_log("CONNECTID: ".var_export($this->_connectionID, true));
+//                     error_log("A: ".var_export($ret,true));
+            }
+            return $ret;
+        }
 
-                    if (is_null($this->_connectionID || $this->_connectionIDR)) {
-                        // mysqli_init only fails if insufficient memory
-                        if ($this->debug) 
-                                       ADOConnection::outp("mysqli_init() failed : "  . $this->ErrorMsg());
-                        return false;
-                    }
-                    /*
-                    I suggest a simple fix which would enable adodb and mysqli driver to
-                    read connection options from the standard mysql configuration file
-                    /etc/my.cnf - "Bastien Duclaux" <bduclaux#yahoo.com>
-                    */
-                    foreach($this->optionFlags as $arr) {	
-                        mysqli_options($this->_connectionID,$arr[0],$arr[1]);
-                        mysqli_options($this->_connectionIDR,$arr[0],$arr[1]);
-                    }
+        function SelectDB($dbName) 
+        {
+error_log("SelectDB : ".var_export($this->_connectionID,true));
+        //      $this->_connectionID = $this->mysqli_resolve_link($this->_connectionID);
+            $this->database = $dbName;
+            $this->databaseName = $dbName; # obsolete, retained for compat with older adodb versions
 
-                    if ($persist && PHP_VERSION > 5.2 && strncmp($databases['write'][0]['dbhost'],'p:',2) != 0) $databases['write'][0]['dbhost'] = 'p:'.$databases['write'][0]['dbhost'];
-                    if ($persist && PHP_VERSION > 5.2 && strncmp($databases['read'][0]['dbhost'],'p:',2) != 0) $databases['read'][0]['dbhost'] = 'p:'.$databases['read'][0]['dbhost'];
+            if ($this->getHostConnectId('read')) {
+                $result = true;
 
-
-                    $ok = mysqli_real_connect($this->_connectionID,
-                                       $databases['write'][0]['dbhost'],
-                                       $databases['write'][0]['dbusername'],
-                                       $databases['write'][0]['dbpasswd'],
-                                       $argDatabasename,
-                                           $this->port,
-                                           $this->socket,
-                                           $this->clientFlags);
-                    $okr = mysqli_real_connect($this->_connectionID,
-                                       $databases['read'][0]['dbhost'],
-                                       $databases['read'][0]['dbusername'],
-                                       $databases['read'][0]['dbpasswd'],
-                                       $argDatabasename,
-                                           $this->port,
-                                           $this->socket,
-                                           $this->clientFlags);
-
-
-                    if ($ok && $okr) 
-                    {
-                        if ($argDatabasename)
-                        {  
-                            return $this->SelectDB($argDatabasename);
-                        }
-
-                        return true;
-
-                    } else {
-                        if ($this->debug) 
-                                ADOConnection::outp("Could't connect : "  . $this->ErrorMsg());
-                        $this->_connectionID = null;
-                        $this->_connectionIDR = null;
-
-                        return false;
-                    }
-                }
-	
-                // returns true or false
-                // How to force a persistent connection
-                function _pconnect(Array $databases, $argDatabasename)
+                $result = @mysqli_select_db($this->getHostConnectId('write'), $dbName);
+                $result = @mysqli_select_db($this->getHostConnectId('read'), $dbName);
+                if (!$result)
                 {
-                    return $this->_connect($databases, $argDatabasename, true);
+                    ADOConnection::outp("Select of database " . $dbName . " failed. " . $this->ErrorMsg());
                 }
+                return $result;
+            }
 
-                // When is this used? Close old connection first?
-                // In _connect(), check $this->forceNewConnect? 
-                function _nconnect($databases, $argDatabasename)
-                {
-                    $this->forceNewConnect = true;
-                    return $this->_connect($databases, $argDatabasename);
-                }
+            return false;
+        }
 
-                function SelectDB($dbName) 
-                {
-                //	    $this->_connectionID = $this->mysqli_resolve_link($this->_connectionID);
-                    $this->database = $dbName;
-                        $this->databaseName = $dbName; # obsolete, retained for compat with older adodb versions
+        function _query($sql,$inputarr=false) {
+//                     error_log("SQL: ".var_export($sql,true));
+            $success = false;
+            if (ADODB_lodel::is_write_query($sql)) {
+                $connectId = $this->getHostConnectId('write');
+                error_log("WRITE");
+            } else {
+                $connectId = $this->getHostConnectId('read');
+                error_log("READ");
+            }
 
-                    if ($this->_connectionID && $this->_connectionIDR) 
-                    {
-                        $result = true;
+            $this->_connectionID = $connectId;
+            $success = mysqli_query($sql, $connectId);
+            return $success;
+        }
 
-                        $result = @mysqli_select_db($this->_connectionID, $dbName);
-                        $result = @mysqli_select_db($this->_connectionIDR, $dbName);
-                        if (!$result)
-                        {
-                            ADOConnection::outp("Select of database " . $dbName . " failed. " . $this->ErrorMsg());
-                        }
-                        return $result;		
+        function getHostConnectId($mode) {
+            $connectId = false;
+            switch ($mode) {
+                case 'write':
+                    if (isset($this->databases['write'][0]['_connectionID']))
+                        $connectId = $this->databases['write'][0]['_connectionID'];
+                    break;
+                default:
+                    if (isset($this->databases['read'][0]['_connectionID']))
+                        $connectId = $this->databases['read'][0]['_connectionID'];
+            }
+            if (!$connectId)
+                $connectId = $this->_connectionID;
+
+            return $connectId;
+        }
+
+        /**
+        * Determine the likelihood that this query could alter anything
+        * @param string query
+        * @return bool
+        */
+        static function is_write_query( $q ) 
+        {
+            // Quick and dirty: only SELECT statements are considered read-only.
+            $q = ltrim($q, "\r\n\t (");
+            return !preg_match('/^(?:SELECT|SHOW|DESCRIBE|EXPLAIN)\s/i', $q);
+        }
+
+        static function createDatabaseArray($hosts, $user = NULL, $password = NULL, $database = NULL) {
+            if (!is_array($hosts)) {
+                $hosts = array(array('host'=>$hosts, 'user'=>$user, 'password'=>$password, 'database'=>$database, 'mode'=>'rw'));
+            } elseif (!is_array($hosts[0])) {
+                $hosts = array($hosts);
+            }
+
+            $databases = array('read' => array(), 'write' => array());
+            foreach ($hosts as $host) {
+                $hostmodes = array('read', 'write');
+                if (isset($host['mode'])) {
+                    switch ($host['mode']) {
+                        case 'r':
+                            unset($hostmodes['write']);
+                            break;
+                        case 'w';
+                            unset($hostmodes['read']);
+                            break;
                     }
-
-                    return false;	
                 }
+                foreach ($hostmodes as $mode)
+                    $databases[$mode][] = $host;
+            }
 
-                function _query($sql,$inputarr=false) {
-                    error_log("SQL: ".var_export($sql,true));
-                    $success = false;
-                    if($this->_connect($databases)){
-                        if(ADODB_lodel::is_write_query($sql))
-                        {
-                            $success = parent::_query($sql,$this->_connectionID);
-                        }else{
-                            $success = parent::_query($sql,$this->_connectionIDR);
-                        }
-                    }
-
-                    return $success;
-                }
-
-                /**
-                * Determine the likelihood that this query could alter anything
-                * @param string query
-                * @return bool
-                */
-                static function is_write_query( $q ) 
-                {
-                    // Quick and dirty: only SELECT statements are considered read-only.
-                    $q = ltrim($q, "\r\n\t (");
-                    return !preg_match('/^(?:SELECT|SHOW|DESCRIBE|EXPLAIN)\s/i', $q);
-                }
+//                     error_log("A: ".var_export($databases,true));
+            return $databases;
+        }
 
     }
 
