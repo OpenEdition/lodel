a4f3e8e1b437c8c75f262827348e0262d295403a
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2014-03-14 16:52:40 +0100

    Aucun intérêt d'imposer une version trop récente de PHP
    PHP 5.2 est largement suffisant.

--- lodeladmin/install.php
+++ lodeladmin/install.php
@@ -102,7 +102,7 @@ elseif (!empty($cfg['installoption']))
 //
 // Test the PHP version
 //
-if (!version_compare(PHP_VERSION, '5.4.0', '>=')) {
+if (!version_compare(PHP_VERSION, '5.2.0', '>=')) {
     $install->problem('version');
     exit;
 }
