d64e8f793947ef5988362331a9ff0ca68006a4d2
ArnAud Cordier <arnaud.cordier@openedition.org>
2014-05-22 15:47:59 +0200

    Harmonisation de l'indentation de oochargement.php

--- lodel/src/lodel/edition/oochargement.php
+++ lodel/src/lodel/edition/oochargement.php
@@ -109,7 +109,7 @@ try
 	$isFrame = ! (C::get('sortietei') || C::get('sortie')) && C::get('adminlodel', 'lodeluser');
 	$task = Logic::getLogic('tasks');
 
-    $file_cache_lifetime = C::get('timeout', 'cfg') ? C::get('timeout', 'cfg') : 3600;
+	$file_cache_lifetime = C::get('timeout', 'cfg') ? C::get('timeout', 'cfg') : 3600;
 
 	if($fileorigin == 'upload' && !empty($_FILES['file1'])) {
 		$file = $_FILES['file1'];
@@ -175,78 +175,78 @@ try
 		{
 			if($isFrame) printJavascript('window.parent.o.changeStep(2);');
 
-            extract_files_from_zip($source, $tmpdir);
+			extract_files_from_zip($source, $tmpdir);
 
-            $dir = opendir($tmpdir);
-            while ($file = readdir($dir)) {
-                if(is_file( $tmpdir . DIRECTORY_SEPARATOR . $file ) && preg_match( "/\.xml$/",$file) ){
-                    $contents = array();
-                    $teiContents = file_get_contents($tmpdir . DIRECTORY_SEPARATOR . $file);
-                    if(empty($context['idtype']))
-                    { // reload
-                        $context['idtype'] = $GLOBALS['db']->GetOne(lq('SELECT idtype FROM #_TP_entities WHERE id='.(int)$context['identity']));
-                    }
-                    try
-                    {
-                        $parser = new TEIParser($context['idtype']);
-                        $contents['contents'] = $parser->parse($teiContents, '', $tmpdir, $sourceoriginale);
-                    }
-                    catch(Exception $e)
-                    {
-            //          printErrors($parser->getLogs(), false);
-                        printErrors($e->getMessage(), true, $isFrame);
-                    }
+			$dir = opendir($tmpdir);
+			while ($file = readdir($dir)) {
+				if(is_file( $tmpdir . DIRECTORY_SEPARATOR . $file ) && preg_match( "/\.xml$/",$file) ){
+					$contents = array();
+					$teiContents = file_get_contents($tmpdir . DIRECTORY_SEPARATOR . $file);
+					if(empty($context['idtype']))
+					{ // reload
+						$context['idtype'] = $GLOBALS['db']->GetOne(lq('SELECT idtype FROM #_TP_entities WHERE id='.(int)$context['identity']));
+					}
+					try
+					{
+						$parser = new TEIParser($context['idtype']);
+						$contents['contents'] = $parser->parse($teiContents, '', $tmpdir, $sourceoriginale);
+					}
+					catch(Exception $e)
+					{
+						printErrors($e->getMessage(), true, $isFrame);
+					}
 
-                    $contents['parserreport'] = $parser->getLogs();
+					$contents['parserreport'] = $parser->getLogs();
 
-                    if(C::get('sortie') && C::get('adminlodel', 'lodeluser'))
-                    {
-                        array_walk_recursive($contents, create_function('&$var', '$var = htmlentities($var, ENT_COMPAT, "UTF-8");'));
-                        echo '<pre>'. print_r($contents, 1) . '</pre>';
-                        die();
-                    }
+					if(C::get('sortie') && C::get('adminlodel', 'lodeluser'))
+					{
+						array_walk_recursive($contents, create_function('&$var', '$var = htmlentities($var, ENT_COMPAT, "UTF-8");'));
+						echo '<pre>'. print_r($contents, 1) . '</pre>';
+						die();
+					}
 
-                    $row = array();
-                    $row['fichier']         = $contents;
-                    $row['tei']             = $teiContents;
-                    $row['sourceoriginale'] = $sourceoriginale;
-                    $row['source']          = file_get_contents($source);
-                    // build the import
-                    $row['importversion']   = "oochargement ".C::get('version', 'cfg').";";
-                    $row['identity']        = $context['identity'];
-                    $row['idparent']        = $context['idparent'];
-                    $row['idtype']          = $context['idtype'];
-                    $row['reload']          = $context['reload'];
+					$row = array();
+					$row['fichier']         = $contents;
+					$row['tei']             = $teiContents;
+					$row['sourceoriginale'] = $sourceoriginale;
+					$row['source']          = file_get_contents($source);
+					// build the import
+					$row['importversion']   = "oochargement ".C::get('version', 'cfg').";";
+					$row['identity']        = $context['identity'];
+					$row['idparent']        = $context['idparent'];
+					$row['idtype']          = $context['idtype'];
+					$row['reload']          = $context['reload'];
+
+					delete_files($source);
+
 					$idtask = $task->createAction("Import $file1", 3, $row);
 					printJavascript('window.parent.o.changeStep(3, "'.$idtask.'");');
 
-                    delete_files($source);
-
-                    die;
-                }
-            }
+					die;
+				}
+			}
 		}
 		else
 		{
 			$oldtmpdir = $tmpdir;
 			$tmpdir = array();
 
-            $extracted_files = extract_files_from_zip($source, $oldtmpdir);
-            if($extracted_files)
-            {
-                foreach($extracted_files as $file)
-                {
-                    if(in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), array('doc', 'docx', 'sxw', 'odt', 'rtf')))
-                    {
-                        $tmp = tmpdir(uniqid('import_', true));
-                        rename($oldtmpdir . DIRECTORY_SEPARATOR . $file, $tmp . DIRECTORY_SEPARATOR . $file);
-                        $sources[$file] = $tmp . DIRECTORY_SEPARATOR . $file;
-                        $tmpdir[] = $tmp;
-                    }
-                }
-            }else{
-                printErrors('No files were extracted from the archive '.$source, true, $isFrame);
-            }
+			$extracted_files = extract_files_from_zip($source, $oldtmpdir);
+			if($extracted_files)
+			{
+				foreach($extracted_files as $file)
+				{
+					if(in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), array('doc', 'docx', 'sxw', 'odt', 'rtf')))
+					{
+						$tmp = tmpdir(uniqid('import_', true));
+						rename($oldtmpdir . DIRECTORY_SEPARATOR . $file, $tmp . DIRECTORY_SEPARATOR . $file);
+						$sources[$file] = $tmp . DIRECTORY_SEPARATOR . $file;
+						$tmpdir[] = $tmp;
+					}
+				}
+			}else{
+				printErrors('No files were extracted from the archive '.$source, true, $isFrame);
+			}
 
 			if(!function_exists('removefilesfromimport'))
 			{
@@ -302,20 +302,21 @@ try
 		}
 
 		$row = array();
-		$row['fichier'] = $contents;
-		$row['tei'] = $teiContents;
+		$row['fichier']         = $contents;
+		$row['tei']             = $teiContents;
 		$row['sourceoriginale'] = $sourceoriginale;
 		// build the import
 		$row['importversion']   = "oochargement ".C::get('version', 'cfg').";";
-		$row['identity']      = $context['identity'];
-		$row['idparent']      = $context['idparent'];
-		$row['idtype']        = $context['idtype'];
-		$row['reload']        = $context['reload'];
+		$row['identity']        = $context['identity'];
+		$row['idparent']        = $context['idparent'];
+		$row['idtype']          = $context['idtype'];
+		$row['reload']          = $context['reload'];
 
 		delete_files($source);
 
 		$idtask = $task->createAction("Import $file1", 3, $row);
 		printJavascript('window.parent.o.changeStep(3, "'.$idtask.'");');
+
 		die;
 	}
 	elseif(!in_array($ext, array('doc', 'docx', 'sxw', 'odt', 'rtf')))
@@ -393,8 +394,8 @@ try
 		$request = array('schema' => $schema);
 		$request['attachment'] = $source;
 		$request['mode'] = 'lodel:'.$mode;
-        $request['site'] = $site;
-        $request['sourceoriginale'] = $sourceoriginale;
+		$request['site'] = $site;
+		$request['sourceoriginale'] = $sourceoriginale;
 
 		$client->request($request);
 
@@ -455,21 +456,21 @@ try
 			}
 
 			$row = array();
-			$row['fichier']			= $contents;
-			$row['odt']				= file_get_contents($odtconverted);
-			$row['tei']				= file_get_contents($tei);
-			$row['source']			= file_get_contents($source);
-			$row['sourceoriginale']	= $sourceoriginale;
+			$row['fichier']         = $contents;
+			$row['odt']             = file_get_contents($odtconverted);
+			$row['tei']             = file_get_contents($tei);
+			$row['source']          = file_get_contents($source);
+			$row['sourceoriginale'] = $sourceoriginale;
 			// build the import
-			$row['importversion']	= "oochargement ".C::get('version', 'cfg').";";
-			$row['identity']		= $context['identity'];
-			$row['idparent']		= $context['idparent'];
-			$row['idtype']			= $context['idtype'];
-            $row['reload']          = $context['reload'];
+			$row['importversion']   = "oochargement ".C::get('version', 'cfg').";";
+			$row['identity']        = $context['identity'];
+			$row['idparent']        = $context['idparent'];
+			$row['idtype']          = $context['idtype'];
+			$row['reload']          = $context['reload'];
 
 			delete_files($source, $tei, $odtconverted);
-			$idtask = $task->createAction("Import $file1", 3, $row);
 
+			$idtask = $task->createAction("Import $file1", 3, $row);
 			if(empty($context['multiple']))
 			{
 				printJavascript('window.parent.o.changeStep(3, "'.$idtask.'");');
