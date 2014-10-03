412adecb066fcf098d50c4b74f72608e5b6798dd
ArnAud <arnaud.cordier@openedition.org>
2014-10-03 13:37:34 +0200

    Début d'implémentation d'une classe LodelSql
    Le but est de contrôler nous même notre classe d'abstraction sql

---
+++ lodel/scripts/lodelsql.php
@@ -0,0 +1,77 @@
+<?php
+
+class LodelSql
+{
+    $debug = false;
+    $memcachecompress = false;
+    $memcachehost;
+    $memcacheport;
+    $memcache;
+
+    public function affected_rows() {
+
+    }
+    public function cacheflush($sql=false,$inputarr=false) {
+
+    }
+    public function cachegetone($secs2cache,$sql=false,$inputarr=false) {
+
+    }
+    public function connect($argHostname = "", $argUsername = "", $argPassword = "", $argDatabaseName = "", $forceNew = false) {
+
+    }
+    public function database($database = null) {
+
+    }
+    public function errormsg() {
+
+    }
+    public function errorno() {
+
+    }
+    public function execute($sql,$inputarr=false) {
+
+    }
+    public function getarray($sql,$inputarr=false) {
+
+    }
+    public function getcol($sql, $inputarr = false, $trim = false) {
+
+    }
+    public function getone($sql,$inputarr=false) {
+
+    }
+    public function getrow($sql,$inputarr=false) {
+
+    }
+    public function insert_id($table='',$column='') {
+
+    }
+    public function metacolumns($table, $normalize=true) {
+
+    }
+    public function metadatabases() {
+
+    }
+    public function metatables($ttype=false,$showSchema=false,$mask=false) {
+
+    }
+    public function qstr($s,$magic_quotes=false) {
+
+    }
+    public function quote($s) {
+
+    }
+    public function selectdb($dbName) {
+
+    }
+    public function selectlimit($sql,$nrows=-1,$offset=-1, $inputarr=false,$secs2cache=0) {
+
+    }
+    public function serverinfo() {
+
+    }
+    public function setfetchmode($mode) {
+
+    }
+}
