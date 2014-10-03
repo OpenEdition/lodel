20321d04d0ec9b84055496f09a7e644fe5b331a6
ArnAud <arnaud.cordier@openedition.org>
2014-10-03 16:59:04 +0200

    lodelSql: efface les methodes non utilisée dans Lodel Organise la place des fonctions dans le fichier

--- lodel/scripts/lodelsql.php
+++ lodel/scripts/lodelsql.php
@@ -22,31 +22,16 @@ class LodelSql
 		$this->memcache = &$this->connectionObject->memcache;
 	}
 
-	public function Affected_Rows() {
-		return $this->connectionObject->Affected_Rows();
-	}
-
-	public function CacheExecute($secs2cache, $sql=false, $inputarr=false){
-		return $this->connectionObject->CacheExecute($secs2cache, $sql, $inputarr);
-	}
-
-	public function CacheFlush($sql=false, $inputarr=false) {
-		return $this->connectionObject->cacheflush($sql, $inputarr);
-	}
-
-	public function cachegetone($secs2cache, $sql=false, $inputarr=false) {
-	}
+	/*
+	Server functions
+	*/
 
 	public function connect($argHostname="", $argUsername="", $argPassword="", $argDatabaseName="", $forceNew=false) {
 		return $this->connectionObject->connect($argHostname, $argUsername, $argPassword, $argDatabaseName);
 	}
 
-	function Concat(){
-		return $this->connectionObject->Concat();
-	}
-
-	public function database($database=null) {
-		$this->connectionObject->database = $database;
+	public function selectDB($dbName) {
+		return $this->connectionObject->SelectDB($dbName);
 	}
 
 	public function errormsg() {
@@ -57,119 +42,128 @@ class LodelSql
 		return $this->connectionObject->errorno();
 	}
 
+	// used only oncescripts/view.php:224
+	public function selectlimit($sql, $nrows=-1, $offset=-1, $inputarr=false, $secs2cache=0) {
+		return $this->connectionObject->SelectLimit($sql, $nrows, $offset, $inputarr, $secs2cache);
+	}
+
+	// used only once src/lodel/edition/tpl/dashboard_information.html:129
+	public function serverInfo() {
+		return $this->connectionObject->ServerInfo();
+	}
+
+	// not used, but could be usefull
+	public function setCharSet($charset){
+		return $this->connectionObject->SetCharSet($charset);
+	}
+
+	// not used, but could be usefull
+	public function setFetchMode($mode) {
+		return $this->connectionObject->SetFetchMode($mode);
+	}
+
+	/*
+	SQL functions
+	*/
+
+	public function prepare($sql){
+			return $this->connectionObject->Prepare($sql);
+	}
+
 	public function execute($sql, $inputarr=false) {
 		return $this->connectionObject->execute($sql, $inputarr);
 	}
 
-	public function GetActiveRecordsClass(
-			$class, $table, $whereOrderBy=false, $bindarr=false,
-			$primkeyArr=false, $extra=array(), $relations=array()){
-		return $this->connectionObject->GetActiveRecordsClass(
-			$class, $table, $whereOrderBy, $bindarr,
-			$primkeyArr, $extra, $relations);
+	public function query($sql, $inputarr=false){
+		return $this->connectionObject->Query($sql, $inputarr);
 	}
 
-	public function GetActiveRecords($table, $where=false, $bindarr=false, $primkeyArr=false){
-		return $this->connectionObject->GetActiveRecords($table, $where, $bindarr, $primkeyArr);
-	}
-
-	public function GetAll($sql, $inputarr=false){
+	public function getAll($sql, $inputarr=false){
 		return $this->connectionObject->GetAll($sql, $inputarr);
+	}
+
+	public function getOne($sql, $inputarr=false) {
+		return $this->connectionObject->GetOne($sql, $inputarr);
 	}
 
 	public function getArray($sql, $inputarr=false) {
 		return $this->connectionObject->GetArray($sql, $inputarr);
 	}
 
-	public function getcol($sql, $inputarr=false, $trim=false) {
+	public function getAssoc() {
+		return $this->connectionObject->GetAssoc();
+	}
+
+	public function getCol($sql, $inputarr=false, $trim=false) {
 		return $this->connectionObject->GetCol($sql, $inputarr, $trim);
 	}
 
-	public function getone($sql, $inputarr=false) {
-		return $this->connectionObject->GetOne($sql, $inputarr);
+	public function getRow($sql, $inputarr=false) {
+		return $this->connectionObject->GetRow($sql,$inputarr);
 	}
 
-	public function getrow($sql, $inputarr=false) {
-		return $this->connectionObject->GetRow($sql,$inputarr);
+	public function Affected_Rows() {
+		return $this->connectionObject->Affected_Rows();
 	}
 
 	public function Insert_ID($table='', $column='') {
 		return $this->connectionObject->Insert_ID($table, $column);
 	}
 
-	public function MetaColumns($table, $normalize=true) {
-		return $this->connectionObject->MetaColumns($table, $normalize);
-	}
-
-	public function MetaDatabases() {
-		return $this->connectionObject->MetaDatabases();
-	}
-
-	public function MetaTables($ttype=false, $showSchema=false, $mask=false) {
-		return $this->connectionObject->MetaTables($ttype, $showSchema, $mask);
-	}
-
-	public function MetaPrimaryKeys($table, $owner=false) {
-		return $this->connectionObject->MetaPrimaryKeys($table, $owner);
-	}
-
-	public function MetaType($t,$len=-1, $fieldobj=false){
-		return $this->connectionObject->MetaType($t, $len, $fieldobj);
-	}
-
-	public function outp_throw($msg, $src='WARN', $sql=''){
-		return $this->connectionObject->outp_throw($msg, $src, $sql);
-	}
-
-	public function Prepare($sql){
-			return $this->connectionObject->Prepare($sql);
-	}
-
-	public function Param($name, $type='C'){
-		return $this->connectionObject->Param($name, $type);
-	}
-
-	public function Parameter(&$stmt, &$var, $name, $isOutput=false, $maxLen=4000, $type=false){
-		return $this->connectionObject->Parameter($stmt, $var, $name, $isOutput, $maxLen, $type);
-	}
-
-	public function qstr($s, $magic_quotes=false) {
-		return $this->connectionObject->qstr($s, $magic_quotes);
-	}
-
-	public function Query($sql, $inputarr=false){
-		return $this->connectionObject->Query($sql, $inputarr);
+	function concat(){
+		return $this->connectionObject->Concat();
 	}
 
 	public function quote($s) {
 		return $this->connectionObject->quote($s);
 	}
 
-	public function Replace($table, $fieldArray, $keyCol, $autoQuote=false, $has_autoinc=false){
-		return $this->connectionObject->Replace($table, $fieldArray, $keyCol, $autoQuote, $has_autoinc);
+	// used only once scripts/loginfunc.php:88
+	public function qstr($s, $magic_quotes=false) {
+		return $this->connectionObject->qstr($s, $magic_quotes);
 	}
 
-	public function SelectDB($dbName) {
-		return $this->connectionObject->SelectDB($dbName);
+	/*
+	Schema
+	*/
+
+	// used only once scripts/tablefields.php:79
+	public function metaColumns($table, $normalize=true) {
+		return $this->connectionObject->MetaColumns($table, $normalize);
 	}
 
-	public function selectlimit($sql, $nrows=-1, $offset=-1, $inputarr=false, $secs2cache=0) {
-		return $this->connectionObject->SelectLimit($sql, $nrows, $offset, $inputarr, $secs2cache);
+	public function metaDatabases() {
+		return $this->connectionObject->MetaDatabases();
 	}
 
-	public function serverinfo() {
-		return $this->connectionObject->ServerInfo();
+	public function metaTables($ttype=false, $showSchema=false, $mask=false) {
+		return $this->connectionObject->MetaTables($ttype, $showSchema, $mask);
 	}
 
-	public function SetCharSet($charset){
-		return $this->connectionObject->SetCharSet($charset);
+	public function metaPrimaryKeys($table, $owner=false) {
+		return $this->connectionObject->MetaPrimaryKeys($table, $owner);
 	}
 
-	public function GetAssoc() {
-		return $this->connectionObject->GetAssoc();
+	public function metaType($t,$len=-1, $fieldobj=false){
+		return $this->connectionObject->MetaType($t, $len, $fieldobj);
 	}
 
-	public function setfetchmode($mode) {
-		return $this->connectionObject->SetFetchMode($mode);
+	/*
+	memcache functions
+	*/
+
+	public function cacheExecute($secs2cache, $sql=false, $inputarr=false){
+		return $this->connectionObject->CacheExecute($secs2cache, $sql, $inputarr);
 	}
+
+	public function cacheFlush($sql=false, $inputarr=false) {
+		return $this->connectionObject->cacheflush($sql, $inputarr);
+	}
+
+	// TODO: test it
+	// scripts/lodelsql.php:162
+	// scripts/loginfunc.php:409
+	public function cacheGetOne($secs2cache, $sql=false, $inputarr=false) {
+	}
+
 }
