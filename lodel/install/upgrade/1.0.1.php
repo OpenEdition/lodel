00319725f32af7bad18ba8642be455725287902e
ArnAud <arnaud.cordier@openedition.org>
2015-11-17 12:22:24 +0100

    Upgrade 0.9 -> 1.0 : scripts cli utilisant l'interface standard

--- lodel/install/upgrade/1.0.1.php
+++ lodel/install/upgrade/1.0.1.php
@@ -9,29 +9,16 @@
  * Ajout des tables pour les attributs d'entrées d'index
  */
 
-define('backoffice-lodeladmin', true);
+require_once('lodel/install/scripts/me_manipulation_func.php');
+define('DO_NOT_DIE', true); // Ne mourir qu'en cas d'erreur grave
+// 	define('QUIET', true); // Pas de sortie du tout
 
-require_once 'lodelconfig.php';
-require_once 'lodel/scripts/context.php';
-C::setCfg($cfg);
-
-require_once 'lodel/scripts/connect.php';
-require_once 'lodel/scripts/auth.php';
-
-/* Vérification que nous sommes en php-cli, sinon nous vérifions les droits*/
-if (!array_key_exists('SHELL', $_ENV))
-authenticate(LEVEL_ADMINLODEL);
+$sites = new ME_sites_iterator($argv, 'errors', 0); // 'errors' ne montre que les erreurs de la fonction ->m(), 0 est le statut minimal du site
 
 global $db;
-$sites = $db->Execute(lq("
-            SELECT name, status 
-                FROM #_MTP_sites 
-                WHERE status > 0")) 
-or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
 
-
-foreach($sites as $site){
-	$db->SelectDB(DATABASE . "_{$site['name']}");
+while ($siteName = $sites->fetch()) {
+	echo "\tAjout des tables pour les attributs d'entrées d'index\n";
 	$classes = $db->Execute(lq('SELECT * FROM #_TP_classes WHERE classtype = "entries";'));
 	foreach($classes as $class){
 		$db->execute(lq ("CREATE TABLE IF NOT EXISTS #_TP_entities_". $class['class'] ." ( idrelation INTEGER UNSIGNED UNIQUE, KEY index_idrelation (idrelation) )"));
--- lodel/install/upgrade/1.0.2.php
+++ lodel/install/upgrade/1.0.2.php
@@ -6,33 +6,20 @@
  */
 
 /**
- * Ajout des tables pour les attributs d'entrées d'index
+ * Mise à jour des tables `tasks` et `relations_ext`
  */
 
-define('backoffice-lodeladmin', true);
+require_once('lodel/install/scripts/me_manipulation_func.php');
+define('DO_NOT_DIE', true); // Ne mourir qu'en cas d'erreur grave
+// 	define('QUIET', true); // Pas de sortie du tout
 
-require_once 'lodelconfig.php';
-require_once 'lodel/scripts/context.php';
-C::setCfg($cfg);
-
-require_once 'lodel/scripts/connect.php';
-require_once 'lodel/scripts/auth.php';
-
-/* Vérification que nous sommes en php-cli, sinon nous vérifions les droits*/
-if (!array_key_exists('SHELL', $_ENV))
-authenticate(LEVEL_ADMINLODEL);
+$sites = new ME_sites_iterator($argv, 'errors', 0); // 'errors' ne montre que les erreurs de la fonction ->m(), 0 est le statut minimal du site
 
 global $db;
-$sites = $db->Execute(lq("
-            SELECT name, status 
-                FROM #_MTP_sites 
-                WHERE status > 0")) 
-or trigger_error("SQL ERROR :<br />".$GLOBALS['db']->ErrorMsg(), E_USER_ERROR);
 
-
-foreach($sites as $site){
-	$db->SelectDB(DATABASE . "_{$site['name']}");
+while ($siteName = $sites->fetch()) {
+	echo "\tMise à jour des tables `tasks` et `relations_ext`\n";
 	$db->execute(lq ("ALTER TABLE #_TP_relations_ext DROP KEY `id1`;"));
 	$db->execute(lq ("ALTER TABLE #_TP_relations_ext ADD UNIQUE KEY `id1` (`id1`,`id2`,`degree`,`nature`, `site`);"));
-    $db->execute(lq ("ALTER TABLE #_TP_tasks CHANGE COLUMN `context`  `context` longtext;"));
+	$db->execute(lq ("ALTER TABLE #_TP_tasks CHANGE COLUMN `context`  `context` longtext;"));
 }
