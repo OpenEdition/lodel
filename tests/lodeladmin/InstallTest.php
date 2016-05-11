677e1b89ec1ab9da128c0fbf60a345b66db7c730
Charles-Edouard Coste <cc@synap.fr>
2016-05-11 17:28:14 +0200

    Ajout de l'intégration continue

---
+++ tests/lodeladmin/InstallTest.php
@@ -0,0 +1,139 @@
+<?php
+class InstallTest extends PHPUnit_Framework_TestCase
+{
+	public function testGet()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testSet()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testTestInstallDB()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testInstallConf()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testMajConfDB()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testManageDB()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testManageAdmin()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testSet_htaccess()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testMaj_options()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testDownloadlodelconfig()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testShowlodelconfig()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testStartInstall()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testTestRights()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testCheckConfig()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testCheckFunc()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testCheckDB()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testSeekDB()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testInstallDB()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testVerifyAdmin()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testCheckHtaccess()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testAskOptions()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testVerifyLodelConfig()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testFinish()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testInclude_tpl()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testProblem_include()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testProblem()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+	public function testMakeSelectLang()
+  {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+}
