5e41927cb0e1794c477db8f31e9f4d0283196727
bcenou <bruno.cenou@openedition.org>
2017-07-24 17:34:11 +0200

    cosmétique

--- lodeladmin/install.php
+++ lodeladmin/install.php
@@ -1,9 +1,24 @@
 <?php
 
-class Install{
-	
-	public function  __construct(){
+class Install {
 
+	static public $php_exts = array(
+				'mbstring',
+				'xml',
+				'gd',
+				'curl',
+				'mysqlnd',
+				'zip'
+				);
+	
+
+	static public function checkPHPExts($ext) { 
+		return extension_loaded($ext);		
 	}
 
+	static public function checkLodelCfgExists() {
+		return is_file('../lodelconfig.php') && is_readable('../lodelconfig.php');
+	}
 }
+
+var_dump(Install::checkLodelCfgExists());
