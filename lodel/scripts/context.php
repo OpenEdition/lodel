09dfbe0979d7bed7c7ae0a9716c0a1f74a0c769c
Hélène Prieto <helene.prieto@openedition.org>
2022-07-04 11:09:25 +0200

    Fix injection problems

--- lodel/scripts/context.php
+++ lodel/scripts/context.php
@@ -199,9 +199,9 @@ class C
 						if(is_array($v))
 						{
 							foreach($v as $kk => $vv)
-								$uri .= $k[$kk]."=".strip_tags($vv).'&';
+								$uri .= $k[$kk]."=".$vv.'&';
 						}
-						else $uri .= $k."=".strip_tags($v).'&';
+						else $uri .= $k."=".$v.'&';
 					}
 				}
 			}
@@ -219,10 +219,15 @@ class C
 
 			self::cleanRequest($_GET);
 			foreach($_GET as $k=>$v)
-			{
-				self::$_context[$k] = strip_tags($v);
+			{	
+				if (is_array($v)) {
+					foreach($v as $value) {
+						self::$_context[$k][] = strip_tags($value);
+					}
+				}else {
+					self::$_context[$k] = strip_tags($v);
+				}
 			}
-
 			if (!empty($_POST)) 
 			{
 				self::$_cfg['isPost'] = true; // needed for template engine (save or not calculed page)
